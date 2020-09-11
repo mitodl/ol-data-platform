6cf636422ba7492b65b33da19b8969f9d609f606
Tobias Macey <tmacey@mit.edu>
2020-09-11 15:37:30 -0400

    Updated poetry lock and added dagster-cron dependency

--- tasks.py
+++ tasks.py
@@ -1,10 +1,9 @@
-import sys
 from pathlib import Path
 
 from invoke import task
 from poetry.factory import Factory
 from vdist.builder import Builder
-from vdist.source import git
+from vdist.source import git  # , directory
 
 
 @task
@@ -16,12 +15,13 @@ def package_pipeline(context):
         name=app_name,
         app=app_name,
         version=poetry.package.version.text,
+        # source=directory(str(Path(__file__).parent)),
         source=git(
             uri=poetry.package.urls['Repository'],
             branch='main'
         ),
         profile='debian',
-        python_version='3.8.4',
+        python_version='3.8.5',
         build_deps=[
             'libffi-dev',
             'build-essential',
@@ -34,10 +34,10 @@ def package_pipeline(context):
             'mongo-tools'
         ],
         fpm_args=(
-            '--config-files=workspace.yaml '
             '--maintainer=odl-devops@mit.edu '
             '--after-install=buildprofiles/after_install.sh '
-            '--template-scripts'
+            '--template-scripts '
+            'workspace.yaml=/etc/dagster/workspace.yaml'
         )
     )
     builder.get_available_profiles()
