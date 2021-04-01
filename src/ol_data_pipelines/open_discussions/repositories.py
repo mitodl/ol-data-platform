3220b525b4d15c79b879599b9023436b6f711e03
abeglova <abeglova@mit.edu>
2021-04-01 17:52:47 -0400

    schedules for enrollment updates

--- src/ol_data_pipelines/open_discussions/repositories.py
+++ src/ol_data_pipelines/open_discussions/repositories.py
@@ -1,6 +1,9 @@
 """Reposiotries for open-discussions pipelines."""
 from dagster import repository
 
+from ol_data_pipelines.open_discussions.sensors import (
+    mitx_bigquery_pipeline_completion_sensor,
+)
 from ol_data_pipelines.open_discussions.solids import (
     pull_open_data_pipeline,
     update_enrollments_pipeline,
@@ -11,6 +14,10 @@ from ol_data_pipelines.open_discussions.solids import (
 def open_data_repository():
     """Repositories for open enrollments pipeline.
 
-    :returns: open data pipelines
+    :returns: open data pipelines and schedules
     """
-    return [pull_open_data_pipeline, update_enrollments_pipeline]
+    return [
+        pull_open_data_pipeline,
+        update_enrollments_pipeline,
+        mitx_bigquery_pipeline_completion_sensor,
+    ]
---
+++ src/ol_data_pipelines/open_discussions/sensors.py
@@ -0,0 +1,32 @@
+from dagster import RunRequest, sensor
+from dagster.core.storage.pipeline_run import PipelineRunsFilter, PipelineRunStatus
+
+from ol_data_pipelines.open_discussions.solids import update_enrollments_pipeline
+
+production_preset = update_enrollments_pipeline.get_preset("production")
+
+
+@sensor(
+    pipeline_name="update_enrollments_pipeline",
+    mode="production",
+    minimum_interval_seconds=600,
+)
+def mitx_bigquery_pipeline_completion_sensor(context):
+    """
+    Run update_enrollments_pipeline after mitx_bigquery_pipeline completes.
+
+    :param context: Dagster execution context for configuration data
+    :type context: SolidExecutionContext
+
+    :yield: A RunRequest to begin update_enrollments_pipeline execution
+    """
+    runs = context.instance.get_runs(
+        filters=PipelineRunsFilter(
+            pipeline_name="mitx_bigquery_pipeline",
+            statuses=[PipelineRunStatus.SUCCESS],
+        ),
+    )
+
+    run = runs[0]
+
+    yield RunRequest(run_key=str(run.run_id), run_config=production_preset.run_config)
