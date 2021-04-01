d4e0697b6e8404824cfe7b7a4cd7a3ea0998a05e
abeglova <abeglova@mit.edu>
2021-04-01 17:52:47 -0400

    schedules for enrollment updates

--- src/ol_data_pipelines/mitx_bigquery/repositories.py
+++ src/ol_data_pipelines/mitx_bigquery/repositories.py
@@ -1,10 +1,14 @@
-"""Repository for pipeline pulling MITx bigquery data to S3"""
+"""Repository for pipeline pulling MITx bigquery data to S3."""
 from dagster import repository
 
+from ol_data_pipelines.mitx_bigquery.schedule import mitx_bigquery_daily_schedule
 from ol_data_pipelines.mitx_bigquery.solids import mitx_bigquery_pipeline
 
 
 @repository
 def mitx_bigquery_repository():
-    """repository for mitx bigquery pipeline"""
-    return [mitx_bigquery_pipeline]
+    """Repository for mitx bigquery pipeline.
+
+    :returns: open data pipelines and schedules
+    """
+    return [mitx_bigquery_pipeline, mitx_bigquery_daily_schedule]
