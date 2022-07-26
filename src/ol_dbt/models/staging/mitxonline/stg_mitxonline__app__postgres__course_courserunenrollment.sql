5caf3d0c1a21aafaf18be86de27422b65e532522
Daniel Seaton <dseaton@Daniels-MacBook-Pro-66.local>
2022-07-26 15:27:54 -0400

    [WIP] Using CTEs and field descriptions/documentation to restyle MITx Online staging SQL.

--- src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__course_courserunenrollment.sql
+++ src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__course_courserunenrollment.sql
@@ -1,9 +1,24 @@
-select
-    id
-    , active
-    , run_id
-    , user_id
-    , cast(created_on[1] as timestamp(6)) as created_on
-    , cast(updated_on[1] as timestamp(6)) as updated_on
-from
-    {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__courses_courserunenrollment') }}
+-- MITx Online Course Run Enrollment Information 
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__courses_courserunenrollment') }}
+),
+
+cleaned as (
+    select 
+        -- int, sequential ID tracking a single user enrollment
+        id
+        -- str, boolean describing whether the enrollment is active
+        , active
+        -- int, unique ID specifying a "run" of an MITx Online course
+        , run_id
+        -- int, unique ID for each user on the MITx Online platform
+        , user_id
+        -- timestamp, specifying when an enrollment was initially created
+        , cast(created_on[1] as timestamp(6)) as created_on
+        -- timestamp, specifying when an enrollment was most recently updated
+        , cast(updated_on[1] as timestamp(6)) as updated_on 
+    from source
+)
+
+select * from cleaned
--- src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
+++ src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
@@ -1,7 +1,19 @@
-select
+-- MITx Online User Information
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__users_user') }}
+),
+
+cleaned as (
+    -- int, sequential ID representing one user in MITx Online
     id
+    -- boolean, ...
     , is_active
+    -- timestamp, specifying when a user account was initially created
     , cast(created_on[1] as timestamp(6)) as created_on
+    -- timestamp, specifying when a user account was most recently updated
     , cast(updated_on[1] as timestamp(6)) as updated_on
-from
-    {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__users_user') }}
+) 
+
+select * from cleaned
+    
