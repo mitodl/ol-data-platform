5edbe141e897984bec48e39a6371a291d5f661fe
Daniel Seaton <dseaton@Daniels-MacBook-Pro-66.local>
2022-07-27 16:07:53 -0400

    Implement intermediate SQL to support an overall enrollment mart for Open Learning.

---
+++ src/ol_dbt/models/intermediate/mitxonline/int_mitxonline__enrollments.sql
@@ -0,0 +1,37 @@
+-- Enrollment information for MITx Online
+
+with enrollments as (
+    select * from {{ ref('stg_mitxonline__app__postgres__course_courserunenrollment') }}
+),
+
+runs as (
+    select
+        id
+        , title
+        , courseware_url_path
+    from {{ ref('stg_mitxonline__app__postgres__courses_courserun') }}
+),
+
+users as (
+    select
+        id
+        , is_active
+    from {{ ref('stg_mitxonline__app__postgres__users_user') }}
+),
+
+mitxonline_enrollments as (
+    select
+        enrollments.id
+        , enrollments.active as "course_run_active"
+        , enrollments.user_id
+        , enrollments.created_on
+        , enrollments.updated_on 
+        , runs.courseware_url_path
+        , runs.title as "course_title"
+        , users.is_active as "user_active"
+    from enrollments
+    join runs on enrollments.run_id = runs.id
+    join users on enrollments.user_id = users.id
+)
+
+select * from mitxonline_enrollments
---
+++ src/ol_dbt/models/intermediate/mitxpro/int_mitxpro__enrollments.sql
@@ -0,0 +1,37 @@
+-- Enrollment information for xPro
+
+with enrollments as (
+    select * from {{ ref('stg_mitxpro__app__postgres__courses_courserunenrollment') }}
+),
+
+runs as (
+    select
+        id
+        , title
+        , courseware_url_path
+    from {{ ref('stg_mitxpro__app__postgres__courses_courserun') }}
+),
+
+users as (
+    select
+        id
+        , is_active
+    from {{ ref('stg_mitxpro__app__postgres__users_user') }}
+),
+
+mitxpro_enrollments as (
+    select
+        enrollments.id
+        , enrollments.active as "course_run_active"
+        , enrollments.user_id
+        , enrollments.created_on
+        , enrollments.updated_on 
+        , runs.courseware_url_path
+        , runs.title as "course_title"
+        , users.is_active as "user_active"
+    from enrollments
+    join runs on enrollments.run_id = runs.id
+    join users on enrollments.user_id = users.id
+)
+
+select * from mitxpro_enrollments
