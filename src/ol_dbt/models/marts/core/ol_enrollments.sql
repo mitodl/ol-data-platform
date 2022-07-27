75d830e5441658c33f691e2f0d9e0577917a392e
Daniel Seaton <dseaton@Daniels-MacBook-Pro-66.local>
2022-07-27 16:49:35 -0400

    Initial OL enrollment mart. Core will contain models widely applicable to OL.

---
+++ src/ol_dbt/models/marts/core/ol_enrollments.sql
@@ -0,0 +1,17 @@
+with mitxonline_enrollments as (
+    select * from {{ ref('int_mitxonline__enrollments') }}
+),
+
+mitxpro_enrollments as (
+    select * from {{ ref('int_mitxpro__enrollments') }}
+),
+
+join_ol_enrollments as (
+    select *, 'MITx Online' as org from mitxonline_enrollments
+
+    union all
+
+    select *, 'xPro' as org from mitxpro_enrollments
+)
+
+select * from join_ol_enrollments
