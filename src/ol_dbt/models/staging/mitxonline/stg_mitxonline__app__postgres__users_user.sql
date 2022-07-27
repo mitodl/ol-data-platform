02eeea84854ea5cea5bef83b05074891d26e2512
Daniel Seaton <dseaton@Daniels-MacBook-Pro-66.local>
2022-07-27 10:45:39 -0400

    [WIP] Fix the missing select/from statements.

--- src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
+++ src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
@@ -5,14 +5,15 @@ with source as (
 ),
 
 cleaned as (
-    -- int, sequential ID representing one user in MITx Online
-    id
-    -- boolean, ...
-    , is_active
-    -- timestamp, specifying when a user account was initially created
-    , cast(created_on[1] as timestamp(6)) as created_on
-    -- timestamp, specifying when a user account was most recently updated
-    , cast(updated_on[1] as timestamp(6)) as updated_on
+    select 
+        -- int, sequential ID representing one user in MITx Online
+        id
+        -- boolean, ...
+        , is_active
+        -- timestamp, specifying when a user account was initially created
+        , cast(created_on[1] as timestamp(6)) as created_on
+        -- timestamp, specifying when a user account was most recently updated
+        , cast(updated_on[1] as timestamp(6)) as updated_on
     from source
 ) 
 
