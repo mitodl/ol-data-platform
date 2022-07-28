91bb64163e12ca1aaaa6196b6244beea3d882daa
Daniel Seaton <dseaton@users.noreply.github.com>
2022-07-28 10:55:33 -0400

    Restyle Staging SQL using CTEs and field descriptions/documentation (#288)
    Updating the syntax of "Staging SQL" using using Common Table Expressions (CTEs) and field descriptions. Impacts MITx Online and xPro data sources. 
    
    Co-authored-by: Daniel Seaton <dseaton@Daniels-MacBook-Pro-66.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
+++ src/ol_dbt/models/staging/mitxonline/stg_mitxonline__app__postgres__users_user.sql
@@ -1,7 +1,20 @@
-select
-    id
-    , is_active
-    , cast(created_on[1] as timestamp(6)) as created_on
-    , cast(updated_on[1] as timestamp(6)) as updated_on
-from
-    {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__users_user') }}
+-- MITx Online User Information
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','mitxonline__app__postgres__users_user') }}
+)
+
+, cleaned as (
+    select
+        -- int, sequential ID representing one user in MITx Online
+        id
+        -- boolean, ...
+        , is_active
+        -- timestamp, specifying when a user account was initially created
+        , cast(created_on[1] as timestamp(6)) as created_on
+        -- timestamp, specifying when a user account was most recently updated
+        , cast(updated_on[1] as timestamp(6)) as updated_on
+    from source
+)
+
+select * from cleaned
