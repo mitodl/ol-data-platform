930022329034918224a55f6e1d05341e4f6ac2cb
Anastasia Beglova <abeglova@mit.edu>
2022-09-27 08:43:53 -0400

    users_user staging

--- src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__users_user.sql
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__users_user.sql
@@ -5,13 +5,15 @@ with source as (
 )
 
 , cleaned as (
+
     select
         id
         , username
         , email
         , is_active
-        , created_on
-        , updated_on
+        , name as full_name
+        , to_iso8601(from_iso8601_timestamp(created_on)) as joined_on
+        , to_iso8601(from_iso8601_timestamp(last_login)) as last_login
     from source
 )
 
