f68141faa1bc935aa3014cd0633a7e7420fdf487
sar <sar.haidar@gmail.com>
2022-10-03 12:57:50 -0500

    Changes based on feedback

--- src/ol_orchestrate/repositories/asset_materialization.py
+++ src/ol_orchestrate/repositories/asset_materialization.py
@@ -1,21 +1,53 @@
-from dagster import repository
+from dagster import repository, with_resources
 from dagster_airbyte import airbyte_resource
 from dagster_dbt import dbt_cli_resource
+import os
 
 from ol_orchestrate.jobs.sync_assets_and_run_models import sync_assets_and_run_models
 
-airbyte_resource = airbyte_resource.configured(
-    {"host": "airbyte-qa.odl.mit.edu", "port": "443"}
-)
+environment = {
+    "qa": {
+        "airbyte_resource": airbyte_resource.configured(
+        {
+            "host": "airbyte-qa.odl.mit.edu", 
+            "port": "443"
+        }),
+        "airbyte_connection": 
+        {
+            "raw_bootcamps_application_database_tables": "d8415e19-9859-480a-830b-18cd2f6c0392",
+            "raw_mitxonline_application_database_tables": "6e8cdba9-c711-4118-a196-91038cee78a7",
+            "raw_mitxpro_application_database_tables": "442c98f7-9a51-4f10-945d-82f022f1e1a1",
+            "raw_mitx_residential_openedx_database_tables": "358a7580-97f8-419f-8636-a3d08f0e30ab",
+            "raw_ocw_studio_application_database_tables": "ba60ac15-de5c-4eef-9c38-0a89c2360653",
+        }
+    },
+    "production": {
+        "airbyte_resource": airbyte_resource.configured(
+        {
+            "host": "airbyte.odl.mit.edu", 
+            "port": "443"
+        }),
+    }
+}
 
+dagster_deployment = os.getenv("DAGSTER_DEPLOYMENT", "qa")
 
 @repository
-def bootcamps_qa_app_db_to_datalake_raw():
+def bootcamps():
     return [
         sync_assets_and_run_models.to_job(
-            name="bootcamps_qa_app_db_to_datalake_raw",
-            resource_defs={
-                "airbyte": airbyte_resource,
+            name="raw_bootcamps_application_database_tables",
+            config={
+                "ops": {
+                    "sync_airbyte": {
+                        "config": {
+                            "connection_id": environment[dagster_deployment]["airbyte_connection"]["raw_bootcamps_application_database_tables"]
+                        }
+                    }
+                },
+            },
+            resource_defs = {
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
                 "dbt": dbt_cli_resource.configured(
                     {
                         "project_dir": "/opt/dbt/models/staging/bootcamps",
@@ -23,26 +55,17 @@ def bootcamps_qa_app_db_to_datalake_raw():
                     }
                 ),
             },
-            config={
-                "ops": {
-                    "sync_airbyte": {
-                        "config": {
-                            "connection_id": "d8415e19-9859-480a-830b-18cd2f6c0392"
-                        }
-                    }
-                },
-            },
         )
     ]
 
 
 @repository
-def mitxonline_qa_app_db_to_datalake_raw():
+def mitxonline():
     return [
         sync_assets_and_run_models.to_job(
-            name="mitxonline_qa_app_db_to_datalake_raw",
+            name="raw_mitxonline_application_database_tables",
             resource_defs={
-                "airbyte": airbyte_resource,
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
                 "dbt": dbt_cli_resource.configured(
                     {
                         "project_dir": "/opt/dbt/models/staging/mitxonline",
@@ -54,7 +77,28 @@ def mitxonline_qa_app_db_to_datalake_raw():
                 "ops": {
                     "sync_airbyte": {
                         "config": {
-                            "connection_id": "6e8cdba9-c711-4118-a196-91038cee78a7"
+                            "connection_id": environment[dagster_deployment]["airbyte_connection"]["raw_mitxonline_application_database_tables"]
+                        }
+                    }
+                },
+            },
+        ),
+        sync_assets_and_run_models.to_job(
+            name="raw_mitxonline_openedx_database_tables",
+            resource_defs={
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
+                "dbt": dbt_cli_resource.configured(
+                    {
+                        "project_dir": "/opt/dbt/models/staging/mitxonline",
+                        "profiles_dir": "/opt/dbt",
+                    }
+                ),
+            },
+            config={
+                "ops": {
+                    "sync_airbyte": {
+                        "config": {
+                            "connection_id": ""
                         }
                     }
                 },
@@ -64,12 +108,12 @@ def mitxonline_qa_app_db_to_datalake_raw():
 
 
 @repository
-def mitxpro_qa_app_db_to_datalake_raw():
+def mitxpro():
     return [
         sync_assets_and_run_models.to_job(
-            name="mitxpro_qa_app_db_to_datalake_raw",
+            name="raw_mitxpro_application_database_tables",
             resource_defs={
-                "airbyte": airbyte_resource,
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
                 "dbt": dbt_cli_resource.configured(
                     {
                         "project_dir": "/opt/dbt/models/staging/mitxpro",
@@ -81,7 +125,28 @@ def mitxpro_qa_app_db_to_datalake_raw():
                 "ops": {
                     "sync_airbyte": {
                         "config": {
-                            "connection_id": "442c98f7-9a51-4f10-945d-82f022f1e1a1"
+                            "connection_id": environment[dagster_deployment]["airbyte_connection"]["raw_mitxpro_application_database_tables"]
+                        }
+                    }
+                },
+            },
+        ),
+        sync_assets_and_run_models.to_job(
+            name="raw_mitxpro_openedx_database_tables",
+            resource_defs={
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
+                "dbt": dbt_cli_resource.configured(
+                    {
+                        "project_dir": "/opt/dbt/models/staging/mitxpro",
+                        "profiles_dir": "/opt/dbt",
+                    }
+                ),
+            },
+            config={
+                "ops": {
+                    "sync_airbyte": {
+                        "config": {
+                            "connection_id": ""
                         }
                     }
                 },
@@ -91,12 +156,12 @@ def mitxpro_qa_app_db_to_datalake_raw():
 
 
 @repository
-def mitx_residential_qa_openedx_db_to_datalake_raw():
+def mitx_residential():
     return [
         sync_assets_and_run_models.to_job(
-            name="mitx_residential_qa_openedx_db_to_datalake_raw",
+            name="raw_mitx_residential_openedx_database_tables",
             resource_defs={
-                "airbyte": airbyte_resource,
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
                 "dbt": dbt_cli_resource.configured(
                     {"project_dir": "/opt/dbt", "profiles_dir": "/opt/dbt"}
                 ),
@@ -105,7 +170,7 @@ def mitx_residential_qa_openedx_db_to_datalake_raw():
                 "ops": {
                     "sync_airbyte": {
                         "config": {
-                            "connection_id": "358a7580-97f8-419f-8636-a3d08f0e30ab"
+                            "connection_id": environment[dagster_deployment]["airbyte_connection"]["raw_mitx_residential_openedx_database_tables"]
                         }
                     }
                 },
@@ -115,12 +180,12 @@ def mitx_residential_qa_openedx_db_to_datalake_raw():
 
 
 @repository
-def ocw_studio_qa_to_datalake_raw():
+def ocw_studio():
     return [
         sync_assets_and_run_models.to_job(
-            name="ocw_studio_qa_to_datalake_raw",
+            name="raw_ocw_studio_application_database_tables",
             resource_defs={
-                "airbyte": airbyte_resource,
+                "airbyte": environment[dagster_deployment]["airbyte_resource"],
                 "dbt": dbt_cli_resource.configured(
                     {"project_dir": "/opt/dbt", "profiles_dir": "/opt/dbt"}
                 ),
@@ -129,7 +194,7 @@ def ocw_studio_qa_to_datalake_raw():
                 "ops": {
                     "sync_airbyte": {
                         "config": {
-                            "connection_id": "ba60ac15-de5c-4eef-9c38-0a89c2360653"
+                            "connection_id": environment[dagster_deployment]["airbyte_connection"]["raw_ocw_studio_application_database_tables"]
                         }
                     }
                 },
