e43a0f34cd2f316cbe98a34d26e520464fdce606
Anastasia Beglova <abeglova@mit.edu>
2022-10-03 16:48:27 -0400

    intermediate user model

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__users.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__users.sql
@@ -1,9 +1,23 @@
 with users as (
-    select
-        id
-        , username
-        , email
+    select *
     from {{ ref('stg__mitxonline__app__postgres__users_user') }}
 )
 
-select * from users
+, users_legaladdress as (
+    select *
+    from {{ ref('stg__mitxonline__app__postgres__users_legaladdress') }}
+)
+
+select
+    users.id
+    , users.username
+    , users.full_name
+    , users.email
+    , users.joined_on
+    , users.last_login
+    , users_legaladdress.user_address_country
+    , users_legaladdress.first_name
+    , users_legaladdress.last_name
+from users
+left join users_legaladdress on users_legaladdress.user_id = users.id
+where users.is_active = true
