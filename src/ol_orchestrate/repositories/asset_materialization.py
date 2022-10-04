16bde2a5d9fb3a7914563c454f4db573f64158fd
sar <sar.haidar@gmail.com>
2022-10-04 10:40:40 -0500

    Changes based on feedback

--- src/ol_orchestrate/repositories/asset_materialization.py
+++ src/ol_orchestrate/repositories/asset_materialization.py
@@ -14,7 +14,9 @@ environment = {
         "airbyte_connection": {
             "raw_bootcamps_application_database_tables": "d8415e19-9859-480a-830b-18cd2f6c0392",
             "raw_mitxonline_application_database_tables": "6e8cdba9-c711-4118-a196-91038cee78a7",
+            "raw_mitxonline_openedx_database_tables": "79b5fe95-7b8c-4a94-ba4e-5b8e925cfdd9",
             "raw_mitxpro_application_database_tables": "442c98f7-9a51-4f10-945d-82f022f1e1a1",
+            "raw_mitxpro_openedx_database_tables": "cda953b8-e089-4eb8-8cf5-95419d4b6fb4",
             "raw_mitx_residential_openedx_database_tables": "358a7580-97f8-419f-8636-a3d08f0e30ab",
             "raw_ocw_studio_application_database_tables": "ba60ac15-de5c-4eef-9c38-0a89c2360653",
         },
@@ -96,7 +98,15 @@ def mitxonline():
                 ),
             },
             config={
-                "ops": {"sync_airbyte": {"config": {"connection_id": ""}}},
+                "ops": {
+                    "sync_airbyte": {
+                        "config": {
+                            "connection_id": environment[dagster_deployment][
+                                "airbyte_connection"
+                            ]["raw_mitxonline_openedx_database_tables"]
+                        }
+                    }
+                },
             },
         ),
     ]
@@ -140,7 +150,15 @@ def mitxpro():
                 ),
             },
             config={
-                "ops": {"sync_airbyte": {"config": {"connection_id": ""}}},
+                "ops": {
+                    "sync_airbyte": {
+                        "config": {
+                            "connection_id": environment[dagster_deployment][
+                                "airbyte_connection"
+                            ]["raw_mitxpro_openedx_database_tables"]
+                        }
+                    }
+                },
             },
         ),
     ]
