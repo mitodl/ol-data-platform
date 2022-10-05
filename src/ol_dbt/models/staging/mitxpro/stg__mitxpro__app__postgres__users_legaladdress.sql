b5ec6ae02298363fc9c64d26c525a51eacfa2746
Anastasia Beglova <abeglova@mit.edu>
2022-10-05 08:37:39 -0400

    user legaladdress staging

---
+++ src/ol_dbt/models/staging/mitxpro/stg__mitxpro__app__postgres__users_legaladdress.sql
@@ -0,0 +1,29 @@
+-- MITx Online User Information
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__xpro__app__postgres__users_legaladdress') }}
+)
+
+, cleaned as (
+
+    select
+        id
+        , country as user_address_country
+        , user_id
+        , first_name
+        , last_name
+        , city as user_address_city
+        , state_or_territory as user_address_state_or_territory
+        , postal_code as user_address_postal_code
+        , concat_ws(
+            chr(10)
+            , nullif(street_address_1, '')
+            , nullif(street_address_2, '')
+            , nullif(street_address_3, '')
+            , nullif(street_address_4, '')
+            , nullif(street_address_5, '')
+        ) as user_street_address
+    from source
+)
+
+select * from cleaned
