d9ce60f130ed52a2567767b6cdf0c9f46e4aeee1
Anastasia Beglova <abeglova@mit.edu>
2022-10-18 16:57:28 -0400

    bootcamps auth user

--- src/ol_dbt/models/staging/bootcamps/stg__bootcamps__app__postgres__auth_user.sql
+++ src/ol_dbt/models/staging/bootcamps/stg__bootcamps__app__postgres__auth_user.sql
@@ -2,15 +2,16 @@ with source as (
     select * from {{ source('ol_warehouse_raw_data', 'raw__bootcamps__app__postgres__auth_user') }}
 )
 
-, renamed as (
+, cleaned as (
+
     select
-        id
-        , email
-        , username
-        , is_active
-        , last_login
-        , date_joined
+        id as user_id
+        , username as user_username
+        , email as user_email
+        , is_active as user_is_active
+        , to_iso8601(from_iso8601_timestamp(date_joined)) as user_joined_on
+        , to_iso8601(from_iso8601_timestamp(last_login)) as user_last_login
     from source
 )
 
-select * from renamed
+select * from cleaned
