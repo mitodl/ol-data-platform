9ac331fdbff033bd3f172178b54d4aca991e9f00
rachellougee <rlougee@mit.edu>
2022-10-20 13:47:44 -0400

    staged courserun from mitxonline

--- src/ol_dbt/models/intermediate/combined/int__combined__enrollments.sql
+++ src/ol_dbt/models/intermediate/combined/int__combined__enrollments.sql
@@ -2,22 +2,11 @@ with mitxonline_enrollments as (
     select * from {{ ref('int__mitxonline__enrollments') }}
 )
 
-, mitxpro_enrollments as (
-    select * from {{ ref('int__mitxpro__enrollments') }}
-)
-
 , join_ol_enrollments as (
     select
         *
         , 'MITx Online' as platform
     from mitxonline_enrollments
-
-    union all
-
-    select
-        *
-        , 'xPro' as platform
-    from mitxpro_enrollments
 )
 
 select * from join_ol_enrollments
