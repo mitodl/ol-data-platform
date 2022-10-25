5355677cfc8d88146249429d6c1af5a4d1b5c20d
Anastasia Beglova <abeglova@mit.edu>
2022-10-25 09:38:55 -0400

    bootcamps user models

--- src/ol_dbt/models/intermediate/bootcamps/int__bootcamps__users.sql
+++ src/ol_dbt/models/intermediate/bootcamps/int__bootcamps__users.sql
@@ -1,9 +1,27 @@
 with users as (
-    select
-        user_id
-        , user_username
-        , user_email
+    select *
     from {{ ref('stg__bootcamps__app__postgres__auth_user') }}
 )
 
-select * from users
+, users_legaladdress as (
+    select *
+    from {{ ref('stg__bootcamps__app__postgres__profiles_legaladdress') }}
+)
+
+, users_profile as (
+    select *
+    from {{ ref('stg__bootcamps__app__postgres__profiles_profile') }}
+)
+
+select
+    users.user_id
+    , users.user_username
+    , users_profile.user_full_name
+    , users.user_email
+    , users.user_joined_on
+    , users.user_last_login
+    , users_legaladdress.user_address_country
+from users
+left join users_legaladdress on users_legaladdress.user_id = users.user_id
+left join users_profile on users_profile.user_id = users.user_id
+where users.user_is_active = true
