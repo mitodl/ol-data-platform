50eb2599e6aab20d2085a4575590d0d752b9dde0
rachellougee <rlougee@mit.edu>
2022-10-27 11:28:08 -0400

    created intermediate tables for courses from mitxonline

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__course_runs.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__course_runs.sql
@@ -1,12 +1,22 @@
+-- Course Runs information for MITx Online
+
 with runs as (
-    select
-        courserun_id
-        , courserun_title
-        , courserun_end_on
-        , courserun_start_on
-        , courserun_readable_id
-        , courserun_url
+    select *
     from {{ ref('stg__mitxonline__app__postgres__courses_courserun') }}
 )
 
-select * from runs
+select
+    courserun_id
+    , course_id
+    , courserun_title
+    , courserun_readable_id
+    , courserun_tag
+    , courserun_url
+    , courserun_start_on
+    , courserun_end_on
+    , courserun_enrollment_start_on
+    , courserun_enrollment_end_on
+    , courserun_upgrade_deadline
+    , courserun_is_self_paced
+    , courserun_is_live
+from runs
---
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__course_to_topics.sql
@@ -0,0 +1,18 @@
+-- Course's Blocked countries information for MITx Online
+-- Keep it as separate model for flexibility to satisfy different use cases
+
+with topics as (
+    select *
+    from {{ ref('stg__mitxonline__app__postgres__courses_coursetopic') }}
+)
+
+, course_to_topics as (
+    select *
+    from {{ ref('stg__mitxonline__app__postgres__courses_course_to_topic') }}
+)
+
+select
+    course_to_topics.course_id
+    , topics.coursetopic_name
+from course_to_topics
+inner join topics on course_to_topics.coursetopic_id = topics.coursetopic_id
