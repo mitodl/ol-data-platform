84c96ef67ab6502e91fa87ba0ca5b80ef12121c4
Anastasia Beglova <abeglova@mit.edu>
2022-11-04 15:02:23 -0400

    mitxonline enrollments

---
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_courserunenrollment.sql
@@ -0,0 +1,22 @@
+-- MITx Online Course Run Enrollment Information
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__courses_courserunenrollment') }}
+)
+
+, cleaned as (
+    select
+        id as courserunenrollment_id
+        , change_status as courserunenrollment_enrollment_status
+        , active as courserunenrollment_is_active
+        , edx_enrolled as courserunenrollment_is_edx_enrolled
+        , run_id as courserun_id
+        , user_id
+        , enrollment_mode as courserunenrollment_enrollment_mode
+        , edx_emails_subscription as courserunenrollment_has_edx_email_subscription
+        , to_iso8601(from_iso8601_timestamp(created_on)) as courserunenrollment_created_on
+        , to_iso8601(from_iso8601_timestamp(updated_on)) as courserunenrollment_updated_on
+    from source
+)
+
+select * from cleaned
