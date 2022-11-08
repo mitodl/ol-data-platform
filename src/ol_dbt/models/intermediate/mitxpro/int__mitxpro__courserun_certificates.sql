550bff61a8fc943d61bd4c1fdbfc259dea1102b2
rachellougee <rlougee@mit.edu>
2022-11-08 16:00:32 -0500

    staging and intermediate models for MIT xPro certificate and grade

---
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__courserun_certificates.sql
@@ -0,0 +1,29 @@
+-- Course Certificate information for MIT xPro
+
+with certificates as (
+    select * from {{ ref('stg__mitxpro__app__postgres__courses_courseruncertificate') }}
+)
+
+, runs as (
+    select * from {{ ref('stg__mitxpro__app__postgres__courses_courserun') }}
+)
+
+, users as (
+    select * from {{ ref('stg__mitxpro__app__postgres__users_user') }}
+)
+
+select
+    certificates.courseruncertificate_id
+    , certificates.courseruncertificate_uuid
+    , certificates.courserun_id
+    , runs.courserun_title
+    , runs.courserun_readable_id
+    , runs.courserun_url
+    , runs.course_id
+    , certificates.courseruncertificate_is_revoked
+    , certificates.user_id
+    , users.user_username
+    , users.user_email
+from certificates
+inner join runs on certificates.courserun_id = runs.courserun_id
+inner join users on certificates.user_id = users.user_id
