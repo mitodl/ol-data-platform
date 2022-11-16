7264d32eb4499ef9d6cf10e544885206b64a1226
rachellougee <rlougee@mit.edu>
2022-11-16 16:53:20 -0500

    added MicroMasters Users models

---
+++ src/ol_dbt/models/intermediate/micromasters/int__micromasters__users.sql
@@ -0,0 +1,40 @@
+with users as (
+    select * from {{ ref('stg__micromasters__app__postgres__auth_user') }}
+)
+
+, profiles as (
+    select * from {{ ref('stg__micromasters__app__postgres__profiles_profile') }}
+)
+
+, mitxonline_auth as (
+    select
+        user_id
+        , user_username as user_mitxonline_username
+    from {{ ref('stg__micromasters__app__postgres__auth_usersocialauth') }}
+    where user_auth_provider = 'mitxonline'
+)
+
+, edxorg_auth as (
+    select
+        user_id
+        , user_username as user_edxorg_username
+    from {{ ref('stg__micromasters__app__postgres__auth_usersocialauth') }}
+    where user_auth_provider = 'edxorg'
+)
+
+
+select
+    users.user_id
+    , users.user_username
+    , users.user_email
+    , users.user_joined_on
+    , users.user_last_login
+    , users.user_is_active
+    , profiles.user_full_name
+    , profiles.user_address_country
+    , mitxonline_auth.user_mitxonline_username
+    , edxorg_auth.user_edxorg_username
+from users
+left join profiles on profiles.user_id = users.user_id
+left join mitxonline_auth on mitxonline_auth.user_id = users.user_id
+left join edxorg_auth on edxorg_auth.user_id = users.user_id
--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__users.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__users.sql
@@ -8,6 +8,26 @@ with users as (
     from {{ ref('stg__mitxonline__app__postgres__users_legaladdress') }}
 )
 
+, micromasters_profiles as (
+    select * from {{ ref('stg__micromasters__app__postgres__profiles_profile') }}
+)
+
+, micromasters_auth as (
+    select *
+    from {{ ref('stg__micromasters__app__postgres__auth_usersocialauth') }}
+    where user_auth_provider = 'mitxonline'
+)
+
+--- this profile is pull from MicroMasters so that we can match MM profile with MITxOnline users based on username
+, users_profiles as (
+    select
+        micromasters_profiles.user_profile_id as user_micromasters_profile_id
+        , micromasters_auth.user_username
+        , micromasters_profiles.user_address_country
+    from micromasters_profiles
+    inner join micromasters_auth on micromasters_auth.user_id = micromasters_profiles.user_id
+)
+
 select
     users.user_id
     , users.user_username
@@ -17,5 +37,7 @@ select
     , users.user_last_login
     , users_legaladdress.user_address_country
     , users.user_is_active
+    , users_profiles.user_micromasters_profile_id
 from users
 left join users_legaladdress on users_legaladdress.user_id = users.user_id
+left join users_profiles on users_profiles.user_username = users.user_username
