a9a155270cf883e673893ed7bf90d595eacc7e17
Anastasia Beglova <abeglova@mit.edu>
2022-12-12 15:59:23 -0500

    mitxonline intermediate refactor

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__ecommerce_purchase.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__ecommerce_purchase.sql
@@ -14,19 +14,39 @@ with lines as (
     )
 )
 
+, orders as (
+    select *
+    from {{ ref('stg__mitxonline__app__postgres__ecommerce_order') }}
+)
+
+, users as (
+    select *
+    from {{ ref('stg__mitxonline__app__postgres__users_user') }}
+)
+
 , intermediate_products_view as (
     select * from {{ ref('int__mitxonline__ecommerce_product') }}
 )
 
 select
-    lines.line_id
-    , lines.order_id
-    , lines.line_created_on
+    orders.order_id
+    , orders.order_state
+    , orders.order_created_on
+    , orders.order_purchaser_user_id
+    , orders.order_reference_number
+    , orders.order_total_price_paid
+    , users.user_id
+    , users.user_username
+    , users.user_full_name
+    , users.user_email
+    , lines.line_id
     , lines.product_version_id
     , intermediate_products_view.product_type
     , intermediate_products_view.product_id
     , intermediate_products_view.courserun_id
     , intermediate_products_view.programrun_id
 from lines
+inner join orders on orders.order_id = lines.order_id
+inner join users on orders.order_purchaser_user_id = users.user_id
 inner join versions on versions.version_id = lines.product_version_id
 inner join intermediate_products_view on intermediate_products_view.product_id = versions.version_object_id
