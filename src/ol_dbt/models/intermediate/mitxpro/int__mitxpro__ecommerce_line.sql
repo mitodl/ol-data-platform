5d498d10a4a28d1b3de3879c7ed1c59eb3d2d58d
Anastasia Beglova <abeglova@mit.edu>
2022-12-20 09:53:38 -0500

    mitxpro ecommerce lines and orders

---
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__ecommerce_line.sql
@@ -0,0 +1,35 @@
+with lines as (
+    select *
+    from {{ ref('stg__mitxpro__app__postgres__ecommerce_line') }}
+)
+
+, productversions as (
+    select *
+    from {{ ref('stg__mitxpro__app__postgres__ecommerce_productversion') }}
+)
+
+, products as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_product') }}
+)
+
+, programrunlines as (
+    select *
+    from {{ ref('stg__mitxpro__app__postgres__ecommerce_programrunline') }}
+)
+
+select
+    lines.line_id
+    , lines.order_id
+    , lines.productversion_id
+    , lines.line_created_on
+    , lines.line_updated_on
+    , products.product_id
+    , products.courserun_id
+    , products.program_id
+    , products.product_type
+    , programrunlines.programrun_id
+from lines
+inner join productversions on productversions.productversion_id = lines.productversion_id
+inner join products on products.product_id = productversions.product_id
+left join programrunlines on programrunlines.line_id = lines.line_id
