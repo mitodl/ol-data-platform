86a4f7eda29c5db5e613cf3d136787b7e6a19aa9
rachellougee <rlougee@mit.edu>
2023-01-13 10:03:22 -0500

    Add email_opt_in and modify intermediate users table to use email_opt_in for username and email

--- src/ol_dbt/models/intermediate/edxorg/int__edxorg__mitx_users.sql
+++ src/ol_dbt/models/intermediate/edxorg/int__edxorg__mitx_users.sql
@@ -1,9 +1,52 @@
---- Intermediate MITx users for edx.org
---- user_info_combo contains user info for each course they enroll in, so need to aggregate it here
---  to get the latest user info per user_id
+---Intermediate MITx users for edx.org
+---user data is mostly in user_info_combo but email or username could be blank in user_info_combo in some older courses,
+---to get the most recent user data, we combine user_info_combo and email_opt_in here to always use username
+-- and email from email_opt_in and the rest profile fields from user_info_combo
+-- Both tables are unique on user_id + course_id
 
 with user_info_combo as (
-    --- use window function to generate row number based on user_last_login for each user
+    select * from {{ ref('stg__edxorg__bigquery__mitx_user_info_combo') }}
+)
+
+--- email_opt_in is one row per course_id with the same user_* data, use window function to pick one per user_id
+, email_opt_in as (
+    select
+        user_id
+        , user_email
+        , user_username
+        , user_full_name
+        , row_number() over (partition by user_id order by user_email_opt_in_updated_on desc) as row_num
+    from {{ ref('stg__edxorg__bigquery__mitx_user_email_opt_in') }}
+)
+
+, most_recent_user_info as (
+    select
+        user_id
+        , user_email
+        , user_username
+        , user_full_name
+    from email_opt_in
+    where row_num = 1
+)
+
+, combined_user_info as (
+    select
+        most_recent_user_info.user_id
+        , most_recent_user_info.user_email
+        , most_recent_user_info.user_username
+        , user_info_combo.user_full_name
+        , user_info_combo.user_country
+        , user_info_combo.user_joined_on
+        , row_number() over (partition by user_info_combo.user_id order by user_info_combo.user_last_login desc
+        ) as row_num
+    from user_info_combo
+    inner join most_recent_user_info
+        on
+            user_info_combo.user_id = most_recent_user_info.user_id
+            and user_info_combo.user_username = most_recent_user_info.user_username
+)
+
+, users as (
     select
         user_id
         , user_email
@@ -11,16 +54,8 @@ with user_info_combo as (
         , user_full_name
         , user_country
         , user_joined_on
-        , row_number() over (partition by user_id order by user_last_login desc) as seq
-    from {{ ref('stg__edxorg__bigquery__mitx_user_info_combo') }}
+    from combined_user_info
+    where row_num = 1
 )
 
-select
-    user_id
-    , user_email
-    , user_username
-    , user_full_name
-    , user_country
-    , user_joined_on
-from user_info_combo
-where seq = 1   -- use the latest row for each user
+select * from users
