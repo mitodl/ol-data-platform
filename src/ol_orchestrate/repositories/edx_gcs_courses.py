3ccf5c20c8b925fc760c0968acdfa8b309c73023
sar <sar.haidar@gmail.com>
2023-01-17 17:57:56 -0600

    Fixing edx_gcs_courses pipeline

---
+++ src/ol_orchestrate/repositories/edx_gcs_courses.py
@@ -0,0 +1,40 @@
+import os
+
+from dagster import repository
+from dagster_aws.s3.resources import s3_resource
+
+from ol_orchestrate.jobs.edx_gcs_courses import (
+    sync_gcs_to_s3,
+)
+from ol_orchestrate.resources.gcp_gcs import gcp_gcs_resource
+from ol_orchestrate.resources.healthchecks import (
+    healthchecks_dummy_resource,
+    healthchecks_io_resource,
+)
+from ol_orchestrate.resources.outputs import daily_dir
+
+environment = {
+    "qa": {
+        "gcp_gcs": gcp_gcs_resource,
+        "s3": s3_resource,
+        "results_dir": daily_dir,
+        "healthchecks": healthchecks_dummy_resource,
+    },
+    "production": {
+        "gcp_gcs": gcp_gcs_resource,
+        "s3": s3_resource,
+        "results_dir": daily_dir,
+        "healthchecks": healthchecks_io_resource,
+    },
+}
+
+dagster_deployment = os.getenv("DAGSTER_ENVIRONMENT", "qa")
+env_map = environment[dagster_deployment]
+
+@repository
+def edx_gcs_courses_repository():
+    return [
+        sync_gcs_to_s3.to_job(
+            resource_defs=env_map
+        ),
+    ]
