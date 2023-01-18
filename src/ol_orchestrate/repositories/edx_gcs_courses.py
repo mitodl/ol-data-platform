622efe040665345868de8787928142e654b2b827
sar <sar.haidar@gmail.com>
2023-01-18 13:31:13 -0600

    Remove healthchecks from edx_gcs_courses

--- src/ol_orchestrate/repositories/edx_gcs_courses.py
+++ src/ol_orchestrate/repositories/edx_gcs_courses.py
@@ -5,10 +5,6 @@ from dagster_aws.s3.resources import s3_resource
 
 from ol_orchestrate.jobs.edx_gcs_courses import sync_gcs_to_s3
 from ol_orchestrate.resources.gcp_gcs import gcp_gcs_resource
-from ol_orchestrate.resources.healthchecks import (
-    healthchecks_dummy_resource,
-    healthchecks_io_resource,
-)
 from ol_orchestrate.resources.outputs import daily_dir
 
 environment = {
@@ -16,13 +12,11 @@ environment = {
         "gcp_gcs": gcp_gcs_resource,
         "s3": s3_resource,
         "results_dir": daily_dir,
-        "healthchecks": healthchecks_dummy_resource,
     },
     "production": {
         "gcp_gcs": gcp_gcs_resource,
         "s3": s3_resource,
         "results_dir": daily_dir,
-        "healthchecks": healthchecks_io_resource,
     },
 }
 
