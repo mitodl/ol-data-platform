4b335a1914f9d79743d8b910bc165f3c5e1e729f
Tobias Macey <tmacey@mit.edu>
2023-01-20 16:11:59 -0500

    feat: Attach GCS sensor to edX.org course bundles sync pipeline

--- src/ol_orchestrate/repositories/edx_gcs_courses.py
+++ src/ol_orchestrate/repositories/edx_gcs_courses.py
@@ -1,11 +1,12 @@
 import os
 
-from dagster import repository
+from dagster import Definitions, SensorDefinition
 from dagster_aws.s3.resources import s3_resource
 
 from ol_orchestrate.jobs.edx_gcs_courses import sync_gcs_to_s3
 from ol_orchestrate.resources.gcp_gcs import gcp_gcs_resource
 from ol_orchestrate.resources.outputs import daily_dir
+from ol_orchestrate.sensors.sync_gcs_to_s3 import check_new_gcs_assets_sensor
 
 environment = {
     "qa": {
@@ -23,9 +24,17 @@ environment = {
 dagster_deployment = os.getenv("DAGSTER_ENVIRONMENT", "qa")
 env_map = environment[dagster_deployment]
 
+gcs_sync_job = sync_gcs_to_s3.to_job(
+    name="edx_gcs_course_retrieval", resource_defs=env_map
+)
 
-@repository
-def edx_gcs_courses_repository():
-    return [
-        sync_gcs_to_s3.to_job(resource_defs=env_map),
-    ]
+edx_gcs_courses = Definitions(
+    sensors=[
+        SensorDefinition(
+            evaluation_fn=check_new_gcs_assets_sensor,
+            minimum_interval_seconds=86400,  # noqa: WPS432 - 1 day
+            job=gcs_sync_job,
+        )
+    ],
+    jobs=[gcs_sync_job],
+)
--- src/ol_orchestrate/sensors/sync_gcs_to_s3.py
+++ src/ol_orchestrate/sensors/sync_gcs_to_s3.py
@@ -1,9 +1,6 @@
-from dagster import RunRequest, SkipReason, sensor
-
-from ol_orchestrate.jobs.edx_gcs_courses import sync_gcs_to_s3
+from dagster import RunRequest, SkipReason
 
 
-@sensor(job=sync_gcs_to_s3, minimum_interval_seconds=86400)  # noqa: WPS432
 def check_new_gcs_assets_sensor(context):
     storage_client = context.resources.gcp_gcs.gcp_gcs_resource
     bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
