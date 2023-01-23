440a83009e5ed5aa7f4d3e8e83b2fef516c59bb8
Tobias Macey <tmacey@mit.edu>
2023-01-23 12:40:12 -0500

    fix: Handle resource initialization for GCS pipeline sensor

--- src/ol_orchestrate/sensors/sync_gcs_to_s3.py
+++ src/ol_orchestrate/sensors/sync_gcs_to_s3.py
@@ -1,14 +1,18 @@
-from dagster import RunRequest, SkipReason
+from dagster import RunRequest, SensorEvaluationContext, SkipReason, build_resources
+
+from ol_orchestrate.resources import gcp_gcs
 
 
-def check_new_gcs_assets_sensor(context):
-    storage_client = context.resources.gcp_gcs.gcp_gcs_resource
-    bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
-    new_files = storage_client.list_blobs(bucket)
-    if new_files:
-        yield RunRequest(
-            run_key="new_gcs_file",
-        )
-    else:
-        yield SkipReason("No new files in GCS bucket")
-        return
+def check_new_gcs_assets_sensor(context: SensorEvaluationContext):
+    with build_resources({"gcp_gcs": gcp_gcs}) as resources:
+        storage_client = resources.gcp_gcs
+        bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
+        new_files = storage_client.list_blobs(bucket)
+        if new_files:
+            context.update_cursor(str(new_files))
+            yield RunRequest(
+                run_key="new_gcs_file",
+            )
+        else:
+            yield SkipReason("No new files in GCS bucket")
+            return
