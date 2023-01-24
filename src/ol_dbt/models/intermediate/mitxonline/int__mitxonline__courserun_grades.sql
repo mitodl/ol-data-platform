410515359469f0174a8a983c50f27115c1879c74
rachellougee <rlougee@mit.edu>
2023-01-24 12:18:05 -0500

    intermediate models updates

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__courserun_grades.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__courserun_grades.sql
@@ -12,19 +12,25 @@ with grades as (
     select * from {{ ref('stg__mitxonline__app__postgres__users_user') }}
 )
 
-select
-    grades.courserungrade_id
-    , grades.courserun_id
-    , runs.course_id
-    , runs.courserun_title
-    , runs.courserun_readable_id
-    , runs.courserun_url
-    , grades.courserungrade_grade
-    , grades.courserungrade_letter_grade
-    , grades.courserungrade_is_passing
-    , grades.user_id
-    , users.user_username
-    , users.user_email
-from grades
-inner join runs on grades.courserun_id = runs.courserun_id
-inner join users on grades.user_id = users.user_id
+, courserun_grades as (
+    select
+        grades.courserungrade_id
+        , grades.courserun_id
+        , runs.course_id
+        , runs.courserun_title
+        , runs.courserun_readable_id
+        , runs.courserun_url
+        , grades.courserungrade_grade
+        , grades.courserungrade_letter_grade
+        , grades.courserungrade_is_passing
+        , grades.courserungrade_created_on
+        , grades.courserungrade_updated_on
+        , grades.user_id
+        , users.user_username
+        , users.user_email
+    from grades
+    inner join runs on grades.courserun_id = runs.courserun_id
+    inner join users on grades.user_id = users.user_id
+)
+
+select * from courserun_grades
