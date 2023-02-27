8c8cfe8d1efdefaf55c3b3e2dc220bf97d872e7d
Anastasia Beglova <abeglova@mit.edu>
2023-02-27 11:55:31 -0500

    micromasters edx usernames

--- src/ol_dbt/models/intermediate/micromasters/int__micromasters__users.sql
+++ src/ol_dbt/models/intermediate/micromasters/int__micromasters__users.sql
@@ -22,6 +22,31 @@ with users as (
     where user_auth_provider = 'edxorg'
 )
 
+, edxorg_users as (
+    select
+        user_username as user_edxorg_username
+        , user_last_login
+    from {{ ref('int__edxorg__mitx_users') }}
+)
+
+, most_recent_edx_username as (
+    select
+        user_id
+        , user_edxorg_username
+    from (
+        select
+            edxorg_auth.user_id
+            , edxorg_auth.user_edxorg_username
+            , row_number() over (
+                partition by edxorg_auth.user_id
+                order by edxorg_users.user_last_login desc
+            ) as row_num
+        from edxorg_auth
+        left join edxorg_users on edxorg_auth.user_edxorg_username = edxorg_users.user_edxorg_username
+    )
+    where row_num = 1
+)
+
 
 select
     users.user_id
@@ -33,8 +58,8 @@ select
     , profiles.user_full_name
     , profiles.user_address_country
     , mitxonline_auth.user_mitxonline_username
-    , edxorg_auth.user_edxorg_username
+    , most_recent_edx_username.user_edxorg_username
 from users
 left join profiles on profiles.user_id = users.user_id
 left join mitxonline_auth on mitxonline_auth.user_id = users.user_id
-left join edxorg_auth on edxorg_auth.user_id = users.user_id
+left join most_recent_edx_username on most_recent_edx_username.user_id = users.user_id
