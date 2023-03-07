394b7880f4adfff04aa3368b53c5e7e720f63a3f
Tobias Macey <tmacey@mit.edu>
2023-03-07 13:23:35 -0500

    fix: Properly initialize GCS sensor resource config

--- src/ol_orchestrate/repositories/edx_gcs_courses.py
+++ src/ol_orchestrate/repositories/edx_gcs_courses.py
@@ -4,28 +4,23 @@ from dagster import DefaultSensorStatus, Definitions, SensorDefinition
 from dagster_aws.s3.resources import s3_resource
 
 from ol_orchestrate.jobs.edx_gcs_courses import sync_gcs_to_s3
+from ol_orchestrate.lib.yaml_config_helper import load_yaml_config
 from ol_orchestrate.resources.gcp_gcs import gcp_gcs_resource
 from ol_orchestrate.resources.outputs import daily_dir
 from ol_orchestrate.sensors.sync_gcs_to_s3 import check_new_gcs_assets_sensor
 
-environment = {
-    "qa": {
-        "gcp_gcs": gcp_gcs_resource,
-        "s3": s3_resource,
-        "results_dir": daily_dir,
-    },
-    "production": {
-        "gcp_gcs": gcp_gcs_resource,
-        "s3": s3_resource,
-        "results_dir": daily_dir,
-    },
+resources = {
+    "gcp_gcs": gcp_gcs_resource.configured(
+        load_yaml_config("/etc/dagster/edxorg_gcp.yaml")
+    ),
+    "s3": s3_resource,
+    "results_dir": daily_dir,
 }
 
 dagster_deployment = os.getenv("DAGSTER_ENVIRONMENT", "qa")
-env_map = environment[dagster_deployment]
 
 gcs_sync_job = sync_gcs_to_s3.to_job(
-    name="edx_gcs_course_retrieval", resource_defs=env_map
+    name="edx_gcs_course_retrieval", resource_defs=resources
 )
 
 edx_gcs_courses = Definitions(
--- src/ol_orchestrate/sensors/sync_gcs_to_s3.py
+++ src/ol_orchestrate/sensors/sync_gcs_to_s3.py
@@ -5,7 +5,10 @@ from ol_orchestrate.lib.yaml_config_helper import load_yaml_config
 
 
 def check_new_gcs_assets_sensor(context: SensorEvaluationContext):
-    with build_resources({"gcp_gcs": gcp_gcs_resource}) as resources:
+    gcs_config = load_yaml_config("/etc/dagster/edxorg_gcp.yaml")
+    with build_resources(
+        resources={"gcp_gcs": gcp_gcs_resource}, resource_config=gcs_config["resources"]
+    ) as resources:
         storage_client = resources.gcp_gcs
         bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
         new_files = storage_client.list_blobs(bucket)
@@ -13,7 +16,7 @@ def check_new_gcs_assets_sensor(context: SensorEvaluationContext):
             context.update_cursor(str(new_files))
             yield RunRequest(
                 run_key="new_gcs_file",
-                run_config=load_yaml_config("/etc/dagster/edxorg_gcp.yaml"),
+                run_config=gcs_config,
             )
         else:
             yield SkipReason("No new files in GCS bucket")
