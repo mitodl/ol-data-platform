53521f821739ebf2bf6a21c5e7fe7bd548314ea4
Quazi <59845076+quazi-h@users.noreply.github.com>
2023-07-07 16:38:48 -0400

    737 Log Normalization Dagster Pipeline (#750)
    * Added a new Dagster pipeline to normalize historical tracking log data.
    Added new ops to:
    -handle getting S3 bucket prefixes and objects.
    -load S3 objects directly to a table in a DuckDB instance.
    -transform records in DuckDB with SQL updates.
    -write normalized files back to S3
    Added normalize_tracking_logs job to string together the new ops
    Started writing up the repository for the new pipeline.
    TODO: Fix eslint errors, properly import/configure a duckDB resource, finalize the config_schema for the new pipeline
    
    * Autoformat updates.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * refactor: String together ops and daily partition
    
    - Add a daily partition definition for being able to launch backfills
    - Wire together the ops and pass through the log date
    - Add definition for DuckDB resource
    
    * refactor: Remove DuckDB IO Manager in favor of persisted DB
    
    Just using the DuckDB Resource allows for passing the data between ops through the
    shared DB file. This refactors the ops for normalizing tracking logs to string together
    via out/in `Nothing` types. This also handles passing the IAM credentials for use in DuckDB
    
    * config: Add build definitions for packaging log processing pipeline
    
    The pipeline needs to be packaged in a Docker image. This adds the build metadata to be
    able to wire it into a container to be run in QA/Production
    
    * Delete normalize_logs.py
    
    * "Added outfile logic using duckdb sql queries
    Refactored transform op to alter and update all columns dynamically"
    
    * "Refactored outfile logic to get code working"
    
    * Create job definitions for normalizing logs in each deployment
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Tobias Macey <tmacey@mit.edu>

---
+++ src/ol_orchestrate/definitions/edx/normalize_tracking_logs.py
@@ -0,0 +1,92 @@
+from functools import partial
+from boto3 import Session
+from typing import Literal
+from dagster_duckdb import DuckDBResource
+from dagster_aws.s3.resources import s3_resource
+import os
+from dagster import (
+    Definitions,
+    daily_partitioned_config,
+)
+from datetime import datetime, UTC  # type: ignore
+
+from ol_orchestrate.jobs.normalize_logs import normalize_tracking_logs
+
+
+dagster_env: Literal["dev", "qa", "production"] = os.environ.get(  # type: ignore
+    "DAGSTER_ENVIRONMENT", "dev"
+)
+# deployment: Literal["mitx", "mitxonline", "xpro"] = os.environ.get(  # type: ignore
+#     "OPEN_EDX_DEPLOYMENT_NAME", "xpro")
+
+
+def earliest_log_date(
+    dagster_env: Literal["dev", "qa", "production"],
+    deployment_name: Literal["mitx", "mitxonline", "xpro"],
+) -> datetime:
+    if dagster_env == "dev":
+        dagster_env = "qa"
+    date_map = {
+        "qa": {
+            "mitx": datetime(2021, 3, 10, tzinfo=UTC),
+            "mitxonline": datetime(2021, 7, 26, tzinfo=UTC),
+            "xpro": datetime(2021, 3, 31, tzinfo=UTC),
+        },
+        "production": {
+            "mitx": datetime(2017, 6, 13, tzinfo=UTC),
+            "mitxonline": datetime(2021, 8, 18, tzinfo=UTC),
+            "xpro": datetime(2019, 8, 28, tzinfo=UTC),
+        },
+    }
+    return date_map[dagster_env][deployment_name]
+
+
+def daily_tracking_log_config(deployment, log_date: datetime, _end: datetime):
+    global dagster_env
+    if dagster_env == "dev":
+        dagster_env = "qa"
+    log_bucket = f"{deployment}-{dagster_env}-edxapp-tracking"
+    session = Session()
+    credentials = session.get_credentials()
+    current_credentials = credentials.get_frozen_credentials()
+    return {
+        "ops": {
+            "load_s3_files_to_duckdb": {
+                "config": {
+                    "tracking_log_bucket": log_bucket,
+                    "s3_key": current_credentials.access_key,
+                    "s3_secret": current_credentials.secret_key,
+                },
+                "inputs": {
+                    "log_date": f"{log_date.strftime('%Y-%m-%d')}/",
+                },
+            },
+            "export_processed_data_to_s3": {
+                "config": {
+                    "tracking_log_bucket": log_bucket,
+                    "s3_key": current_credentials.access_key,
+                    "s3_secret": current_credentials.secret_key,
+                },
+                "inputs": {
+                    "log_date": f"{log_date.strftime('%Y-%m-%d')}/",
+                },
+            },
+        }
+    }
+
+
+normalize_logs = Definitions(
+    resources={
+        "duckdb": DuckDBResource(database="tracking_logs.duckdb"),
+        "s3": s3_resource,
+    },
+    jobs=[
+        normalize_tracking_logs.to_job(
+            config=daily_partitioned_config(
+                start_date=earliest_log_date(dagster_env, deployment)  # type: ignore
+            )(partial(daily_tracking_log_config, deployment)),
+            name=f"normalize_{deployment}_{dagster_env}_tracking_logs",
+        )
+        for deployment in ["xpro", "mitx", "mitxonline"]
+    ],
+)
