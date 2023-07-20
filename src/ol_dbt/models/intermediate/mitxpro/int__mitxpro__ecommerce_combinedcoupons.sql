c77c00a45ce1f7a17474e6b8a8f1331bd747ccbf
Katelyn Delaney <katelyn@mit.edu>
2023-07-20 08:10:46 -0400

    test

--- src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__ecommerce_combinedcoupons.sql
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__ecommerce_combinedcoupons.sql
@@ -69,15 +69,24 @@ with coupon as (
 )
 
 , payment_transactions as (
+<<<<<<< Updated upstream
     select distinct
         couponversion.coupon_id
         , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
     from (
         select
+=======
+    select distinct 
+        couponversion.coupon_id
+        , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
+    from (
+        select 
+>>>>>>> Stashed changes
             couponpayment_name
             , max(couponpaymentversion_id) as couponpaymentversion_id
         from int__mitxpro__ecommerce_couponpaymentversion
         group by couponpayment_name
+<<<<<<< Updated upstream
     ) as couponpaymentversion
     inner join ecommerce_couponpaymentversion
         on couponpaymentversion.couponpaymentversion_id = ecommerce_couponpaymentversion.couponpaymentversion_id
@@ -86,13 +95,27 @@ with coupon as (
 )
 
 select
+=======
+        ) as couponpaymentversion
+    join  ecommerce_couponpaymentversion as ecommerce_couponpaymentversion
+        on couponpaymentversion.couponpaymentversion_id = ecommerce_couponpaymentversion.couponpaymentversion_id
+    join couponversion as couponversion
+        on couponpaymentversion.couponpaymentversion_id = couponversion.couponpaymentversion_id
+)
+
+select 
+>>>>>>> Stashed changes
     productcouponassignment.productcouponassignment_email as coupon_email
     , coupon.coupon_code
     , coupon.coupon_created_on
     , coupon.couponpayment_name
     , b2b_ecommerce_b2border.b2border_contract_number
     , productcouponassignment.product_id
+<<<<<<< Updated upstream
     , payment_transactions.couponpaymentversion_payment_transaction
+=======
+    , payment_transactions.couponpaymentversion_payment_transaction 
+>>>>>>> Stashed changes
     , null as req_reference_number
     , program_runs.programrun_readable_id
     , program_runs.program_title
@@ -102,7 +125,11 @@ select
     , 'regular coupon' as combinedcoupon_table_source
     , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
     , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+<<<<<<< Updated upstream
     , coalesce (count(couponredemption.couponredemption_id) >= 1, false) as coupon_redeemed
+=======
+    , case when (count(couponredemption.couponredemption_id) >= 1) then 'True' else 'False' end as coupon_redeemed
+>>>>>>> Stashed changes
     , array_join(array_distinct(array_agg(users_user.user_email)), ', ') as combined_user_emails
     , array_join(array_distinct(array_agg(users_user.user_address_country)), ', ') as user_address_countries
     , array_join(array_distinct(array_agg(ecommerce_order.order_id)), ', ') as order_ids
@@ -110,21 +137,37 @@ select
 from coupon
 left join productcouponassignment
     on productcouponassignment.coupon_id = coupon.coupon_id
+<<<<<<< Updated upstream
 left join ecommerce_couponversion
     on coupon.coupon_id = ecommerce_couponversion.couponversion_id
 left join ecommerce_couponpaymentversion
     on ecommerce_couponpaymentversion.couponpaymentversion_id = ecommerce_couponversion.couponpaymentversion_id
 left join b2b_ecommerce_b2border
+=======
+left join ecommerce_couponversion 
+    on coupon.coupon_id = ecommerce_couponversion.couponversion_id
+left join ecommerce_couponpaymentversion 
+    on ecommerce_couponpaymentversion.couponpaymentversion_id = ecommerce_couponversion.couponpaymentversion_id
+left join b2b_ecommerce_b2border 
+>>>>>>> Stashed changes
     on ecommerce_couponpaymentversion.couponpaymentversion_id = b2b_ecommerce_b2border.couponpaymentversion_id
 left join couponredemption
     on ecommerce_couponversion.couponversion_id = couponredemption.couponversion_id
 left join ecommerce_order
     on ecommerce_order.order_id = couponredemption.order_id
+<<<<<<< Updated upstream
 left join users_user
     on ecommerce_order.order_purchaser_user_id = users_user.user_id
 left join payment_transactions
     on coupon.coupon_id = payment_transactions.coupon_id
 left join ecommerce_line
+=======
+left join users_user    
+    on ecommerce_order.order_purchaser_user_id = users_user.user_id
+left join payment_transactions
+    on coupon.coupon_id = payment_transactions.coupon_id
+left join ecommerce_line 
+>>>>>>> Stashed changes
     on ecommerce_line.order_id = ecommerce_order.order_id
 left join program_runs
     on ecommerce_line.programrun_id = program_runs.programrun_id
@@ -134,38 +177,63 @@ left join courses
     on course_runs.course_id = courses.course_id
 left join productversion
     on productversion.productversion_id = ecommerce_line.productversion_id
+<<<<<<< Updated upstream
 group by
     productcouponassignment.productcouponassignment_email
+=======
+group by 
+    productcouponassignment.productcouponassignment_email 
+>>>>>>> Stashed changes
     , coupon.coupon_code
     , coupon.coupon_created_on
     , coupon.couponpayment_name
     , b2b_ecommerce_b2border.b2border_contract_number
     , productcouponassignment.product_id
+<<<<<<< Updated upstream
     , payment_transactions.couponpaymentversion_payment_transaction
     , null
+=======
+    , payment_transactions.couponpaymentversion_payment_transaction 
+    , null 
+>>>>>>> Stashed changes
     , program_runs.programrun_readable_id
     , program_runs.program_title
     , course_runs.courserun_readable_id
     , course_runs.course_title
     , productversion.productversion_readable_id
+<<<<<<< Updated upstream
     , 'regular coupon'
+=======
+    , 'regular coupon' 
+>>>>>>> Stashed changes
     , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
     , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
 
 union distinct
 
+<<<<<<< Updated upstream
 select
+=======
+select 
+>>>>>>> Stashed changes
     b2b_ecommerce_b2border.b2border_email as coupon_email
     , coupon.coupon_code
     , coupon.coupon_created_on
     , coupon.couponpayment_name
     , b2b_ecommerce_b2border.b2border_contract_number
     , b2b_ecommerce_b2border.product_id
+<<<<<<< Updated upstream
     , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
     , replace(cast(
         json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number')
         as varchar
     ), '"', '') as req_reference_number
+=======
+    , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction 
+    , replace(cast(json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number')
+        as varchar), '"', ''
+    ) as req_reference_number
+>>>>>>> Stashed changes
     , program_runs.programrun_readable_id
     , program_runs.program_title
     , course_runs.courserun_readable_id
@@ -174,11 +242,16 @@ select
     , 'b2b coupon' as combinedcoupon_table_source
     , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
     , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+<<<<<<< Updated upstream
     , coalesce (count(ecommerce_couponredemption.couponredemption_id) >= 1, false) as coupon_redeemed
+=======
+    , case when (count(ecommerce_couponredemption.couponredemption_id) >= 1) then 'True' else 'False' end as coupon_redeemed
+>>>>>>> Stashed changes
     , array_join(array_distinct(array_agg(users_user.user_email)), ', ') as combined_user_emails
     , array_join(array_distinct(array_agg(users_user.user_address_country)), ', ') as user_address_countries
     , array_join(array_distinct(array_agg(ecommerce_order.order_id)), ', ') as order_ids
     , array_join(array_distinct(array_agg(ecommerce_order.order_state)), ', ') as combined_order_state
+<<<<<<< Updated upstream
 from b2b_ecommerce_b2border
 inner join ecommerce_couponpaymentversion
     on ecommerce_couponpaymentversion.couponpaymentversion_id = b2b_ecommerce_b2border.couponpaymentversion_id
@@ -195,6 +268,24 @@ left join users_user
 left join b2b_ecommerce_b2breceipt
     on b2b_ecommerce_b2breceipt.b2border_id = b2b_ecommerce_b2border.b2border_id
 left join ecommerce_line
+=======
+from b2b_ecommerce_b2border as b2b_ecommerce_b2border
+join ecommerce_couponpaymentversion as ecommerce_couponpaymentversion
+    on ecommerce_couponpaymentversion.couponpaymentversion_id = b2b_ecommerce_b2border.couponpaymentversion_id
+join coupon as coupon
+    on coupon.coupon_id = b2b_ecommerce_b2border.b2bcoupon_id 
+join ecommerce_couponversion 
+    on ecommerce_couponversion.coupon_id = coupon.coupon_id
+left join ecommerce_couponredemption 
+    on ecommerce_couponredemption.couponversion_id = ecommerce_couponversion.couponversion_id
+left join ecommerce_order 
+    on ecommerce_order.order_id = ecommerce_couponredemption.order_id
+left join users_user    
+    on ecommerce_order.order_purchaser_user_id = users_user.user_id
+left join b2b_ecommerce_b2breceipt 
+    on b2b_ecommerce_b2breceipt.b2border_id = b2b_ecommerce_b2border.b2border_id
+left join ecommerce_line 
+>>>>>>> Stashed changes
     on ecommerce_line.order_id = ecommerce_order.order_id
 left join program_runs
     on ecommerce_line.programrun_id = program_runs.programrun_id
@@ -204,23 +295,42 @@ left join courses
     on course_runs.course_id = courses.course_id
 left join productversion
     on productversion.productversion_id = b2b_ecommerce_b2border.productversion_id
+<<<<<<< Updated upstream
 group by
     b2b_ecommerce_b2border.b2border_email
+=======
+group by 
+    b2b_ecommerce_b2border.b2border_email 
+>>>>>>> Stashed changes
     , coupon.coupon_code
     , coupon.coupon_created_on
     , coupon.couponpayment_name
     , b2b_ecommerce_b2border.b2border_contract_number
     , b2b_ecommerce_b2border.product_id
+<<<<<<< Updated upstream
     , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
     , replace(cast(
         json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number')
         as varchar
     ), '"', '')
+=======
+    , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction 
+    , replace(cast(
+        json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number')
+        as varchar), '"', ''
+    ) 
+>>>>>>> Stashed changes
     , program_runs.programrun_readable_id
     , program_runs.program_title
     , course_runs.courserun_readable_id
     , course_runs.course_title
     , productversion.productversion_readable_id
+<<<<<<< Updated upstream
     , 'b2b coupon'
     , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
     , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+=======
+    , 'b2b coupon' 
+    , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
+    , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+>>>>>>> Stashed changes
