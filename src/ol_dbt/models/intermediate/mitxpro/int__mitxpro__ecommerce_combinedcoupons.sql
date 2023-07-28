f8100e6f019cc6ff6c15563b569b5ec9136b1fab
Katelyn Delaney <katelyn@mit.edu>
2023-07-28 11:57:20 -0400

    update

--- src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__ecommerce_combinedcoupons.sql
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__ecommerce_combinedcoupons.sql
@@ -69,45 +69,19 @@ with coupon as (
 )
 
 , payment_transactions as (
-    select distinct
+    select distinct 
         ecommerce_couponversion.coupon_id
         , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
     from ecommerce_couponpaymentversion
-    inner join ecommerce_couponversion
+    inner join ecommerce_couponversion 
         on ecommerce_couponpaymentversion.couponpaymentversion_id = ecommerce_couponversion.couponpaymentversion_id
     where ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction is not null
 )
 
 , reg_coupon_summary as (
-    select
+    select 
         coupon.coupon_id
-        , case when (count(couponredemption.couponredemption_id) >= 1) then 'true' else 'false' end as coupon_redeemed
-        , array_join(array_distinct(array_agg(users_user.user_email)), ', ') as combined_user_emails
-        , array_join(array_distinct(array_agg(users_user.user_address_country)), ', ') as user_address_countries
-        , array_join(array_distinct(array_agg(ecommerce_order.order_id)), ', ') as order_ids
-        , array_join(array_distinct(array_agg(ecommerce_order.order_state)), ', ') as combined_order_state
-    from coupon
-    left join ecommerce_couponversion
-        on coupon.coupon_id = ecommerce_couponversion.coupon_id
-    left join couponredemption
-        on ecommerce_couponversion.couponversion_id = couponredemption.couponversion_id
-    left join ecommerce_order
-        on ecommerce_order.order_id = couponredemption.order_id
-    left join users_user
-        on ecommerce_order.order_purchaser_user_id = users_user.user_id
-    group by
-        coupon.coupon_id
-)
-
-, reg_coupon_fields as (
-    select
-        productcouponassignment.productcouponassignment_email as coupon_email
-        , coupon.coupon_code
-        , coupon.coupon_id
-        , coupon.coupon_created_on
-        , coupon.couponpayment_name
-        , productcouponassignment.product_id
-        , payment_transactions.couponpaymentversion_payment_transaction
+        , ecommerce_couponversion.couponversion_updated_on
         , program_runs.programrun_readable_id
         , program_runs.program_title
         , course_runs.courserun_readable_id
@@ -115,20 +89,21 @@ with coupon as (
         , productversion.productversion_readable_id
         , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
         , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+        , case when (count(couponredemption.couponredemption_id) >= 1) then 'true' else 'false' end as coupon_redeemed
+        , array_join(array_distinct(array_agg(users_user.user_email)), ', ') as combined_user_emails
+        , array_join(array_distinct(array_agg(users_user.user_address_country)), ', ') as user_address_countries
+        , array_join(array_distinct(array_agg(ecommerce_order.order_id)), ', ') as order_ids
+        , array_join(array_distinct(array_agg(ecommerce_order.order_state)), ', ') as combined_order_state
     from coupon
-    left join productcouponassignment
-        on productcouponassignment.coupon_id = coupon.coupon_id
-    left join ecommerce_couponversion
+    left join ecommerce_couponversion 
         on coupon.coupon_id = ecommerce_couponversion.coupon_id
-    left join ecommerce_couponpaymentversion
-        on ecommerce_couponpaymentversion.couponpaymentversion_id = ecommerce_couponversion.couponpaymentversion_id
     left join couponredemption
         on ecommerce_couponversion.couponversion_id = couponredemption.couponversion_id
+    left join ecommerce_couponpaymentversion 
+        on ecommerce_couponpaymentversion.couponpaymentversion_id = ecommerce_couponversion.couponpaymentversion_id
     left join ecommerce_order
         on ecommerce_order.order_id = couponredemption.order_id
-    left join payment_transactions
-        on coupon.coupon_id = payment_transactions.coupon_id
-    left join ecommerce_line
+    left join ecommerce_line 
         on ecommerce_line.order_id = ecommerce_order.order_id
     left join program_runs
         on ecommerce_line.programrun_id = program_runs.programrun_id
@@ -138,14 +113,11 @@ with coupon as (
         on course_runs.course_id = courses.course_id
     left join productversion
         on productversion.productversion_id = ecommerce_line.productversion_id
-    group by
-        productcouponassignment.productcouponassignment_email
-        , coupon.coupon_code
-        , coupon.coupon_id
-        , coupon.coupon_created_on
-        , coupon.couponpayment_name
-        , productcouponassignment.product_id
-        , payment_transactions.couponpaymentversion_payment_transaction
+    left join users_user    
+        on ecommerce_order.order_purchaser_user_id = users_user.user_id
+    group by 
+        coupon.coupon_id
+        , ecommerce_couponversion.couponversion_updated_on
         , program_runs.programrun_readable_id
         , program_runs.program_title
         , course_runs.courserun_readable_id
@@ -155,45 +127,34 @@ with coupon as (
         , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
 )
 
-, b2b_coupon_summary as (
-    select
-        coupon.coupon_id
-        , case
-            when (count(couponredemption.couponredemption_id) >= 1)
-                then 'true'
-            else 'false'
-        end as coupon_redeemed
-        , array_join(array_distinct(array_agg(users_user.user_email)), ', ') as combined_user_emails
-        , array_join(array_distinct(array_agg(users_user.user_address_country)), ', ') as user_address_countries
-        , array_join(array_distinct(array_agg(ecommerce_order.order_id)), ', ') as order_ids
-        , array_join(array_distinct(array_agg(ecommerce_order.order_state)), ', ') as combined_order_state
-    from b2b_ecommerce_b2border
-    inner join ecommerce_couponpaymentversion
-        on ecommerce_couponpaymentversion.couponpaymentversion_id = b2b_ecommerce_b2border.couponpaymentversion_id
-    inner join ecommerce_couponversion
-        on ecommerce_couponversion.couponpaymentversion_id = ecommerce_couponpaymentversion.couponpaymentversion_id
-    inner join coupon
-        on coupon.coupon_id = ecommerce_couponversion.coupon_id
-    left join couponredemption
-        on couponredemption.couponversion_id = ecommerce_couponversion.couponversion_id
-    left join ecommerce_order
-        on ecommerce_order.order_id = couponredemption.order_id
-    left join users_user
-        on ecommerce_order.order_purchaser_user_id = users_user.user_id
-    group by
-        coupon.coupon_id
+, reg_coupon_fields as (
+    select 
+        productcouponassignment.productcouponassignment_email as coupon_email
+        , coupon.coupon_code
+        , coupon.coupon_id
+        , coupon.coupon_created_on
+        , coupon.couponpayment_name
+        , productcouponassignment.product_id
+        , payment_transactions.couponpaymentversion_payment_transaction 
+        , productcouponassignment.productcouponassignment_is_redeemed
+    from coupon
+    left join productcouponassignment
+        on productcouponassignment.coupon_id = coupon.coupon_id
+    left join payment_transactions
+        on coupon.coupon_id = payment_transactions.coupon_id
 )
 
 , b2b_coupon_fields as (
     select
         b2b_ecommerce_b2border.b2border_email
+        , ecommerce_couponversion.couponversion_updated_on
         , coupon.coupon_code
         , coupon.coupon_id
         , coupon.coupon_created_on
         , coupon.couponpayment_name
         , b2b_ecommerce_b2border.b2border_contract_number
         , b2b_ecommerce_b2border.product_id
-        , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
+        , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction 
         , program_runs.programrun_readable_id
         , program_runs.program_title
         , course_runs.courserun_readable_id
@@ -201,6 +162,10 @@ with coupon as (
         , productversion.productversion_readable_id
         , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
         , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
+        , users_user.user_email as combined_user_emails
+        , users_user.user_address_country as user_address_countries
+        , ecommerce_order.order_id as order_ids
+        , ecommerce_order.order_state as combined_order_state
         , replace(
             cast(
                 json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number'
@@ -208,20 +173,25 @@ with coupon as (
                 as varchar
             ), '"', ''
         ) as req_reference_number
-    from b2b_ecommerce_b2border
-    inner join ecommerce_couponpaymentversion
+        , case 
+            when couponredemption.couponredemption_id is not null
+                then 'true' 
+            else 'false' 
+        end as coupon_redeemed
+    from b2b_ecommerce_b2border 
+    inner join ecommerce_couponpaymentversion 
         on ecommerce_couponpaymentversion.couponpaymentversion_id = b2b_ecommerce_b2border.couponpaymentversion_id
-    inner join ecommerce_couponversion
+    inner join ecommerce_couponversion 
         on ecommerce_couponversion.couponpaymentversion_id = ecommerce_couponpaymentversion.couponpaymentversion_id
-    inner join coupon
+    inner join coupon 
         on coupon.coupon_id = ecommerce_couponversion.coupon_id
-    left join couponredemption
+    left join couponredemption 
         on couponredemption.couponversion_id = ecommerce_couponversion.couponversion_id
-    left join ecommerce_order
+    left join ecommerce_order 
         on ecommerce_order.order_id = couponredemption.order_id
-    left join b2b_ecommerce_b2breceipt
+    left join b2b_ecommerce_b2breceipt 
         on b2b_ecommerce_b2breceipt.b2border_id = b2b_ecommerce_b2border.b2border_id
-    left join ecommerce_line
+    left join ecommerce_line 
         on ecommerce_line.order_id = ecommerce_order.order_id
     left join program_runs
         on ecommerce_line.programrun_id = program_runs.programrun_id
@@ -231,35 +201,16 @@ with coupon as (
         on course_runs.course_id = courses.course_id
     left join productversion
         on productversion.productversion_id = b2b_ecommerce_b2border.productversion_id
-    group by
-        b2b_ecommerce_b2border.b2border_email
-        , coupon.coupon_code
-        , coupon.coupon_id
-        , coupon.coupon_created_on
-        , coupon.couponpayment_name
-        , b2b_ecommerce_b2border.b2border_contract_number
-        , b2b_ecommerce_b2border.product_id
-        , ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
-        , program_runs.programrun_readable_id
-        , program_runs.program_title
-        , course_runs.courserun_readable_id
-        , courses.course_title
-        , productversion.productversion_readable_id
-        , ecommerce_couponpaymentversion.couponpaymentversion_coupon_type
-        , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount
-        , replace(
-            cast(
-                json_extract(b2b_ecommerce_b2breceipt.b2breceipt_data, '$.req_reference_number')
-                as varchar
-            ), '"', ''
-        )
+    left join users_user    
+        on ecommerce_order.order_purchaser_user_id = users_user.user_id
 )
 
 
-select
+select 
     reg_coupon_fields.coupon_email
     , reg_coupon_fields.coupon_code
     , reg_coupon_fields.coupon_id
+    , reg_coupon_fields.couponversion_updated_on
     , reg_coupon_fields.coupon_created_on
     , reg_coupon_fields.couponpayment_name
     , null as b2border_contract_number
@@ -286,10 +237,11 @@ where reg_coupon_fields.coupon_id not in (select coupon_id from b2b_coupon_summa
 
 union distinct
 
-select
+select 
     b2b_coupon_fields.b2border_email as coupon_email
     , b2b_coupon_fields.coupon_code
     , b2b_coupon_fields.coupon_id
+    , b2b_coupon_fields.couponversion_updated_on
     , b2b_coupon_fields.coupon_created_on
     , b2b_coupon_fields.couponpayment_name
     , b2b_coupon_fields.b2border_contract_number
@@ -310,5 +262,3 @@ select
     , b2b_coupon_summary.order_ids
     , b2b_coupon_summary.combined_order_state
 from b2b_coupon_fields
-inner join b2b_coupon_summary
-    on b2b_coupon_fields.coupon_id = b2b_coupon_summary.coupon_id
