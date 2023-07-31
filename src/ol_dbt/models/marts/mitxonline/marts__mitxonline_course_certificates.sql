6aad1fcdfbd2b49275ba471a8e4ad47aebe35e79
rachellougee <rlougee@mit.edu>
2023-07-31 13:47:36 -0400

    stage openedx mitxonline users and add openedx_user_id to marts__mitxonline_course_certificates

--- src/ol_dbt/models/marts/mitxonline/marts__mitxonline_course_certificates.sql
+++ src/ol_dbt/models/marts/mitxonline/marts__mitxonline_course_certificates.sql
@@ -3,13 +3,19 @@ with course_certificates as (
     where platform = '{{ var("mitxonline") }}'
 )
 
+, users as (
+    select * from {{ ref('int__mitxonline__users') }}
+)
+
 select
-    course_number
-    , courserun_title
-    , courserun_readable_id
-    , courseruncertificate_url
-    , courseruncertificate_created_on
-    , user_mitxonline_username as user_username
-    , user_email
-    , user_full_name
+    course_certificates.course_number
+    , course_certificates.courserun_title
+    , course_certificates.courserun_readable_id
+    , course_certificates.courseruncertificate_url
+    , course_certificates.courseruncertificate_created_on
+    , course_certificates.user_mitxonline_username as user_username
+    , course_certificates.user_email
+    , course_certificates.user_full_name
+    , users.openedx_user_id
 from course_certificates
+left join users on course_certificates.user_mitxonline_username = users.user_username
---
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__openedx__mysql__auth_user.sql
@@ -0,0 +1,19 @@
+-- MITx Online open edX User Information
+
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__mitxonline__openedx__mysql__auth_user') }}
+)
+
+, cleaned as (
+
+    select
+        id as openedx_user_id
+        , username as user_username
+        , email as user_email
+        , is_active as user_is_active
+        , is_staff as user_is_staff
+        , is_superuser as user_is_superuser
+    from source
+)
+
+select * from cleaned
