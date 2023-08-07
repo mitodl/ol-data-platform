1ac7587e25037787240c21b02b4bb373019559f3
Katelyn Delaney <katelyn@mit.edu>
2023-08-07 08:00:15 -0400

    new

---
+++ src/ol_dbt/models/marts/mitxpro/marts__mitxpro_ecommerce_productlist.sql
@@ -0,0 +1,79 @@
+with ecommerce_product as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_product') }}
+)
+
+with ecommerce_productversion as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_productversion') }}
+)
+
+with course_runs as (
+    select *
+    from {{ ref('int__mitxpro__course_runs') }}
+)
+
+with programs as (
+    select *
+    from {{ ref('int__mitxpro__programs') }}
+)
+
+with program_runs as (
+    select *
+    from {{ ref('int__mitxpro__program_runs') }}
+)
+
+with ecommerce_productversion_latest as (
+    select *
+    from (
+            select * ,
+            rank() over(partition by product_id order by productversion_updated_on desc) as rank
+            from ecommerce_productversion
+        ) as a 
+    where rank = 1
+    order by product_id
+)
+
+select  
+'xPRO' as product_platform
+,ecommerce_product.product_id as eProductId
+,course_runs.courserun_title as product_name
+,course_runs.courserun_readable_id  as product_id
+,ecommerce_product.product_type
+,ecommerce_productversion_latest.productversion_price as list_price
+,ecommerce_productversion_latest.productversion_description as Product_description
+,course_runs.courserun_start_on as start_date
+,course_runs.courserun_end_on as end_date
+,course_runs.courserun_enrollment_start_on as enrollment_start
+,course_runs.courserun_enrollment_end_on as enrollment_end
+,'<a href="https://xpro.mit.edu/checkout?product=' || cast(ecommerce_product.product_id as varchar(50)) || '">' || course_runs.courserun_readable_id  || '</a>' as link
+from ecommerce_product
+left join ecommerce_productversion_latest
+on ecommerce_product.product_id=ecommerce_productversion_latest.product_id
+left join course_runs
+on ecommerce_product.courserun_id=course_runs.courserun_id
+where ecommerce_product.product_type= 'course run'
+
+union distinct 
+
+select 
+'xPRO' as product_platform
+,ecommerce_product.product_id as eProductId
+,program_runs.program_title as product_name
+,program_runs.programrun_readable_id as product_id
+,'programrun' as product_type
+,ecommerce_productversion_latest.productversion_price as list_price
+,ecommerce_productversion_latest.productversion_description as Product_description
+,program_runs.programrun_start_on as start_date
+,program_runs.programrun_end_on as end_date
+,null as enrollment_start
+,null as enrollment_end
+,'<a href="https://xpro.mit.edu/checkout?product=' || program_runs.programrun_readable_id || '">' || program_runs.programrun_readable_id || '</a>' as link           
+from ecommerce_product
+left join ecommerce_productversion_latest
+on ecommerce_product.product_id=ecommerce_productversion_latest.product_id
+left join programs
+on ecommerce_product.program_id=programs.program_id
+left join program_runs
+on programs.program_id=program_runs.program_id
+where ecommerce_product.product_type = 'program'  and program_runs.programrun_id is not null
