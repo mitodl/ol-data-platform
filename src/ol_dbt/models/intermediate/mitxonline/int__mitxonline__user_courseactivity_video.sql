9dad1df61a9a8ab136536bf4e3c190ae315f73ac
Rachel Lougee <rlougee@mit.edu>
2023-08-14 14:36:17 -0400

    Create MITx Online user activities intermediate model, updates to xPro (#795)
    * Create MITx Online user activities intermediate model, updates to xPro
    
    * updates for production data
    
    * Create MITx Online user activities intermediate model, updates to xPro

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_video.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_video.sql
@@ -1,6 +1,8 @@
 {{ config(materialized='view') }}
+
 with course_activities as (
-    select * from {{ ref('__mitxpro__user_courseactivities') }}
+    select * from {{ ref('stg__mitxonline__openedx__tracking_logs__user_activity') }}
+    where courserun_readable_id is not null
 )
 
 select
@@ -9,15 +11,9 @@ select
     , openedx_user_id
     , useractivity_event_source
     , useractivity_event_type
-    , useractivity_event_name
-    , useractivity_event_object
     , useractivity_timestamp
     , json_query(useractivity_event_object, 'lax $.id' omit quotes) as useractivity_video_id
     , json_query(useractivity_event_object, 'lax $.duration' omit quotes) as useractivity_video_duration
     , json_query(useractivity_event_object, 'lax $.currentTime' omit quotes) as useractivity_video_currenttime
 from course_activities
-where useractivity_event_type in (
-    'load_video', 'play_video', 'pause_video', 'stop_video', 'seek_video'
-    , 'speed_change_video', 'show_transcript', 'hide_transcript'
-    , 'edx.video.closed_captions.hidden', 'edx.video.closed_captions.shown'
-)
+where useractivity_event_type in ('play_video', 'pause_video', 'stop_video')
