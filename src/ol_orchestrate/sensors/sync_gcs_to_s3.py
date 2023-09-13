0b3ba1364940a99314fd4a782f4cc94e171d9de8
Tobias Macey <tmacey@mit.edu>
2023-09-13 16:11:21 -0400

    fix,refactor: Migrate GCS sensor to use Pythonic config and fix sensor
    The sensor code was written with a hard-coded `run_key` which would always cause it to
    skip any runs after the first because that run key had already been triggered. This
    migrates the GCP GCS resource to use the Pythonic config so it can be more easily
    integrated into the sensor, and then updates the usage of the cursor to do set
    arithmetic for any files that have not yet been processed.

--- src/ol_orchestrate/sensors/sync_gcs_to_s3.py
+++ src/ol_orchestrate/sensors/sync_gcs_to_s3.py
@@ -1,22 +1,21 @@
-from dagster import RunRequest, SensorEvaluationContext, SkipReason, build_resources
+import json
 
-from ol_orchestrate.lib.yaml_config_helper import load_yaml_config
-from ol_orchestrate.resources.gcp_gcs import gcp_gcs_resource
+from dagster import RunRequest, SensorEvaluationContext, SkipReason
+
+from ol_orchestrate.resources.gcp_gcs import GCSConnection
 
 
-def check_new_gcs_assets_sensor(context: SensorEvaluationContext):
-    gcs_config = load_yaml_config("/etc/dagster/edxorg_gcp.yaml")
-    with build_resources(
-        resources={"gcp_gcs": gcp_gcs_resource}, resource_config=gcs_config["resources"]
-    ) as resources:
-        storage_client = resources.gcp_gcs
-        bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
-        new_files = storage_client.list_blobs(bucket)
-        if new_files:
-            context.update_cursor(str(new_files))
-            yield RunRequest(
-                run_key="new_gcs_file",
-                run_config=gcs_config,
-            )
-        else:
-            yield SkipReason("No new files in GCS bucket")
+def check_new_gcs_assets_sensor(
+    context: SensorEvaluationContext, gcp_gcs: GCSConnection
+):
+    storage_client = gcp_gcs.client
+    bucket = storage_client.get_bucket("simeon-mitx-course-tarballs")
+    bucket_files = {file.name for file in storage_client.list_blobs(bucket)}
+    new_files = bucket_files.difference(set(context.cursor or []))
+    if new_files:
+        context.update_cursor(json.dumps(list(bucket_files)))
+        yield RunRequest(
+            run_config={},
+        )
+    else:
+        yield SkipReason("No new files in GCS bucket")
