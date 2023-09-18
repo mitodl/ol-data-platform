790ac49aa31337566d2d1c99d55fe5c5714776a3
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2023-09-18 06:12:50 -0400

    add platform name to tables (#834)
    adding platform_name to int__mitxpro__programs and int__mitxpro__courses

--- src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__programs.sql
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__programs.sql
@@ -8,13 +8,17 @@ with programs as (
     select * from {{ ref('stg__mitxpro__app__postgres__cms_programpage') }}
 )
 
+, platform as (
+    select * from {{ ref('stg__mitxpro__app__postgres__courses_platform') }}
+)
+
 select
     programs.program_id
     , programs.program_title
     , programs.program_is_live
     , programs.program_readable_id
     , programs.program_is_external
-    , programs.platform_id
+    , platform.platform_name
     , cms_programs.cms_programpage_description
     , cms_programs.cms_programpage_subhead
     , cms_programs.cms_programpage_catalog_details
@@ -24,3 +28,5 @@ select
 from programs
 left join cms_programs
     on programs.program_id = cms_programs.program_id
+left join platform
+    on programs.platform_id = platform.platform_id
