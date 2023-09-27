8e59a8469c19e54a3a04bd060db6bc41de33066d
Tobias Macey <tmacey@mit.edu>
2023-09-27 10:26:33 -0400

    refactor,feat: Add explicit S3 download and upload ops
    There are cases where a direct copy from bucket to bucket won't work (e.g. incompatible
    IAM permissions). This adds explicit steps for downloading files from s3 to a local
    filesystem and then uploading from a local filesystem to S3.

--- src/ol_orchestrate/definitions/edx/sync_program_credential_reports.py
+++ src/ol_orchestrate/definitions/edx/sync_program_credential_reports.py
@@ -4,14 +4,22 @@ from functools import partial
 from dagster import DefaultSensorStatus, Definitions, SensorDefinition, job
 from dagster_aws.s3 import S3Resource
 
-from ol_orchestrate.ops.object_storage import SyncS3ObjectsConfig, sync_files_to_s3
+from ol_orchestrate.ops.object_storage import (
+    S3DownloadConfig,
+    S3UploadConfig,
+    download_files_from_s3,
+    upload_files_to_s3,
+)
+from ol_orchestrate.resources.outputs import SimpleResultsDir
 from ol_orchestrate.sensors.sync_gcs_to_s3 import check_new_s3_assets_sensor
 
 dagster_deployment = os.getenv("DAGSTER_ENVIRONMENT", "qa")
-sync_config = SyncS3ObjectsConfig(
+download_config = S3DownloadConfig(
     source_bucket="edx-program-reports",
-    destination_bucket=f"ol-data-lake-raw-{dagster_deployment.lower()}",
-    destination_prefix="landing_zone",
+)
+upload_config = S3UploadConfig(
+    destination_bucket=f"ol-data-lake-landing-zone-{dagster_deployment.lower()}",
+    destination_prefix="edxorg-program-credentials",
 )
 
 
@@ -19,14 +27,24 @@ sync_config = SyncS3ObjectsConfig(
     name="sync_edxorg_program_reports",
     description="Replicate program credential reports from edx.org into our own S3 "
     "bucket so that it can be ingested into the OL data lake.",
-    config={"ops": {"sync_files_to_s3": {"config": sync_config.dict()}}},
+    config={
+        "ops": {
+            "download_files_from_s3": {"config": download_config.dict()},
+            "upload_files_to_s3": {"config": upload_config.dict()},
+        }
+    },
 )
 def sync_edxorg_program_reports():
-    sync_files_to_s3()
+    upload_files_to_s3(download_files_from_s3())
 
 
 edxorg_program_reports = Definitions(
-    resources={"s3": S3Resource(profile_name="edxorg-programs")},
+    resources={
+        "s3": S3Resource(profile_name="edxorg"),
+        "s3_download": S3Resource(profile_name="edxorg"),
+        "s3_upload": S3Resource(),
+        "results_dir": SimpleResultsDir.configure_at_launch(),
+    },
     sensors=[
         SensorDefinition(
             name="edxorg_program_reports_sensor",
@@ -36,12 +54,13 @@ edxorg_program_reports = Definitions(
                 bucket_prefix="reports_v2/MITx/",
                 run_config_fn=lambda new_keys: {
                     "ops": {
-                        "sync_files_to_s3": {
+                        "download_files_from_s3": {
                             "config": {
-                                **sync_config.dict(),
+                                **download_config.dict(),
                                 "object_keys": list(new_keys),
                             }
-                        }
+                        },
+                        "upload_files_to_s3": {"config": upload_config.dict()},
                     }
                 },
             ),
