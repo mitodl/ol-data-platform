7e2f9db4d4bd040096df1748abc1ae5f7b6012d8
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2023-10-06 09:01:54 -0400

    add tax fields to order (#857)
    Adds order_tax_country_code, order_tax_rate, and order_tax_rate_name to the stg__mitxpro__app__postgres__ecommerce_order table

--- src/ol_dbt/models/staging/mitxpro/stg__mitxpro__app__postgres__ecommerce_order.sql
+++ src/ol_dbt/models/staging/mitxpro/stg__mitxpro__app__postgres__ecommerce_order.sql
@@ -10,6 +10,9 @@ with source as (
         , status as order_state
         , total_price_paid as order_total_price_paid
         , purchaser_id as order_purchaser_user_id
+        , tax_country_code as order_tax_country_code
+        , tax_rate as order_tax_rate
+        , tax_rate_name as order_tax_rate_name
         ,{{ cast_timestamp_to_iso8601('updated_on') }} as order_updated_on
         ,{{ cast_timestamp_to_iso8601('created_on') }} as order_created_on
     from source
