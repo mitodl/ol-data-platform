366aae9ccdf98b19b53fb9812d4e68b54636b7f0
Tobias Macey <tmacey@mit.edu>
2023-11-13 12:40:17 -0500

    feat: Add course and block content hash to unnested blocks records

--- src/ol_orchestrate/lib/openedx.py
+++ src/ol_orchestrate/lib/openedx.py
@@ -1,4 +1,6 @@
 import datetime
+import hashlib
+import json
 from typing import Any
 
 
@@ -23,6 +25,9 @@ def un_nest_course_structure(
     # We need to loop through the course structure twice. Once to populate the full
     # ancestry and a second time to build the record structure so that we can pull the
     # block parents.
+    content_hash = hashlib.sha256(
+        json.dumps(course_structure).encode("utf-8")
+    ).hexdigest()
     for block_id, block_contents in course_structure.items():
         if block_contents["category"] == "course":
             course_title = block_contents["metadata"].get("display_name")
@@ -32,17 +37,21 @@ def un_nest_course_structure(
     for block_id, block_contents in course_structure.items():
         course_blocks.append(
             {
-                "course_id": course_id,
-                "block_id": block_id,
+                "block_content_hash": hashlib.sha256(
+                    json.dumps(block_contents).encode("utf-8")
+                ).hexdigest(),
                 "block_details": block_contents,
+                "block_due": block_contents["metadata"].get("due"),
+                "block_id": block_id,
                 "block_parent": ancestry.get(block_id),
+                "block_start": block_contents["metadata"].get("start"),
                 "block_title": block_contents["metadata"].get("display_name"),
                 "block_type": block_contents["category"],
-                "block_start": block_contents["metadata"].get("start"),
-                "block_due": block_contents["metadata"].get("due"),
-                "retrieved_at": retrieved_at,
-                "course_title": course_title,
+                "course_content_hash": content_hash,
+                "course_id": course_id,
                 "course_start": course_start,
+                "course_title": course_title,
+                "retrieved_at": retrieved_at,
             }
         )
     return course_blocks
