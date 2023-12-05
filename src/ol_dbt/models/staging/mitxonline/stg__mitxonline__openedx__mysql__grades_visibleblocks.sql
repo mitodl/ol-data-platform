19322b8197ba66f0952de35502b985084f0d390b
Rachel Lougee <rlougee@mit.edu>
2023-12-05 12:51:47 -0500

    add subsection grading to staging and intermediate (#936)
    * add subsection grading to staging and intermediate
    
    * update field description

---
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__openedx__mysql__grades_visibleblocks.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__mitxonline__openedx__mysql__grades_visibleblocks') }}
+)
+
+, cleaned as (
+
+    select
+        id as visibleblocks_id
+        , course_id as courserun_readable_id
+        , hashed as visibleblocks_hash
+        , blocks_json as visibleblocks_json
+    from source
+)
+
+select * from cleaned
