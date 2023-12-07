d440c43f93e651e80ee5564cfc4ff946339077b6
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2023-12-07 13:55:41 -0500

    update

--- src/ol_dbt/models/marts/combined/marts__combined__orders.sql
+++ src/ol_dbt/models/marts/combined/marts__combined__orders.sql
@@ -29,9 +29,14 @@ with bootcamps__ecommerce_order as (
     select * from {{ ref('int__mitxpro__ecommerce_order') }}
 )
 
+, mitxpro__ecommerce_allcoupons as (
+    select * from {{ ref('int__mitxpro__ecommerce_allcoupons') }}
+)
+
 , mitxpro_orders as (
-    select
+    select 
         mitxpro__ecommerce_allorders.line_id
+        , mitxpro__ecommerce_allcoupons.coupon_name
         , mitxpro__ecommerce_allorders.order_created_on
         , mitxpro__ecommerce_allorders.order_state
         , mitxpro__ecommerce_allorders.product_id
@@ -41,11 +46,12 @@ with bootcamps__ecommerce_order as (
         , mitxpro__ecommerce_order.order_purchaser_user_id
         , mitxpro__ecommerce_order.order_total_price_paid
         , mitxpro__b2becommerce_b2border.b2border_total_price
+        , coalesce(mitxpro__ecommerce_allorders.coupon_id, mitxpro__ecommerce_allorders.b2bcoupon_id) as coupon_id
         , coalesce(mitxpro__ecommerce_allorders.order_id, mitxpro__ecommerce_allorders.b2border_id) as order_id
-        , case
-            when mitxpro__ecommerce_allorders.b2border_id is not null
-                then 'Y'
-        end as b2b_indicator
+        , case 
+            when mitxpro__ecommerce_allorders.order_id is null 
+                then 'Y' 
+        end as b2b_only_indicator
     from mitxpro__ecommerce_allorders
     left join mitxpro__course_runs
         on mitxpro__ecommerce_allorders.courserun_readable_id = mitxpro__course_runs.courserun_readable_id
@@ -53,10 +59,12 @@ with bootcamps__ecommerce_order as (
         on mitxpro__ecommerce_allorders.order_id = mitxpro__ecommerce_order.order_id
     left join mitxpro__b2becommerce_b2border
         on mitxpro__ecommerce_allorders.b2border_id = mitxpro__b2becommerce_b2border.b2border_id
+    left join mitxpro__ecommerce_allcoupons
+        on mitxpro__ecommerce_allorders.coupon_id = mitxpro__ecommerce_allcoupons.coupon_id
 )
 
 , bootcamps_orders as (
-    select
+    select 
         bootcamps__ecommerce_order.order_id
         , bootcamps__ecommerce_order.line_id
         , bootcamps__ecommerce_order.order_created_on
@@ -83,7 +91,9 @@ with bootcamps__ecommerce_order as (
         , product_type
         , user_email
         , user_id
-        , null as b2b_indicator
+        , null as b2b_only_indicator
+        , null as coupon_id
+        , null as coupon_name
     from mitxonline__ecommerce_order
 
     union all
@@ -100,7 +110,9 @@ with bootcamps__ecommerce_order as (
         , product_type
         , user_email
         , order_purchaser_user_id as user_id
-        , b2b_indicator
+        , b2b_only_indicator
+        , coupon_id
+        , coupon_name
     from mitxpro_orders
 
     union all
@@ -117,7 +129,9 @@ with bootcamps__ecommerce_order as (
         , null as product_type
         , user_email
         , order_purchaser_user_id as user_id
-        , null as b2b_indicator
+        , null as b2b_only_indicator
+        , null as coupon_id
+        , null as coupon_name
     from bootcamps_orders
 
 )
