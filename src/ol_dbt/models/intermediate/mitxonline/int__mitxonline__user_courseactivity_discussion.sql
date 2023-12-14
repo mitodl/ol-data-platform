730a687f330d2cbc42b709d776c71f8edff60b2e
Rachel Lougee <rlougee@mit.edu>
2023-12-14 11:37:05 -0500

    update description and page_url

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_discussion.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_discussion.sql
@@ -11,7 +11,6 @@ select
     , openedx_user_id
     , useractivity_event_source
     , useractivity_event_type
-    , useractivity_path
     , useractivity_timestamp
     , json_query(useractivity_event_object, 'lax $.id' omit quotes) as useractivity_discussion_post_id
     , json_query(useractivity_event_object, 'lax $.title' omit quotes) as useractivity_discussion_post_title
--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_problemcheck.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_problemcheck.sql
@@ -10,7 +10,6 @@ select
     , courserun_readable_id
     , openedx_user_id
     , useractivity_event_type
-    , useractivity_path
     , useractivity_timestamp
     , json_query(useractivity_context_object, 'lax $.module.display_name' omit quotes) as useractivity_problem_name
     , json_query(useractivity_event_object, 'lax $.problem_id' omit quotes) as useractivity_problem_id
