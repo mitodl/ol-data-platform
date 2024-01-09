132511bef600af0db466ee75dd6b6a3290960c0b
Collin Preston <collin_preston@yahoo.com>
2024-01-09 14:08:07 -0500

    Code review comments

--- src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_course_to_department.sql
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_course_to_department.sql
@@ -1,11 +1,12 @@
--- MITx Online Course to Department Information
-
 with source as (
-    select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__courses_department') }}
+    select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__courses_course_departments') }}
 )
 
 , cleaned as (
-    select id as coursetodepartment_id
+    select 
+          id as coursetodepartment_id
+         , department_id as coursedepartment_id
+          , course_id
     from source
 )
 
