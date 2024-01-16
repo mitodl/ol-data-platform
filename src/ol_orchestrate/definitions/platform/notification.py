d1934fb16d37dc27e86dce0e236599184e0d04ac
Tobias Macey <tmacey@mit.edu>
2024-01-16 15:36:27 -0500

    feat: Add slack notification for pipeline failurs
    - Adds a Vault resource for accessing production secrets
    - Adds a code location for monitoring failed pipline runs and notifying with a Slack hook

---
+++ src/ol_orchestrate/definitions/platform/notification.py
@@ -0,0 +1,30 @@
+import os
+
+from dagster import Definitions
+from dagster_slack import make_slack_on_run_failure_sensor
+
+from ol_orchestrate.resources.secrets.vault import Vault
+
+DAGSTER_ENV = os.environ.get("DAGSTER_ENVIRONMENT", "dev")
+vault_addr = os.getenv("VAULT_ADDR", "localhost:8200")
+if DAGSTER_ENV == "dev":
+    vault = Vault(vault_addr=vault_addr, vault_auth_type="github")
+    vault._auth_github()  # noqa: SLF001
+else:
+    vault = Vault(
+        vault_addr=vault_addr, vault_role="dagster-server", aws_auth_mount="aws"
+    )
+    vault._auth_aws_iam()  # noqa: SLF001
+
+notifications = Definitions(
+    sensors=[
+        make_slack_on_run_failure_sensor(
+            channel="#data-platform-alerts",
+            webserver_base_url=os.getenv("DAGSTER_HOSTNAME", "localhost:3000"),
+            monitor_all_repositories=True,
+            slack_token=vault._client.secrets.kv.v2.read_secret(  # noqa: SLF001
+                path="slack", mount_point="secret-data"
+            )["data"]["data"]["token"],
+        )
+    ]
+)
---
+++ src/ol_orchestrate/resources/secrets/vault.py
@@ -0,0 +1,39 @@
+import os
+from typing import Optional
+
+import boto3
+import hvac
+from dagster import ConfigurableResource
+from pydantic import PrivateAttr
+
+
+class Vault(ConfigurableResource):
+    vault_addr: str
+    vault_token: Optional[str]
+    vault_role: Optional[str]
+    vault_auth_type: str = "aws-iam"  # can be one of ["github", "aws-iam", "token"]
+    auth_mount: Optional[str] = None
+    _client: hvac.Client = PrivateAttr()
+
+    def _auth_aws_iam(self):
+        session = boto3.Session()
+        credentials: boto3.iam.Credentials = session.get_credentials()
+
+        self._client = hvac.Client()
+        self._client.auth.aws.iam_login(
+            credentials.access_key,
+            credentials.secret_key,
+            credentials.token,
+            use_token=True,
+            role=self.vault_role,
+            mount_point=self.auth_mount or "aws",
+        )
+
+    def _auth_github(self):
+        gh_token = os.environ.get("GITHUB_TOKEN")
+        self._client = hvac.Client()
+        self._client.auth.github.login(
+            token=gh_token,
+            use_token=True,
+            mount_point=self.auth_mount or "github",
+        )
