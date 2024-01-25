5547e0fb53b407c1fcb34f256e9c2d47b778f318
Rachel Lougee <rlougee@mit.edu>
2024-01-25 15:55:12 -0500

    xPro user activities intermediate models and a few small updates to MITx Online models (#985)
    * xPro user activities intermediate models
    
    * additional xPro user activity intermediate models, changes to MITx Online models

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_video.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__user_courseactivity_video.sql
@@ -21,4 +21,4 @@ select
     , json_query(useractivity_event_object, 'lax $.new_speed' omit quotes) as useractivity_video_new_speed
     , json_query(useractivity_event_object, 'lax $.old_speed' omit quotes) as useractivity_video_old_speed
 from course_activities
-where useractivity_event_type like '%video' or useractivity_event_type like 'edx.video%'
+where useractivity_event_type like '%\_video' escape '\' or useractivity_event_type like 'edx.video.%' --noqa
---
+++ src/ol_dbt/models/intermediate/mitxpro/int__mitxpro__user_courseactivities_daily.sql
@@ -0,0 +1,19 @@
+with course_activities as (
+    select * from {{ ref('stg__mitxpro__openedx__tracking_logs__user_activity') }}
+    where courserun_readable_id is not null
+)
+
+, daily_activities_stats as (
+    select
+        user_username
+        , courserun_readable_id
+        , date(from_iso8601_timestamp(useractivity_timestamp)) as courseactivity_date
+        , count(*) as courseactivity_num_events
+    from course_activities
+    group by
+        user_username
+        , courserun_readable_id
+        , date(from_iso8601_timestamp(useractivity_timestamp))
+)
+
+select * from daily_activities_stats
