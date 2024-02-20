3d88ad63bd17068089939041117ad8fe1cae92f3
Tobias Macey <tmacey@mit.edu>
2024-02-20 09:32:34 -0500

    fix: Set Dagster URL for Slack notifierr

--- src/ol_orchestrate/definitions/platform/notification.py
+++ src/ol_orchestrate/definitions/platform/notification.py
@@ -7,9 +7,15 @@ from ol_orchestrate.lib.constants import DAGSTER_ENV, VAULT_ADDRESS
 from ol_orchestrate.resources.secrets.vault import Vault
 
 if DAGSTER_ENV == "dev":
+    dagster_url = "http://localhost:3000"
     vault = Vault(vault_addr=VAULT_ADDRESS, vault_auth_type="github")
     vault._auth_github()  # noqa: SLF001
 else:
+    dagster_url = (
+        "https://pipelines.odl.mit.edu"
+        if DAGSTER_ENV == "production"
+        else "https://pipelines-qa.odl.mit.edu"
+    )
     vault = Vault(
         vault_addr=VAULT_ADDRESS, vault_role="dagster-server", aws_auth_mount="aws"
     )
@@ -19,7 +25,7 @@ notifications = Definitions(
     sensors=[
         make_slack_on_run_failure_sensor(
             channel="#data-platform-alerts",
-            webserver_base_url=os.getenv("DAGSTER_HOSTNAME", "localhost:3000"),
+            webserver_base_url=os.getenv("DAGSTER_HOSTNAME", dagster_url),
             monitor_all_repositories=True,
             slack_token=vault.client.secrets.kv.v1.read_secret(
                 path="dagster/slack", mount_point="secret-data"
