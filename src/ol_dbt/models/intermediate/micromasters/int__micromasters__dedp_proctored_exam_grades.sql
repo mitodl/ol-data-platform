54ada1d04851fda639f7f04b52b70904c65237f6
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-02-29 14:58:36 -0500

    update

--- src/ol_dbt/models/intermediate/micromasters/int__micromasters__dedp_proctored_exam_grades.sql
+++ src/ol_dbt/models/intermediate/micromasters/int__micromasters__dedp_proctored_exam_grades.sql
@@ -15,6 +15,10 @@ with exam_grades as (
     select * from {{ ref('__micromasters__users') }}
 )
 
+, mixonline_users as (
+    select * from {{ ref('int__mitxonline__users') }}
+)
+
 select
     programs.program_title
     , programs.micromasters_program_id
@@ -24,7 +28,8 @@ select
     , mm_users.user_edxorg_username
     , mm_users.user_mitxonline_username
     , mm_users.user_full_name
-    , mm_users.user_email
+    , mm_users.user_micromasters_email
+    , mixonline_users.user_email as user_mitxonline_email
     , exam_grades.proctoredexamgrade_score
     , exam_grades.proctoredexamgrade_is_passing
     , exam_grades.proctoredexamgrade_passing_score
@@ -37,3 +42,4 @@ from exam_grades
 inner join courses on exam_grades.course_id = courses.course_id
 inner join programs on courses.program_id = programs.micromasters_program_id
 inner join mm_users on exam_grades.user_id = mm_users.user_id
+left join mixonline_users on mm_users.user_mitxonline_username = mixonline_users.user_username
