fb11d32ba67b6c6c0057b50e538a8b55d8269829
Rachel Lougee <rlougee@mit.edu>
2024-03-01 11:27:29 -0500

    create staging and intermediate models for bootcamps application related tables

---
+++ src/ol_dbt/models/staging/bootcamps/stg__bootcamps__app__postgres__applications_applicationstep.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__bootcamps__app__postgres__applications_applicationstep') }}
+)
+
+, cleaned as (
+
+    select
+        id as applicationstep_id
+        , bootcamp_id as course_id
+        , step_order as applicationstep_step_order
+        , submission_type as applicationstep_submission_type
+    from source
+)
+
+select * from cleaned
---
+++ src/ol_dbt/models/staging/bootcamps/stg__bootcamps__app__postgres__courses_installment.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__bootcamps__app__postgres__klasses_installment') }}
+)
+
+, cleaned as (
+
+    select
+        id as installment_id
+        , bootcamp_run_id as courserun_id
+        , cast(amount as decimal(38, 2)) as installment_amount
+        , {{ cast_timestamp_to_iso8601('deadline') }} as installment_deadline
+    from source
+)
+
+select * from cleaned
