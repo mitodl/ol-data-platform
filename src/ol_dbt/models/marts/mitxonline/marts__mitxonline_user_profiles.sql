4dc3acaf685a1ccbf99ffd2a1f0f68776aca9351
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-03-07 08:11:02 -0500

    add income fields to mitxonline users mart

--- src/ol_dbt/models/marts/mitxonline/marts__mitxonline_user_profiles.sql
+++ src/ol_dbt/models/marts/mitxonline/marts__mitxonline_user_profiles.sql
@@ -2,13 +2,31 @@ with users as (
     select * from {{ ref('int__mitxonline__users') }}
 )
 
+, user_income as (
+    select 
+        user_id
+        , flexiblepriceapplication_income_usd
+        , flexiblepriceapplication_original_income
+        , flexiblepriceapplication_original_currency
+        , rank() over (partition by user_id order by flexiblepriceapplication_updated_on desc) as rnk  
+    from int__mitxonline__flexiblepricing_flexiblepriceapplication
+)
+
+
 select
-    user_username
-    , user_full_name
-    , user_email
-    , user_address_country
-    , user_address_state
-    , user_birth_year
-    , user_gender
-    , user_highest_education
+    users.user_username
+    , users.user_full_name
+    , users.user_email
+    , users.user_address_country
+    , users.user_address_state
+    , users.user_birth_year
+    , users.user_gender
+    , users.user_highest_education
+    , user_income.flexiblepriceapplication_income_usd as latest_income_usd
+    , user_income.flexiblepriceapplication_original_income as latest_original_income
+    , user_income.flexiblepriceapplication_original_currency as latest_original_currency
 from users
+left join user_income
+    on 
+        users.user_id = users.user_id
+        and user_income.rnk = 1
