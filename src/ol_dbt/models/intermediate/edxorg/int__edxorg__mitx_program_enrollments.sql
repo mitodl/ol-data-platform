376c159a71d7b863ae7499d4e3f04936cafa3190
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-03-22 11:29:00 -0400

    add a edx program enrollment int table

---
+++ src/ol_dbt/models/intermediate/edxorg/int__edxorg__mitx_program_enrollments.sql
@@ -0,0 +1,32 @@
+with program_learners as (
+    select * from {{ ref('stg__edxorg__s3__program_learner_report') }}
+)
+
+, micromasters_programs as (
+    select * from {{ ref('int__mitx__programs') }}
+    where is_micromasters_program = true
+)
+
+, program_learners_sorted as (
+    select
+        *
+        , row_number() over (partition by user_id, program_uuid order by courserunenrollment_created_on desc) as row_num
+    from program_learners
+)
+
+select
+    program_learners_sorted.program_type
+    , program_learners_sorted.program_uuid
+    , program_learners_sorted.program_title
+    , program_learners_sorted.user_id
+    , program_learners_sorted.user_username
+    , program_learners_sorted.user_full_name
+    , program_learners_sorted.user_has_completed_program
+    , micromasters_programs.micromasters_program_id
+from program_learners_sorted
+left join micromasters_programs
+    on (
+        program_learners_sorted.program_title like micromasters_programs.program_title || '%'
+        or program_learners_sorted.program_title like '%' || micromasters_programs.program_title
+    )
+where program_learners_sorted.row_num = 1
