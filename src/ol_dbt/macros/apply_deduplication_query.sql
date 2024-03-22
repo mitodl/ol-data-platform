1b85e27afc42e9b136c6d3a587bb77b63110185c
Rachel Lougee <rlougee@mit.edu>
2024-03-22 11:42:43 -0400

    Apply deduplication to dbt models for airbyte incremental sync using a timestamp cursor field (#1042)
    * process course xml to extract video metadata
    
    * apply dedup to open edx tables - part 1
    
    * apply dedup to open edx tables - part 2
    
    * revert
    
    * update comment

---
+++ src/ol_dbt/macros/apply_deduplication_query.sql
@@ -0,0 +1,22 @@
+{% macro deduplicate_query(cte_name1='source', cte_name2='most_recent_source') %}
+    /*
+        Add additional queries to handle duplicated data introduced by airbyte sync mode "Incremental Sync - Append"
+        where cursor is set to a timestamp field. The deduplication logic works like this:
+        - If there are multiple copies of the same record based on primary key id, we will use the record from most
+          recent sync in the source table.
+        - If there is only one record for the same primary key id, it will just load that record as it is.
+    */
+   , source_sorted as (
+        select
+            *
+            , row_number() over ( partition by id order by _airbyte_emitted_at desc) as row_num
+        from {{ cte_name1 }}
+    )
+
+    , {{ cte_name2 }} as (
+        select *
+        from source_sorted
+        where row_num = 1
+    )
+
+{% endmacro %}
--- src/ol_dbt/models/external/irx/xpro/irx__xpro__openedx__mysql__grades_persistentcoursegrade.sql
+++ src/ol_dbt/models/external/irx/xpro/irx__xpro__openedx__mysql__grades_persistentcoursegrade.sql
@@ -2,6 +2,8 @@ with grades_persistentcoursegrade as (
     select * from {{ source('ol_warehouse_raw_data','raw__xpro__openedx__mysql__grades_persistentcoursegrade') }}
 )
 
+{{ deduplicate_query('grades_persistentcoursegrade', 'most_recent_source') }}
+
 select
     course_id
     , user_id
@@ -11,5 +13,5 @@ select
     , passed_timestamp
     , created
     , modified
-from grades_persistentcoursegrade
+from most_recent_source
 order by user_id
