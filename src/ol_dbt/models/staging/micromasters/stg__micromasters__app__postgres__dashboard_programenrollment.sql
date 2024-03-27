8e30fdbf171e6a2ed303fb18f6b49522ac8a824e
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-03-27 16:02:18 -0400

    add a new staging program enrollment table (#1056)
    * add a new staging program enrollment table

---
+++ src/ol_dbt/models/staging/micromasters/stg__micromasters__app__postgres__dashboard_programenrollment.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__micromasters__app__postgres__dashboard_programenrollment') }}
+)
+
+, cleaned as (
+    select
+        id as programenrollment_id
+        , user_id
+        , program_id
+        , share_hash
+    from source
+)
+
+select * from cleaned
