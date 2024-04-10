827396ac76134cf23742335cd7a7ae7681108b94
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-04-10 10:28:04 -0400

    update

--- src/ol_dbt/models/marts/micromasters/marts__micromasters__summary.sql
+++ src/ol_dbt/models/marts/micromasters/marts__micromasters__summary.sql
@@ -26,24 +26,24 @@ with micromasters_enrollments as (
 , enrollments as (
     select    
         case when 
-            micromasters_program_certificates.program_title in (
+            micromasters_enrollments.program_title in (
                 'Data, Economics, and Design of Policy'
                 , 'Data, Economics, and Design of Policy: International Development'
                 , 'Data, Economics, and Design of Policy: Public Policy'
             )
             then 'Data, Economics, and Design of Policy' 
         when 
-            micromasters_program_certificates.program_title in (
+            micromasters_enrollments.program_title in (
                 'Statistics and Data Science'
                 , 'Statistics and Data Science (General track)'
             ) 
             then 'Statistics and Data Science'
         when 
-            micromasters_program_certificates.program_title in (
+            micromasters_enrollments.program_title in (
                 'MIT Finance'
             )
             then 'Finance'
-        else micromasters_program_certificates.program_title end as program_title
+        else micromasters_enrollments.program_title end as program_title
         , count(distinct micromasters_enrollments.courserun_readable_id || user_email) as total_enrollments
         , count(distinct micromasters_enrollments.user_email) as unique_users
         , count(distinct micromasters_enrollments.user_address_country) as unique_countries
@@ -56,49 +56,30 @@ with micromasters_enrollments as (
                 then micromasters_enrollments.user_email 
         end) as unique_verified_users
     from micromasters_enrollments
-    group by 1
-
-    union all
-
-    select
-        'total' as program_title
-        , count(distinct micromasters_enrollments.courserun_readable_id || micromasters_enrollments.user_email)
-        as total_enrollments
-        , count(distinct micromasters_enrollments.user_email) as unique_users
-        , count(distinct micromasters_enrollments.user_address_country) as unique_countries
-        , count(distinct case 
-            when micromasters_enrollments.courserunenrollment_enrollment_mode = 'verified'
-                then (micromasters_enrollments.courserun_readable_id || micromasters_enrollments.user_email) 
-        end) as verified_enrollments
-        , count(distinct case 
-            when micromasters_enrollments.courserunenrollment_enrollment_mode = 'verified' 
-                then micromasters_enrollments.user_email 
-        end)
-        as unique_verified_users
-    from micromasters_enrollments     
+    group by 1    
 )
 
 , course_certs as (    
     select
         case when 
-            micromasters_program_certificates.program_title in (
+            micromasters_course_certificates.program_title in (
                 'Data, Economics, and Design of Policy'
                 , 'Data, Economics, and Design of Policy: International Development'
                 , 'Data, Economics, and Design of Policy: Public Policy'
             )
             then 'Data, Economics, and Design of Policy' 
         when 
-            micromasters_program_certificates.program_title in (
+            micromasters_course_certificates.program_title in (
                 'Statistics and Data Science'
                 , 'Statistics and Data Science (General track)'
             ) 
             then 'Statistics and Data Science'
         when 
-            micromasters_program_certificates.program_title in (
+            micromasters_course_certificates.program_title in (
                 'MIT Finance'
             )
             then 'Finance'
-        else micromasters_program_certificates.program_title end as program_title
+        else micromasters_course_certificates.program_title end as program_title
         , count(
             distinct
             micromasters_course_certificates.courserun_readable_id 
@@ -115,26 +96,6 @@ with micromasters_enrollments as (
         mitxonline_course_certificates.courseruncertificate_is_revoked = false 
         or mitxonline_course_certificates.courseruncertificate_is_revoked is null
     group by 1
-
-    union all
- 
-    select
-        'total' as program_title
-        , count(
-            distinct
-            micromasters_course_certificates.courserun_readable_id 
-            || micromasters_course_certificates.user_email
-        ) as course_certificates
-        , count(distinct micromasters_course_certificates.user_email) as unique_course_certificate_earners
-    from micromasters_course_certificates
-    left join mitxonline_course_certificates
-        on 
-            micromasters_course_certificates.courserun_readable_id 
-            = mitxonline_course_certificates.courserun_readable_id
-            and micromasters_course_certificates.user_mitxonline_username = mitxonline_course_certificates.user_username
-    where 
-        mitxonline_course_certificates.courseruncertificate_is_revoked = false 
-        or mitxonline_course_certificates.courseruncertificate_is_revoked is null
 )
 
 , program_certs as (
@@ -169,42 +130,6 @@ with micromasters_enrollments as (
         mitxonline_program_certificates.programcertificate_is_revoked = false 
         or mitxonline_program_certificates.programcertificate_is_revoked is null
     group by 1
-
-    union all
-
-    select
-        'total' as program_title
-        , count(
-            distinct case when 
-                micromasters_program_certificates.program_title in (
-                    'Data, Economics, and Design of Policy'
-                    , 'Data, Economics, and Design of Policy: International Development'
-                    , 'Data, Economics, and Design of Policy: Public Policy'
-                )
-                then 'Data, Economics, and Design of Policy' 
-            when 
-                micromasters_program_certificates.program_title in (
-                    'Statistics and Data Science'
-                    , 'Statistics and Data Science (General track)'
-                ) 
-                then 'Statistics and Data Science'
-            when 
-                micromasters_program_certificates.program_title in (
-                    'MIT Finance'
-                )
-                then 'Finance'
-            else micromasters_program_certificates.program_title end
-            || micromasters_program_certificates.user_email
-        ) as program_certificates
-    from micromasters_program_certificates
-    left join mitxonline_program_certificates
-        on 
-            micromasters_program_certificates.mitxonline_program_id = mitxonline_program_certificates.program_id
-            and micromasters_program_certificates.user_mitxonline_username 
-            = mitxonline_program_certificates.user_username
-    where 
-        mitxonline_program_certificates.programcertificate_is_revoked = false 
-        or mitxonline_program_certificates.programcertificate_is_revoked is null
 )
 
 select
