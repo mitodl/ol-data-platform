f698f90e3ec3139396fac5b66ef5d95c60f35024
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-04-12 11:15:24 -0400

    Update the Micromasters Summary Mart (#1071)
    Update the logic for the Micromasters Summary Mart to remove revoked certificates, update logic, and combine programs that are reported on the same way. Also made updates to filter revoked certificates from int__micromasters__program_certificates and int__micromasters__course_certificates and add a new dedp indicator to int__micromasters__program_certificates, int__micromasters__course_certificates, and int__micromasters__course_enrollments.

--- src/ol_dbt/models/intermediate/micromasters/int__micromasters__program_certificates.sql
+++ src/ol_dbt/models/intermediate/micromasters/int__micromasters__program_certificates.sql
@@ -95,24 +95,25 @@ with program_certificates_dedp_from_micromasters as (
 
 
 select
-    program_certificate_hashed_id
-    , user_edxorg_username
-    , user_mitxonline_username
-    , user_email
-    , program_title
-    , micromasters_program_id
-    , mitxonline_program_id
-    , user_edxorg_id
-    , program_completion_timestamp
-    , user_gender
-    , user_address_city
-    , user_first_name
-    , user_last_name
-    , user_full_name
-    , user_year_of_birth
-    , user_country
-    , user_address_postal_code
-    , user_street_address
-    , user_address_state_or_territory
+    report.program_certificate_hashed_id
+    , report.user_edxorg_username
+    , report.user_mitxonline_username
+    , report.user_email
+    , report.program_title
+    , report.micromasters_program_id
+    , report.mitxonline_program_id
+    , report.user_edxorg_id
+    , report.program_completion_timestamp
+    , report.user_gender
+    , report.user_address_city
+    , report.user_first_name
+    , report.user_last_name
+    , report.user_full_name
+    , report.user_year_of_birth
+    , report.user_country
+    , report.user_address_postal_code
+    , report.user_street_address
+    , report.user_address_state_or_territory
+    , if(report.mitxonline_program_id in (1, 2, 3), true, false) as program_is_dedp
 from report
-order by program_completion_timestamp desc
+order by report.program_completion_timestamp desc
