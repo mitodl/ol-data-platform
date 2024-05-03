b13462db8173163a8373780c0eff9b4c66cf73a8
Rachel Lougee <rlougee@mit.edu>
2024-05-03 16:43:15 -0400

    add edx.org course structure staging model

---
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__course_structure.sql
@@ -0,0 +1,33 @@
+with course_block_source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__edxorg__s3__course_structure__course_blocks') }}
+)
+
+, course_block as (
+    select * from (
+        select
+            *
+            , lag(
+                course_content_hash) over
+            (partition by block_id, course_id order by retrieved_at asc) as previous_content_hash
+        from course_block_source
+    )
+    where previous_content_hash is null or previous_content_hash != course_content_hash
+)
+
+, cleaned as (
+    select
+        course_title as courserun_title
+        , block_index as coursestructure_block_index
+        , block_id as coursestructure_block_id
+        , block_parent as coursestructure_parent_block_id
+        , block_type as coursestructure_block_category
+        , block_title as coursestructure_block_title
+        , course_content_hash as coursestructure_content_hash
+        , block_content_hash as coursestructure_block_content_hash
+        , replace(element_at(course_id, 1), '-', '/') as courserun_readable_id
+        , json_query(block_details, 'lax $.metadata') as coursestructure_block_metadata
+        , {{ cast_timestamp_to_iso8601('retrieved_at') }} as coursestructure_retrieved_at
+    from course_block
+)
+
+select * from cleaned
