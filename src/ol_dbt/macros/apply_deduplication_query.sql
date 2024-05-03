4bc96b31019ced56d7b524f6c42921a147f92117
Rachel Lougee <rlougee@mit.edu>
2024-05-03 12:23:26 -0400

    staging edxorg course enrollment and grade (#1107)
    * staging edxorg course enrollment and grade
    
    * update description

--- src/ol_dbt/macros/apply_deduplication_query.sql
+++ src/ol_dbt/macros/apply_deduplication_query.sql
@@ -1,15 +1,15 @@
-{% macro deduplicate_query(cte_name1='source', cte_name2='most_recent_source') %}
+{% macro deduplicate_query(cte_name1='source', cte_name2='most_recent_source', partition_columns='id') %}
     /*
         Add additional queries to handle duplicated data introduced by airbyte sync mode "Incremental Sync - Append"
         where cursor is set to a timestamp field. The deduplication logic works like this:
-        - If there are multiple copies of the same record based on primary key id, we will use the record from most
+        - If there are multiple copies of the same record based on partition_columns, we will use the record from most
           recent sync in the source table.
-        - If there is only one record for the same primary key id, it will just load that record as it is.
+        - If there is only one record for the same partition_columns, it will just load that record as it is.
     */
    , source_sorted as (
         select
             *
-            , row_number() over ( partition by id order by _airbyte_emitted_at desc) as row_num
+            , row_number() over ( partition by {{ partition_columns }} order by _airbyte_emitted_at desc) as row_num
         from {{ cte_name1 }}
     )
 
