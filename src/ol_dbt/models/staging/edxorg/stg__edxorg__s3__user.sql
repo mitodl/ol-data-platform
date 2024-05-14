98e523fc20787bc83b78dd057d0fd02867ba0d39
Rachel Lougee <rlougee@mit.edu>
2024-05-14 15:08:57 -0400

    staging edx.org users, profiles and certificate models

---
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__user.sql
@@ -0,0 +1,28 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__edxorg__s3__tables__auth_user') }}
+)
+
+{{ deduplicate_query(cte_name1='source', cte_name2='most_recent_source' , partition_columns = 'id') }}
+
+, cleaned as (
+    select
+        ---all values are ingested as string, so we need to cast here to match other data sources
+        cast(id as integer) as user_id
+        , username as user_username
+        , email as user_email
+        , cast(is_active as boolean) as user_is_active
+        , cast(is_staff as boolean) as user_is_staff
+        , cast(is_superuser as boolean) as user_is_superuser
+        , to_iso8601(from_iso8601_timestamp_nanos(
+            regexp_replace(date_joined, '(\d{4}-\d{2}-\d{2})[ ](\d{2}:\d{2}:\d{2})(.*?)', '$1T$2$3')
+        )) as user_joined_on
+        , case
+            when lower(last_login) = 'null' then null
+            else to_iso8601(from_iso8601_timestamp_nanos(
+                regexp_replace(last_login, '(\d{4}-\d{2}-\d{2})[ ](\d{2}:\d{2}:\d{2})(.*?)', '$1T$2$3')
+            ))
+        end as user_last_login
+    from most_recent_source
+)
+
+select * from cleaned
