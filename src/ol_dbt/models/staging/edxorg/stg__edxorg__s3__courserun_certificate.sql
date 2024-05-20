6862251a866cbfc855ac9f952bbde6a0e7a51ff5
Rachel Lougee <rlougee@mit.edu>
2024-05-20 11:50:57 -0400

    fix dbt failure (#1135)
    * fix dbt failure
    
    * fix dbt failure

--- src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__courserun_certificate.sql
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__courserun_certificate.sql
@@ -18,8 +18,14 @@ with source as (
         , verify_uuid as courseruncertificate_verify_uuid
         , status as courseruncertificate_status
         , try_cast(grade as decimal(38, 2)) as courseruncertificate_grade
-        , to_iso8601(date_parse(created_date, '%Y-%m-%d %H:%i:%s')) as courseruncertificate_created_on
-        , to_iso8601(date_parse(modified_date, '%Y-%m-%d %H:%i:%s')) as courseruncertificate_updated_on
+        , case
+            when lower(created_date) = 'null' or lower(created_date) = 'none' then null
+            else to_iso8601(date_parse(created_date, '%Y-%m-%d %H:%i:%s'))
+        end as courseruncertificate_created_on
+        , case
+            when lower(modified_date) = 'null' or lower(modified_date) = 'none' then null
+            else to_iso8601(date_parse(modified_date, '%Y-%m-%d %H:%i:%s'))
+        end as courseruncertificate_updated_on
     from most_recent_source
 )
 
