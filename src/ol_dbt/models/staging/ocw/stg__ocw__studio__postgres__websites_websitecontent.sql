19e19466420fc526478e81e50c3862b676b40488
pt2302 <1553279+pt2302@users.noreply.github.com>
2024-05-27 22:16:22 -0400

    Adding image and video metadata fields

--- src/ol_dbt/models/staging/ocw/stg__ocw__studio__postgres__websites_websitecontent.sql
+++ src/ol_dbt/models/staging/ocw/stg__ocw__studio__postgres__websites_websitecontent.sql
@@ -42,6 +42,17 @@ with source as (
         , {{ cast_timestamp_to_iso8601('deleted') }} as websitecontent_deleted_on
         , {{ cast_timestamp_to_iso8601('created_on') }} as websitecontent_created_on
         , {{ cast_timestamp_to_iso8601('updated_on') }} as websitecontent_updated_on
+        -- image_metadata for image resources
+        , json_query(metadata, 'lax $.image_metadata') as image_metadata
+        , json_query(image_metadata, 'lax $.credit' omit quotes) as image_credit
+        , json_query(image_metadata, 'lax $.caption' omit quotes) as image_caption
+        , json_query(image_metadata, 'lax $.alt_text' omit quotes) as image_alt_text
+        -- video_metadata for video resources
+        , json_query(metadata, 'lax $.video_metadata') as video_metadata
+        , json_query(video_metadata, 'lax $.youtube_id' omit quotes) as video_youtube_id
+        , json_query(video_metadata, 'lax $.youtube_description' omit quotes) as video_youtube_description
+        , json_query(video_metadata, 'lax $.video_speakers' omit quotes) as video_youtube_speakers
+        , json_query(video_metadata, 'lax $.video_tags' omit quotes) as video_youtube_tags
 
     from source
 
