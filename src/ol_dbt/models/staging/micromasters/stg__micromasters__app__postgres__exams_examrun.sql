5a249929d5e3c9527a7851e5ce21243655944ba7
Rachel Lougee <rlougee@mit.edu>
2024-06-07 11:53:01 -0400

    Stage MicroMasters examruns and update intermediate proctored exam grades

---
+++ src/ol_dbt/models/staging/micromasters/stg__micromasters__app__postgres__exams_examrun.sql
@@ -0,0 +1,29 @@
+with source as (
+
+    select * from {{ source('ol_warehouse_raw_data', 'raw__micromasters__app__postgres__exams_examrun') }}
+
+)
+
+, renamed as (
+
+    select
+        id as examrun_id
+        , course_id
+        , exam_series_code
+        , edx_exam_course_key as examrun_readable_id
+        , semester as examrun_semester
+        , description as examrun_description
+        , authorized as examrun_is_authorized
+        , passing_score as examrun_passing_score
+        ,{{ cast_timestamp_to_iso8601('date_first_schedulable') }} as examrun_first_schedulable_on
+        ,{{ cast_timestamp_to_iso8601('date_last_schedulable') }} as examrun_last_schedulable_on
+        ,{{ cast_timestamp_to_iso8601('date_first_eligible') }} as examrun_first_eligible_on
+        ,{{ cast_timestamp_to_iso8601('date_first_eligible') }} as examrun_last_eligible_on
+        ,{{ cast_timestamp_to_iso8601('date_grades_available') }} as examrun_grades_available_on
+        ,{{ cast_timestamp_to_iso8601('created_on') }} as examrun_created_on
+        ,{{ cast_timestamp_to_iso8601('updated_on') }} as examrun_updated_on
+    from source
+
+)
+
+select * from renamed
