69ad707491f8b97a43c3eb5f65621c7923a4c477
Rachel Lougee <rlougee@mit.edu>
2024-06-10 14:09:34 -0400

    Changes to int__micromasters__dedp_proctored_exam_grades and upstream models (#1160)
    * Stage MicroMasters examruns and update intermediate proctored exam grades
    
    * updates to staging
    
    * updates

--- src/ol_dbt/models/intermediate/micromasters/int__micromasters__dedp_proctored_exam_grades.sql
+++ src/ol_dbt/models/intermediate/micromasters/int__micromasters__dedp_proctored_exam_grades.sql
@@ -3,6 +3,10 @@ with exam_grades as (
     from {{ ref('stg__micromasters__app__postgres__grades_proctoredexamgrade') }}
 )
 
+, examruns as (
+    select * from {{ ref('stg__micromasters__app__postgres__exams_examrun') }}
+)
+
 , programs as (
     select * from {{ ref('int__mitx__programs') }}
 )
@@ -11,7 +15,7 @@ with exam_grades as (
     select * from {{ ref('stg__micromasters__app__postgres__courses_course') }}
 )
 
-, mm_users as (
+, micromasters_users as (
     select * from {{ ref('__micromasters__users') }}
 )
 
@@ -25,21 +29,24 @@ select
     , programs.mitxonline_program_id
     , courses.course_title
     , courses.course_number
-    , mm_users.user_edxorg_username
-    , mm_users.user_mitxonline_username
-    , mm_users.user_full_name
-    , mm_users.user_email as user_micromasters_email
+    , examruns.examrun_readable_id
+    , examruns.examrun_semester
+    , micromasters_users.user_edxorg_username
+    , micromasters_users.user_mitxonline_username
+    , micromasters_users.user_full_name
+    , micromasters_users.user_email as user_micromasters_email
     , mixonline_users.user_email as user_mitxonline_email
-    , exam_grades.proctoredexamgrade_score
-    , exam_grades.proctoredexamgrade_is_passing
     , exam_grades.proctoredexamgrade_passing_score
+    , exam_grades.proctoredexamgrade_score
     , exam_grades.proctoredexamgrade_percentage_grade
+    , exam_grades.proctoredexamgrade_is_passing
     , exam_grades.proctoredexamgrade_exam_on
     , exam_grades.proctoredexamgrade_created_on
     , exam_grades.proctoredexamgrade_updated_on
     , exam_grades.proctoredexamgrade_id
 from exam_grades
+inner join examruns on exam_grades.examrun_id = examruns.examrun_id
 inner join courses on exam_grades.course_id = courses.course_id
 inner join programs on courses.program_id = programs.micromasters_program_id
-inner join mm_users on exam_grades.user_id = mm_users.user_id
-left join mixonline_users on mm_users.user_mitxonline_username = mixonline_users.user_username
+inner join micromasters_users on exam_grades.user_id = micromasters_users.user_id
+left join mixonline_users on micromasters_users.user_mitxonline_username = mixonline_users.user_username
---
+++ src/ol_dbt/models/staging/micromasters/stg__micromasters__app__postgres__exams_examrun.sql
@@ -0,0 +1,24 @@
+with source as (
+
+    select * from {{ source('ol_warehouse_raw_data', 'raw__micromasters__app__postgres__exams_examrun') }}
+
+)
+
+, renamed as (
+
+    select
+        id as examrun_id
+        , course_id
+        , exam_series_code
+        , edx_exam_course_key as examrun_readable_id
+        , semester as examrun_semester
+        , description as examrun_description
+        , authorized as examrun_is_authorized
+        , passing_score * 100 as examrun_passing_score
+        ,{{ cast_timestamp_to_iso8601('created_on') }} as examrun_created_on
+        ,{{ cast_timestamp_to_iso8601('updated_on') }} as examrun_updated_on
+    from source
+
+)
+
+select * from renamed
