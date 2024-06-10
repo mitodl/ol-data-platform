a92088e06ee0a097d957f251078f53c9184f0150
pt2302 <1553279+pt2302@users.noreply.github.com>
2024-06-09 23:26:03 -0400

    Updating intermediate OCW resources model, including OCW Studio URL

--- src/ol_dbt/models/intermediate/ocw/int__ocw__resources.sql
+++ src/ol_dbt/models/intermediate/ocw/int__ocw__resources.sql
@@ -14,8 +14,18 @@ select
     websites.website_uuid as course_uuid
     , websites.website_name as course_name
     , websites.website_title as course_title
-    , websitecontents.websitecontent_type as resource_type
+    , websitecontents.metadata_resource_type as resource_type
     , websitecontents.websitecontent_text_id as resource_uuid
+    , websitecontents.websitecontent_filename as resource_filename
+    , websitecontents.metadata_draft as resource_draft
+    , websites.primary_course_number as course_number
+    , 'https://ocw-studio.odl.mit.edu/sites/'
+    || websites.website_name
+    || '/type/'
+    || websitecontents.websitecontent_type
+    || '/edit/'
+    || websitecontents.websitecontent_text_id
+    || '/' as studio_url
 
 from websites
 inner join websitecontents
@@ -23,5 +33,5 @@ inner join websitecontents
 inner join websitestarters
     on websites.websitestarter_id = websitestarters.websitestarter_id
 where
-    websitecontents.websitecontent_type not in ('navmenu', 'sitemetadata')
+    websitecontents.websitecontent_type = 'resource'
     and websitestarters.websitestarter_name = 'ocw-course'
