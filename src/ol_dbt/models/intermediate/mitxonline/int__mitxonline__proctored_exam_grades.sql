0f5f02317e2866ca0dcfc59f9ea6bb8d74269ecd
Rachel Lougee <rlougee@mit.edu>
2024-06-12 09:08:08 -0400

    Changes to dedp proctored exam grades mart and intermidiate

---
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__proctored_exam_grades.sql
@@ -0,0 +1,86 @@
+with exam_unit_grades as (
+    select * from {{ ref('int__mitxonline__courserun_subsection_grades') }}
+    where
+        lower(coursestructure_block_title) = 'proctored exam'
+        and subsectiongrade_total_graded_score > 0
+)
+
+, exam_courserun_grades as (
+    select * from {{ ref('int__mitxonline__courserun_grades') }}
+    where lower(courserun_title) like '%proctored exam'
+)
+
+, exam_courseruns_from_micromasters as (
+    --- pulling exam courseruns from MicroMasters to associate a exam run with a course
+    --- since all these MITxT exam runs and their associations to courses exist in MicroMasters.
+    select
+        examrun_readable_id
+        , examrun_semester
+        , course_id
+    from {{ ref('stg__micromasters__app__postgres__exams_examrun') }}
+)
+
+, courses_from_micromasters as (
+    select * from {{ ref('stg__micromasters__app__postgres__courses_course') }}
+)
+
+, courseruns_from_mitxonline as (
+    select * from {{ ref('stg__mitxonline__app__postgres__courses_courserun') }}
+)
+
+, courses_from_mitxonline as (
+    select * from {{ ref('stg__mitxonline__app__postgres__courses_course') }}
+)
+
+, users as (
+    select * from {{ ref('int__mitxonline__users') }}
+)
+
+
+select
+    courses_from_mitxonline.course_number
+    , courses_from_mitxonline.course_title
+    , exam_courseruns_from_micromasters.examrun_semester as semester
+    , exam_courserun_grades.courserun_readable_id
+    , users.user_id
+    , users.openedx_user_id
+    , users.user_edxorg_username
+    , users.user_username
+    , users.user_full_name
+    , users.user_email
+    , exam_courserun_grades.courserungrade_grade as proctoredexamgrade_grade
+    , exam_courserun_grades.courserungrade_created_on as proctoredexamgrade_created_on
+from exam_courserun_grades
+inner join users on exam_courserun_grades.user_id = users.user_id
+left join exam_courseruns_from_micromasters
+    on exam_courserun_grades.courserun_readable_id = exam_courseruns_from_micromasters.examrun_readable_id
+left join courses_from_micromasters on exam_courseruns_from_micromasters.course_id = courses_from_micromasters.course_id
+left join courses_from_mitxonline on courses_from_micromasters.course_number = courses_from_mitxonline.course_number
+
+union all
+
+select
+    courses_from_mitxonline.course_number
+    , courses_from_mitxonline.course_title
+    , courseruns_from_mitxonline.courserun_tag as semester
+    , exam_unit_grades.courserun_readable_id
+    , users.user_id
+    , users.openedx_user_id
+    , users.user_edxorg_username
+    , users.user_username
+    , users.user_full_name
+    , users.user_email
+    , exam_unit_grades.subsectiongrade_total_earned_graded_score / exam_unit_grades.subsectiongrade_total_graded_score
+    as proctoredexamgrade_grade
+    , exam_unit_grades.subsectiongrade_created_on as proctoredexamgrade_created_on
+from exam_unit_grades
+inner join users on exam_unit_grades.openedx_user_id = users.openedx_user_id
+inner join
+    courseruns_from_mitxonline
+    on exam_unit_grades.courserun_readable_id = courseruns_from_mitxonline.courserun_readable_id
+inner join courses_from_mitxonline on courseruns_from_mitxonline.course_id = courses_from_mitxonline.course_id
+left join exam_courserun_grades
+    on
+        users.user_username = exam_courserun_grades.user_username
+        and exam_unit_grades.courserun_readable_id = exam_courserun_grades.courserun_readable_id
+where exam_courserun_grades.courserungrade_id is null
--- src/ol_dbt/models/marts/micromasters/marts__micromasters_dedp_exam_grades.sql
+++ src/ol_dbt/models/marts/micromasters/marts__micromasters_dedp_exam_grades.sql
@@ -1,6 +1,48 @@
-with exam_grades as (
+with micromasters_exam_grades as (
     select * from {{ ref('int__micromasters__dedp_proctored_exam_grades') }}
 )
 
-select *
-from exam_grades
+, mitxonline_exam_grades as (
+    select * from {{ ref('int__mitxonline__proctored_exam_grades') }}
+)
+
+, micromasters_users as (
+    select * from {{ ref('int__micromasters__users') }}
+)
+
+select
+    course_number
+    , course_title
+    , examrun_readable_id as examrun_courserun_readable_id
+    , user_edxorg_username
+    , user_mitxonline_username
+    , user_full_name
+    , user_micromasters_email
+    , user_mitxonline_email
+    , proctoredexamgrade_percentage_grade
+    , proctoredexamgrade_created_on
+    , examrun_semester as semester
+
+from micromasters_exam_grades
+
+union all
+
+select
+    mitxonline_exam_grades.course_number
+    , mitxonline_exam_grades.course_title
+    , mitxonline_exam_grades.courserun_readable_id as examrun_courserun_readable_id
+    , mitxonline_exam_grades.user_edxorg_username
+    , mitxonline_exam_grades.user_username as user_mitxonline_username
+    , mitxonline_exam_grades.user_full_name
+    , micromasters_users.user_email as user_micromasters_email
+    , mitxonline_exam_grades.user_email as user_mitxonline_email
+    , mitxonline_exam_grades.proctoredexamgrade_grade as proctoredexamgrade_percentage_grade
+    , mitxonline_exam_grades.proctoredexamgrade_created_on
+    , mitxonline_exam_grades.semester
+from mitxonline_exam_grades
+left join micromasters_users on mitxonline_exam_grades.user_username = micromasters_users.user_mitxonline_username
+left join micromasters_exam_grades
+    on
+        mitxonline_exam_grades.courserun_readable_id = micromasters_exam_grades.examrun_readable_id
+        and mitxonline_exam_grades.user_username = micromasters_exam_grades.user_mitxonline_username
+where micromasters_exam_grades.user_mitxonline_username is null and micromasters_exam_grades.examrun_readable_id is null
