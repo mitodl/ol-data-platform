264697127747ae371f0f45db8e1aa3ac4886d2ed
Rachel Lougee <rlougee@mit.edu>
2024-06-18 11:57:28 -0400

    update enrollment detail mart

--- src/ol_dbt/models/marts/combined/marts__combined_course_enrollment_detail.sql
+++ src/ol_dbt/models/marts/combined/marts__combined_course_enrollment_detail.sql
@@ -43,11 +43,6 @@ with combined_enrollments as (
     where order_state in ('fulfilled', 'refunded')
 )
 
----Unlike other platforms, xPro lines and orders are not 1 to 1, so we can't combined them into one table
-, mitxpro_lines as (
-    select * from {{ ref('int__mitxpro__ecommerce_line') }}
-)
-
 , bootcamps_completed_orders as (
     select * from {{ ref('int__bootcamps__ecommerce_order') }}
     where order_state in ('fulfilled', 'refunded')
@@ -80,30 +75,7 @@ with combined_enrollments as (
         , combined_enrollments.courseruncertificate_url
         , combined_enrollments.courseruncertificate_uuid
         , mitxonline_completed_orders.order_id
-        , mitxonline_completed_orders.order_state
         , mitxonline_completed_orders.order_reference_number
-        , mitxonline_completed_orders.order_created_on
-        , mitxonline_completed_orders.discount_source as payment_type
-        , mitxonline_completed_orders.discount_redemption_type as coupon_type
-        , mitxonline_completed_orders.discount_code as coupon_code
-        , mitxonline_completed_orders.discountredemption_timestamp as coupon_redeemed_on
-        , mitxonline_completed_orders.payment_transaction_id
-        , mitxonline_completed_orders.payment_authorization_code
-        , mitxonline_completed_orders.payment_method
-        , mitxonline_completed_orders.payment_req_reference_number
-        , mitxonline_completed_orders.payment_bill_to_address_state
-        , mitxonline_completed_orders.payment_bill_to_address_country
-        , null as order_tax_country_code
-        , null as order_tax_rate
-        , null as order_tax_rate_name
-        , mitxonline_completed_orders.product_price as unit_price
-        , mitxonline_completed_orders.order_total_price_paid
-        , null as order_tax_amount
-        , mitxonline_completed_orders.order_total_price_paid as order_total_price_paid_plus_tax
-        , mitxonline_completed_orders.product_id
-        , mitxonline_completed_orders.program_readable_id as product_program_readable_id
-        , mitxonline_completed_orders.discount_amount_text as discount
-        , mitxonline_completed_orders.discount_amount
         , combined_enrollments.courserungrade_grade
         , combined_enrollments.courserungrade_is_passing
         , combined_courseruns.course_title
@@ -151,30 +123,7 @@ with combined_enrollments as (
         , combined_enrollments.courseruncertificate_url
         , combined_enrollments.courseruncertificate_uuid
         , micromasters_completed_orders.order_id
-        , micromasters_completed_orders.order_state
         , micromasters_completed_orders.order_reference_number
-        , micromasters_completed_orders.order_created_on
-        , null as payment_type
-        , micromasters_completed_orders.coupon_type
-        , micromasters_completed_orders.coupon_code
-        , micromasters_completed_orders.redeemedcoupon_created_on as coupon_redeemed_on
-        , micromasters_completed_orders.receipt_transaction_id as payment_transaction_id
-        , micromasters_completed_orders.receipt_authorization_code as payment_authorization_code
-        , micromasters_completed_orders.receipt_payment_method as payment_method
-        , micromasters_completed_orders.receipt_reference_number as payment_req_reference_number
-        , micromasters_completed_orders.receipt_bill_to_address_state as payment_bill_to_address_state
-        , micromasters_completed_orders.receipt_bill_to_address_country as payment_bill_to_address_country
-        , null as order_tax_country_code
-        , null as order_tax_rate
-        , null as order_tax_rate_name
-        , micromasters_completed_orders.line_price as unit_price
-        , micromasters_completed_orders.order_total_price_paid
-        , null as order_tax_amount
-        , micromasters_completed_orders.order_total_price_paid as order_total_price_paid_plus_tax
-        , null as product_id
-        , null as product_program_readable_id
-        , micromasters_completed_orders.coupon_discount_amount_text as discount
-        , micromasters_completed_orders.coupon_amount as discount_amount
         , combined_enrollments.courserungrade_grade
         , combined_enrollments.courserungrade_is_passing
         , combined_courseruns.course_title
@@ -224,34 +173,7 @@ with combined_enrollments as (
         , combined_enrollments.courseruncertificate_url
         , combined_enrollments.courseruncertificate_uuid
         , mitxpro_completed_orders.order_id
-        , mitxpro_completed_orders.order_state
         , mitxpro_completed_orders.receipt_reference_number as order_reference_number
-        , mitxpro_completed_orders.order_created_on
-        , mitxpro_completed_orders.couponpaymentversion_discount_source as payment_type
-        , mitxpro_completed_orders.couponpaymentversion_coupon_type as coupon_type
-        , mitxpro_completed_orders.coupon_code
-        , mitxpro_completed_orders.couponredemption_created_on as coupon_redeemed_on
-        , mitxpro_completed_orders.receipt_transaction_id as payment_transaction_id
-        , mitxpro_completed_orders.receipt_authorization_code as payment_authorization_code
-        , mitxpro_completed_orders.receipt_payment_method as payment_method
-        , mitxpro_completed_orders.receipt_reference_number as payment_req_reference_number
-        , mitxpro_completed_orders.receipt_bill_to_address_state as payment_bill_to_address_state
-        , mitxpro_completed_orders.receipt_bill_to_address_country as payment_bill_to_address_country
-        , mitxpro_completed_orders.order_tax_country_code
-        , mitxpro_completed_orders.order_tax_rate
-        , mitxpro_completed_orders.order_tax_rate_name
-        , mitxpro_lines.product_price as unit_price
-        , mitxpro_completed_orders.order_total_price_paid
-        , mitxpro_completed_orders.order_tax_amount
-        , mitxpro_completed_orders.order_total_price_paid_plus_tax
-        , mitxpro_lines.product_id
-        , mitxpro_lines.program_readable_id as product_program_readable_id
-        , mitxpro_completed_orders.couponpaymentversion_discount_amount_text as discount
-        , case
-            when mitxpro_completed_orders.couponpaymentversion_discount_type = 'percent-off'
-                then mitxpro_lines.product_price * mitxpro_completed_orders.couponpaymentversion_discount_amount
-            else mitxpro_completed_orders.couponpaymentversion_discount_amount
-        end as discount_amount
         , combined_enrollments.courserungrade_grade
         , combined_enrollments.courserungrade_is_passing
         , combined_courseruns.course_title
@@ -265,8 +187,6 @@ with combined_enrollments as (
             and combined_enrollments.platform = combined_users.platform
     left join mitxpro_completed_orders
         on mitxpro_enrollments.ecommerce_order_id = mitxpro_completed_orders.order_id
-    left join mitxpro_lines
-        on mitxpro_completed_orders.order_id = mitxpro_lines.order_id
     left join combined_courseruns
         on mitxpro_enrollments.courserun_readable_id = combined_courseruns.courserun_readable_id
     where combined_enrollments.platform = '{{ var("mitxpro") }}'
@@ -300,30 +220,7 @@ with combined_enrollments as (
         , combined_enrollments.courseruncertificate_url
         , combined_enrollments.courseruncertificate_uuid
         , bootcamps_completed_orders.order_id
-        , bootcamps_completed_orders.order_state
         , bootcamps_completed_orders.order_reference_number
-        , bootcamps_completed_orders.order_created_on
-        , null as payment_type
-        , null as coupon_type
-        , null as coupon_code
-        , null as coupon_redeemed_on
-        , bootcamps_completed_orders.receipt_transaction_id as payment_transaction_id
-        , bootcamps_completed_orders.receipt_authorization_code as payment_authorization_code
-        , bootcamps_completed_orders.receipt_payment_method as payment_method
-        , bootcamps_completed_orders.receipt_reference_number as payment_req_reference_number
-        , bootcamps_completed_orders.receipt_bill_to_address_state as payment_bill_to_address_state
-        , bootcamps_completed_orders.receipt_bill_to_address_country as payment_bill_to_address_country
-        , null as order_tax_country_code
-        , null as order_tax_rate
-        , null as order_tax_rate_name
-        , bootcamps_completed_orders.line_price as unit_price
-        , bootcamps_completed_orders.order_total_price_paid
-        , null as order_tax_amount
-        , bootcamps_completed_orders.order_total_price_paid as order_total_price_paid_plus_tax
-        , null as product_id
-        , null as product_program_readable_id
-        , null as discount
-        , null as discount_amount
         , combined_enrollments.courserungrade_grade
         , combined_enrollments.courserungrade_is_passing
         , combined_courseruns.course_title
@@ -345,16 +242,13 @@ with combined_enrollments as (
 select
     platform
     , courserunenrollment_id
-    , coupon_code
-    , coupon_redeemed_on
-    , coupon_type
     , course_readable_id
     , course_title
-    , courserun_end_on
     , courserun_id
     , courserun_is_current
     , courserun_readable_id
     , courserun_start_on
+    , courserun_end_on
     , courserun_title
     , courseruncertificate_created_on
     , courseruncertificate_is_earned
@@ -367,18 +261,8 @@ select
     , courserunenrollment_is_edx_enrolled
     , courserungrade_grade
     , courserungrade_is_passing
-    , discount
-    , discount_amount
     , order_id
     , order_reference_number
-    , payment_bill_to_address_country
-    , payment_bill_to_address_state
-    , payment_method
-    , payment_transaction_id
-    , payment_type
-    , product_id
-    , product_program_readable_id
-    , unit_price
     , user_company
     , user_country_code
     , user_email
