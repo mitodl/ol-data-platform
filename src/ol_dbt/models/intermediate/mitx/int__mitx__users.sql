6a7f863c783970003fd93746c5044eb1cf2a6704
Rachel Lougee <rlougee@mit.edu>
2024-06-25 13:33:40 -0400

    augment int__mitx__users with micromasters profile data

--- src/ol_dbt/models/intermediate/mitx/int__mitx__users.sql
+++ src/ol_dbt/models/intermediate/mitx/int__mitx__users.sql
@@ -42,10 +42,6 @@ with mitxonline_users as (
         , edxorg_users.user_username as user_edxorg_username
         , edxorg_users.user_email as user_edxorg_email
         , edxorg_users.user_full_name
-        , edxorg_users.user_country as user_address_country
-        , edxorg_users.user_highest_education
-        , edxorg_users.user_gender
-        , edxorg_users.user_birth_year
         , edxorg_users.user_joined_on
         , edxorg_users.user_last_login
         , edxorg_users.user_is_active
@@ -53,6 +49,14 @@ with mitxonline_users as (
         , micromasters_users.user_company_industry
         , micromasters_users.user_job_position
         , true as is_edxorg_user
+        , coalesce(micromasters_users.user_address_country, edxorg_users.user_country) as user_address_country
+        , coalesce(
+            micromasters_users.user_highest_education, edxorg_users.user_highest_education
+        ) as user_highest_education
+        , coalesce(micromasters_users.user_gender, edxorg_users.user_gender) as user_gender
+        , coalesce(
+            cast(substring(micromasters_users.user_birth_date, 1, 4) as int), edxorg_users.user_birth_year
+        ) as user_birth_year
     from edxorg_users
     left join micromasters_users on edxorg_users.user_username = micromasters_users.user_edxorg_username
 )
