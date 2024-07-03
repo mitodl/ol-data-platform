c65fa1cdf05abd611be1ada12cd2c6e0295a4c9a
Rachel Lougee <rlougee@mit.edu>
2024-07-03 12:09:46 -0400

    add cybersource fields to mitxonline int transaction model

--- src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__ecommerce_transaction.sql
+++ src/ol_dbt/models/intermediate/mitxonline/int__mitxonline__ecommerce_transaction.sql
@@ -4,17 +4,47 @@ with transactions as (
 )
 
 select
-    transactions.transaction_id
+    transaction_id
     , transaction_data
-    , transactions.transaction_amount
-    , transactions.order_id
-    , transactions.transaction_created_on
-    , transactions.transaction_readable_identifier
-    , transactions.transaction_type
+    , transaction_amount
+    , order_id
+    , transaction_created_on
+    , transaction_readable_identifier
+    , transaction_type
     , transaction_status
     , transaction_authorization_code
     , transaction_payment_method
     , transaction_reference_number
     , transaction_bill_to_address_state
     , transaction_bill_to_address_country
+    , json_query(transaction_data, 'lax $.req_transaction_type' omit quotes) as transaction_payment_type
+    , concat(
+        json_query(transaction_data, 'lax $.req_bill_to_forename' omit quotes)
+        , ' '
+        , json_query(transaction_data, 'lax $.req_bill_to_surname' omit quotes)
+    ) as transaction_payer
+    , case
+        when transaction_type = 'payment'
+            then json_query(transaction_data, 'lax $.req_transaction_uuid' omit quotes)
+        when transaction_type = 'refund'
+            then json_query(transaction_data, 'lax $.clientReferenceInformation.transactionId' omit quotes)
+    end as transaction_uuid
+    , case
+        when transaction_type = 'payment'
+            then json_query(transaction_data, 'lax $.auth_code' omit quotes)
+        when transaction_type = 'refund'
+            then json_query(transaction_data, 'lax $.processorInformation.approvalCode' omit quotes)
+    end as transaction_auth_approval_code
+    , case
+        when transaction_type = 'payment'
+            then json_query(transaction_data, 'lax $.signed_date_time' omit quotes)
+        when transaction_type = 'refund'
+            then json_query(transaction_data, 'lax $.submitTimeUtc' omit quotes)
+    end as transaction_timestamp
+    , case
+        when transaction_type = 'payment'
+            then json_query(transaction_data, 'lax $.req_amount' omit quotes)
+        when transaction_type = 'refund'
+            then json_query(transaction_data, 'lax $.refundAmountDetails.refundAmount' omit quotes)
+    end as transaction_req_refund_amount
 from transactions
