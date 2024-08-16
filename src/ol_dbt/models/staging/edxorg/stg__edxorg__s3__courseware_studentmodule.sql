52a3dfde1a71c9f6bfb88afd8a77944364eda7ac
Rachel Lougee <rlougee@mit.edu>
2024-08-16 15:03:40 -0400

    Stage edxorg studentmodule table (#1237)
    * stage edxorg studentmudule
    
    * data clean and transform to appropriate data type
    
    * update description
    
    * update description

---
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__courseware_studentmodule.sql
@@ -0,0 +1,28 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__edxorg__s3__tables__courseware_studentmodule') }}
+)
+
+{{ deduplicate_query(
+   cte_name1='source',
+   cte_name2='most_recent_source' ,
+   partition_columns = 'course_id, student_id, module_id'
+   )
+}}
+
+, cleaned as (
+
+    select
+        cast(id as bigint) as studentmodule_id
+        , course_id as courserun_readable_id
+        , module_id as coursestructure_block_id
+        , module_type as coursestructure_block_category
+        , cast(student_id as integer) as user_id
+        , state as studentmodule_state_data
+        , try_cast(grade as double) as studentmodule_problem_grade
+        , try_cast(max_grade as double) as studentmodule_problem_max_grade
+        , to_iso8601(date_parse(created, '%Y-%m-%d %H:%i:%s')) as studentmodule_created_on
+        , to_iso8601(date_parse(modified, '%Y-%m-%d %H:%i:%s')) as studentmodule_updated_on
+    from most_recent_source
+)
+
+select * from cleaned
