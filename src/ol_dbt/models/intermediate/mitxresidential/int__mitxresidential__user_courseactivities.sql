58aaf52591331793eaf5587e0c8bcb78be6b4130
Rachel Lougee <rlougee@mit.edu>
2024-08-22 13:13:42 -0400

    Residential intermediate course activity modules

---
+++ src/ol_dbt/models/intermediate/mitxresidential/int__mitxresidential__user_courseactivities.sql
@@ -0,0 +1,94 @@
+with course_activities as (
+    select * from {{ ref('stg__mitxresidential__openedx__tracking_logs__user_activity') }}
+    where courserun_readable_id is not null
+)
+
+, course_activities_video as (
+    select * from {{ ref('int__mitxresidential__user_courseactivity_video') }}
+)
+
+, problem_check as (
+    select * from {{ ref('int__mitxresidential__user_courseactivity_problemcheck') }}
+)
+
+, course_state as (
+    select * from {{ ref('stg__mitxonline__openedx__mysql__courseware_studentmodule') }}
+)
+
+, course_chapters_stats as (
+    select
+        course_state.user_id
+        , course_state.courserun_readable_id
+        , count(distinct course_structure.coursestructure_chapter_id) as courseactivity_num_chapters_visited
+    from course_state
+    inner join course_structure
+        on
+            course_state.courserun_readable_id = course_structure.courserun_readable_id
+            and course_state.coursestructure_block_id = course_structure.coursestructure_block_id
+    group by course_state.user_id, course_state.courserun_readable_id
+)
+
+, problem_check_stats as (
+    select
+        user_username
+        , courserun_readable_id
+        , max(useractivity_timestamp) as courseactivity_last_problem_check_timestamp
+    from problem_check
+    group by user_username, courserun_readable_id
+)
+
+, play_video_stats as (
+    select
+        user_username
+        , courserun_readable_id
+        , count(distinct useractivity_video_id) as courseactivity_num_unique_play_video
+        , count(*) as courseactivity_num_play_video
+        , max(useractivity_timestamp) as courseactivity_last_play_video_timestamp
+    from course_activities_video
+    where useractivity_event_type = 'play_video'
+    group by user_username, courserun_readable_id
+)
+
+
+, all_course_activities_stats as (
+    select
+        course_activities.user_username
+        , course_activities.user_id
+        , course_activities.courserun_readable_id
+        , count(distinct date(from_iso8601_timestamp(course_activities.useractivity_timestamp)))
+        as courseactivity_num_days_activity
+        , count(*) as courseactivity_num_events
+        , min(course_activities.useractivity_timestamp) as courseactivity_first_event_timestamp
+        , max(course_activities.useractivity_timestamp) as courseactivity_last_event_timestamp
+    from course_activities
+    group by
+        course_activities.user_username
+        , course_activities.user_id
+        , course_activities.courserun_readable_id
+)
+
+select
+    all_course_activities_stats.user_username
+    , all_course_activities_stats.courserun_readable_id
+    , all_course_activities_stats.courseactivity_num_days_activity
+    , all_course_activities_stats.courseactivity_num_events
+    , play_video_stats.courseactivity_num_unique_play_video
+    , play_video_stats.courseactivity_num_play_video
+    , play_video_stats.courseactivity_last_play_video_timestamp
+    , problem_check_stats.courseactivity_last_problem_check_timestamp
+    , course_chapters_stats.courseactivity_num_chapters_visited
+    , all_course_activities_stats.courseactivity_first_event_timestamp
+    , all_course_activities_stats.courseactivity_last_event_timestamp
+from all_course_activities_stats
+left join play_video_stats
+    on
+        all_course_activities_stats.user_username = play_video_stats.user_username
+        and all_course_activities_stats.courserun_readable_id = play_video_stats.courserun_readable_id
+left join problem_check_stats
+    on
+        all_course_activities_stats.user_username = problem_check_stats.user_username
+        and all_course_activities_stats.courserun_readable_id = problem_check_stats.courserun_readable_id
+left join course_chapters_stats
+    on
+        all_course_activities_stats.user_id = course_chapters_stats.user_id
+        and all_course_activities_stats.courserun_readable_id = course_chapters_stats.courserun_readable_id
