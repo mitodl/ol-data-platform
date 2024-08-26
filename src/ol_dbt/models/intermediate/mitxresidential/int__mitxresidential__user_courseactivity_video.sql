7aff85ce914aad42fc361f8683a1e738ceca8efe
Rachel Lougee <rlougee@mit.edu>
2024-08-26 11:34:24 -0400

    Residential MITx staging and intermediate course engagement models (#1246)
    * stage Residential MITx tracking log table
    
    * stage Residential MITx student module table
    
    * Residential intermediate course activity modules
    
    * updates to fix failing tests
    
    * update to fix failing test
    
    * Illegal instant due to time zone offset transition error on staging enrollment

---
+++ src/ol_dbt/models/intermediate/mitxresidential/int__mitxresidential__user_courseactivity_video.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='view') }}
+
+with course_activities as (
+    select * from {{ ref('stg__mitxresidential__openedx__tracking_logs__user_activity') }}
+    where courserun_readable_id is not null
+)
+
+select
+    user_username
+    , courserun_readable_id
+    , user_id
+    , useractivity_event_source
+    , useractivity_event_type
+    , useractivity_event_object
+    , useractivity_page_url
+    , useractivity_timestamp
+    , json_query(useractivity_event_object, 'lax $.id' omit quotes) as useractivity_video_id
+    , json_query(useractivity_event_object, 'lax $.duration' omit quotes) as useractivity_video_duration
+    , json_query(useractivity_event_object, 'lax $.currentTime' omit quotes) as useractivity_video_currenttime
+    , json_query(useractivity_event_object, 'lax $.old_time' omit quotes) as useractivity_video_old_time
+    , json_query(useractivity_event_object, 'lax $.new_time' omit quotes) as useractivity_video_new_time
+    , json_query(useractivity_event_object, 'lax $.new_speed' omit quotes) as useractivity_video_new_speed
+    , json_query(useractivity_event_object, 'lax $.old_speed' omit quotes) as useractivity_video_old_speed
+from course_activities
+where
+    regexp_like(useractivity_event_type, '(^[\w]+)_video') = true
+    or regexp_like(useractivity_event_type, '(^[\w]+)_transcript') = true
+    or useractivity_event_type like 'edx.video.%'
