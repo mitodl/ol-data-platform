eddfb33f82b2e1e6c15c61d1e41e04a9f93d3451
Rachel Lougee <rlougee@mit.edu>
2024-09-11 14:39:16 -0400

    mitxonline coursepage and topics

---
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_coursetopic.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__courses_coursestopic') }}
+)
+
+, cleaned as (
+    select
+        id as coursetopic_id
+        , name as coursetopic_name
+        , parent_id as coursetopic_parent_id
+        ,{{ cast_timestamp_to_iso8601('created_on') }} as coursetopic_created_on
+        ,{{ cast_timestamp_to_iso8601('updated_on') }} as coursetopic_updated_on
+    from source
+)
+
+select * from cleaned
