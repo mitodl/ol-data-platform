08942e7f6dd334f0a32a4af1acc864ca7935bd65
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-09-17 09:53:03 -0400

    add coursesinprogram mart

---
+++ src/ol_dbt/models/marts/combined/marts__combined_coursesinprogram.sql
@@ -0,0 +1,43 @@
+with mitx_courserun_enroll_w_programs as (
+    select * from {{ ref('int__mitx__courserun_enrollments_with_programs') }}
+)
+
+, mitx__courses as (
+    select * from {{ ref('int__mitx__courses') }}
+)
+
+, mitxpro__courses as (
+    select * from {{ ref('int__mitxpro__courses') }}
+)
+
+, mitxpro__programs as (
+    select * from {{ ref('int__mitxpro__programs') }}
+)
+
+select 
+    platform
+    , mitx__courses.course_title
+    , mitx_courserun_enroll_w_programs.program_title
+    , case when mitx_courserun_enroll_w_programs.platform= 'MITx Online' 
+        then mitx__courses.mitxonline_course_id 
+        else null end as course_id
+    , case when mitx_courserun_enroll_w_programs.platform= 'MITx Online' 
+        then  mitx_courserun_enroll_w_programs.mitxonline_program_id 
+        else null end as program_id
+    , mitx__courses.course_readable_id
+from int__mitx__courserun_enrollments_with_programs mitx_courserun_enroll_w_programs
+inner join int__mitx__courses mitx__courses
+    on  mitx_courserun_enroll_w_programs.course_number = mitx__courses.course_number
+
+union all
+
+select
+    'xPRO' as platform
+    , mitx__courses.course_readable_id
+    , mitx__courses.course_title
+    , mitxpro__programs.program_title
+    , mitx__courses.course_id
+    , mitx__courses.program_id
+from mitx__courses 
+left join mitxpro__programs 
+    on courses.program_id=programs.program_id
