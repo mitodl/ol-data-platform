bccc3ecdd33643221007674cd25f08f866e5156a
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2024-09-18 15:28:53 -0400

    add program readable id

--- src/ol_dbt/models/marts/combined/marts__combined_coursesinprogram.sql
+++ src/ol_dbt/models/marts/combined/marts__combined_coursesinprogram.sql
@@ -14,30 +14,44 @@ with mitx_programs as (
     select * from {{ ref('int__mitxpro__programs') }}
 )
 
-select
+, mitxonline__programs as (
+    select * from {{ ref('int__mitxonline__programs') }}
+)
+
+, micromasters__program_requirements as (
+    select * from {{ ref('int__micromasters__program_requirements') }}
+)
+
+select 
     'MITx Online' as platform
     , mitx__courses.course_title
     , mitx_programs.program_title
     , mitx__courses.mitxonline_course_id as course_id
     , mitx_programs.mitxonline_program_id as program_id
     , mitx__courses.course_readable_id
+    , mitxonline__programs.program_readable_id
 from mitx_programs
 inner join mitx__courses
     on mitx_programs.course_number = mitx__courses.course_number
+left join mitxonline__programs
+    on mitx_programs.mitxonline_program_id = mitxonline__programs.program_id
 where mitx__courses.is_on_mitxonline = true
 
 union all
 
-select
+select 
     'edX.org' as platform
     , mitx__courses.course_title
     , mitx_programs.program_title
-    , mitx__courses.mitxonline_course_id as course_id
-    , mitx_programs.mitxonline_program_id as program_id
+    , micromasters__program_requirements.course_id 
+    , mitx_programs.micromasters_program_id as program_id
     , mitx__courses.course_readable_id
+    , null as program_readable_id
 from mitx_programs
 inner join mitx__courses
     on mitx_programs.course_number = mitx__courses.course_number
+left join micromasters__program_requirements
+    on mitx_programs.micromasters_program_id = micromasters__program_requirements.program_id
 where mitx__courses.is_on_edxorg = true
 
 union all
@@ -49,6 +63,7 @@ select
     , mitxpro__courses.course_id
     , mitxpro__courses.program_id
     , mitxpro__courses.course_readable_id
-from mitxpro__courses
-left join mitxpro__programs
+    , mitxpro__programs.program_readable_id
+from mitxpro__courses 
+left join mitxpro__programs 
     on mitxpro__courses.program_id = mitxpro__programs.program_id
