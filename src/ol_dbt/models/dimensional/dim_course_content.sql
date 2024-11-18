a79373c1b5f44ace14a811fc950cb7e61066074b
Rachel Lougee <rlougee@mit.edu>
2024-11-18 12:21:30 -0500

    add table definition and update to dim_course_content

--- src/ol_dbt/models/dimensional/dim_course_content.sql
+++ src/ol_dbt/models/dimensional/dim_course_content.sql
@@ -1,26 +1,22 @@
 with mitxonline_course_structure as (
     select * from {{ ref('stg__mitxonline__openedx__api__course_structure') }}
-    order by courserun_readable_id, coursestructure_retrieved_at, coursestructure_block_index
 )
 
 , edxorg_course_structure as (
     select * from {{ ref('stg__edxorg__s3__course_structure') }}
-    order by courserun_readable_id, coursestructure_retrieved_at, coursestructure_block_index
 )
 
 , xpro_course_structure as (
     select * from {{ ref('stg__mitxpro__openedx__api__course_structure') }}
-    order by courserun_readable_id, coursestructure_retrieved_at, coursestructure_block_index
 )
 
 , residential_course_structure as (
     select * from {{ ref('stg__mitxresidential__openedx__api__course_structure') }}
-    order by courserun_readable_id, coursestructure_retrieved_at, coursestructure_block_index
 )
 
 , combined as (
     select
-        {{ generate_hash_id('mitxonline') }} as platform_id
+        '{{ var("mitxonline") }}' as platform
         , courserun_readable_id
         , coursestructure_block_index
         , coursestructure_block_id
@@ -34,7 +30,7 @@ with mitxonline_course_structure as (
     union all
 
     select
-         {{ generate_hash_id('edxorg') }} as platform_id
+        '{{ var("edxorg") }}' as platform
         , courserun_readable_id
         , coursestructure_block_index
         , coursestructure_block_id
@@ -48,7 +44,7 @@ with mitxonline_course_structure as (
     union all
 
     select
-         {{ generate_hash_id('mitxpro') }} as platform_id
+        '{{ var("mitxpro") }}' as platform
         , courserun_readable_id
         , coursestructure_block_index
         , coursestructure_block_id
@@ -62,7 +58,7 @@ with mitxonline_course_structure as (
     union all
 
     select
-        {{ generate_hash_id('residential') }} as platform_id
+        '{{ var("residential") }}' as platform
         , courserun_readable_id
         , coursestructure_block_index
         , coursestructure_block_id
@@ -74,16 +70,45 @@ with mitxonline_course_structure as (
     from residential_course_structure
 )
 
-select
-    {{ generate_hash_id('courserun_readable_id || coursestructure_block_id || coursestructure_retrieved_at') }} as content_id
-    , platform_id
-    , courserun_readable_id
-    , coursestructure_block_index as content_block_index
-    , coursestructure_block_id as content_block_id
-    , coursestructure_parent_block_id as parent_content_block_id
-    , coursestructure_block_category as content_block_category
-    , coursestructure_block_title as content_block_title
-    , coursestructure_block_metadata as content_block_metadata
-    , coursestructure_retrieved_at as retrieved_at
+, latest_course_structure as (
+    select
+        platform
+        , courserun_readable_id
+        , max(coursestructure_retrieved_at) as max_retrieved_date
+    from combined
+    group by platform, courserun_readable_id
+)
 
-from combined
+, combined_course_content as (
+    select
+        combined.platform
+        , combined.courserun_readable_id
+        , combined.coursestructure_block_index as content_block_index
+        , combined.coursestructure_block_id as content_block_id
+        , combined.coursestructure_parent_block_id as parent_content_block_id
+        , combined.coursestructure_block_category as content_block_category
+        , combined.coursestructure_block_title as content_block_title
+        , combined.coursestructure_block_metadata as content_block_metadata
+        , combined.coursestructure_retrieved_at as content_retrieved_at
+        , if(latest_course_structure.max_retrieved_date is not null, true, false) as content_is_latest
+    from combined
+    left join latest_course_structure
+        on
+            combined.platform = latest_course_structure.platform
+            and combined.courserun_readable_id = latest_course_structure.courserun_readable_id
+            and combined.coursestructure_retrieved_at = latest_course_structure.max_retrieved_date
+
+)
+
+select
+    {{ generate_hash_id('platform || content_block_id || content_retrieved_at') }} as content_id
+    , courserun_readable_id
+    , content_block_index
+    , content_block_id
+    , parent_content_block_id
+    , content_block_category
+    , content_block_title
+    , content_block_metadata
+    , content_retrieved_at
+    , content_is_latest
+from combined_course_content
