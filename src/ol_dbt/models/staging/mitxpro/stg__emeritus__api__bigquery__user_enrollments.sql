c0c0efa280a78fdf0819f1fa28c557839d0d3df4
Rachel Lougee <rlougee@mit.edu>
2024-12-17 16:35:42 -0500

    stage emeritus enrollment

---
+++ src/ol_dbt/models/staging/mitxpro/stg__emeritus__api__bigquery__user_enrollments.sql
@@ -0,0 +1,52 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__emeritus__bigquery__api_enrollments') }}
+)
+
+, cleaned as (
+    select
+        ---course run
+        batch_id as courserun_id
+        , batch as courserun_title
+        , batch_start_date as courserun_start_on
+        , batch_end_date as courserun_end_on
+        --wrike_run_code maps to external_course_run_id in xPro app
+        , wrike_run_code as courserun_external_readable_id
+
+        ---user
+        , cast(student_id as bigint) as user_id
+        , first_name as user_first_name
+        , last_name as user_last_name
+        , company_name as user_company
+        , work_exp_slab as user_years_experience
+        , nationality as user_nationality
+        , residential_zip_code as user_address_postal_code
+        , country as user_address_country
+        , gdpr_degree as user_gdpr_degree
+        , gdpr_consent_date as user_gdpr_consent_date
+
+        ---enrollment
+        , enrollment_type
+        , opportunity_created_date as enrollment_created_on
+        , opportunity_last_modified_date as enrollment_updated_on
+
+        , if(email = '(blank)', null, email) as user_email
+        , if(lower(status) = 'enrolled', true, false) as is_enrolled
+        , if("deferred" = true, 'deferred', null) as enrollment_status
+        , if("function" = 'No Response', null, "function") as user_job_function
+        , if(job_title in ('No Response', '(Blank)'), null, job_title) as user_job_title
+        , if(industry in ('No Response', '(blank)'), null, industry) as user_industry
+        , if(residential_address = 'NO RESPONSE', null, residential_address) as user_address_street
+        , if(residential_city = 'NO RESPONSE', null, rresidential_city) as user_address_city
+        , if(residential_state = 'NO RESPONSE', null, residential_state) as user_address_state
+        --- match these values to our existing values from other platforms
+        , case
+            when gender = 'Prefer not to answer' then 'Other/Prefer Not to Say'
+            when gender = 'Nonbinary' then 'Non-binary/non-conforming'
+            when gender = 'Prefer not to answer' then 'Other/Prefer Not to Say'
+            when gender = 'No Response' then null
+            else gender
+        end as user_gender
+    from source
+)
+
+select * from cleaned
