5c6c160bfcd3397f6b881d08d0eab131dbd6042e
Rachel Lougee <rlougee@mit.edu>
2025-02-12 14:18:53 -0500

    update cast_timestamp_to_iso8601 to work with timestamp and string

--- src/ol_dbt/macros/cast_timestamp_to_iso8601.sql
+++ src/ol_dbt/macros/cast_timestamp_to_iso8601.sql
@@ -1,3 +1,9 @@
 {% macro cast_timestamp_to_iso8601(column_name) %}
-    to_iso8601(from_iso8601_timestamp({{ column_name }}))
+  case
+       -- If the column is already a timestamp, convert to ISO 8601
+        when try_cast({{ column_name }} AS timestamp) is not null
+           then to_iso8601(try_cast({{ column_name }} AS timestamp))
+        -- otherwise, convert to ISO 8601 from a string
+        else to_iso8601(from_iso8601_timestamp(try_cast({{ column_name }} AS varchar)))
+   end
 {% endmacro %}
