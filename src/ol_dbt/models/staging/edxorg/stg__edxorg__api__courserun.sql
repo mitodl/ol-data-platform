09cded8a8c74a77d90d9ac109b713f366d000aae
Rachel Lougee <rlougee@mit.edu>
2025-02-14 16:35:18 -0500

    Stage MITx courses and runs ingested from API (#1474)
    * stage MITx courses and runs ingested from discovery API
    
    * remove empty courserun_instructors

---
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__api__courserun.sql
@@ -0,0 +1,41 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__edxorg__s3__mitx_course_runs') }}
+)
+
+{{ deduplicate_raw_table(order_by='retrieved_at' , partition_columns = 'run_key') }}
+
+, cleaned as (
+    select
+        run_key as courserun_readable_id
+        , course_key as course_readable_id
+        , title as courserun_title
+        , short_description as courserun_short_description
+        , full_description as courserun_full_description
+        , level_type as courserun_level
+        , marketing_url as courserun_marketing_url
+        , languages as courserun_languages
+        , min_effort as courserun_min_weekly_hours
+        , max_effort as courserun_max_weekly_hours
+        , estimated_hours as courserun_estimated_hours
+        , pacing_type as courserun_pace
+        , enrollment_type as courserun_enrollment_mode
+        , availability as courserun_availability
+        , status as courserun_status
+        , if(staff = '[]', null, staff) as courserun_instructors
+        , if(seats = '[]', null, seats) as courserun_enrollment_modes
+        , json_query(image, 'lax $.url' omit quotes) as courserun_image_url
+        , case
+            when weeks_to_complete = 1 then cast(weeks_to_complete as varchar) || ' week'
+            when weeks_to_complete > 1 then cast(weeks_to_complete as varchar) || ' weeks'
+        end as courserun_duration
+        , {{ cast_timestamp_to_iso8601('start_on') }} as courserun_start_on
+        , {{ cast_timestamp_to_iso8601('end_on') }} as courserun_end_on
+        , {{ cast_timestamp_to_iso8601('enrollment_start') }} as courserun_enrollment_start_on
+        , {{ cast_timestamp_to_iso8601('enrollment_end') }} as courserun_enrollment_end_on
+        , {{ cast_timestamp_to_iso8601('announcement') }} as courserun_announce_on
+        , {{ cast_timestamp_to_iso8601('modified') }} as courserun_updated_at
+
+    from most_recent_source
+)
+
+select * from cleaned
