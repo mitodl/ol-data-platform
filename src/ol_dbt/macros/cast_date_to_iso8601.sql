ab1d0019d65449c75fcec7d509f1868856fc3fd0
Rachel Lougee <rlougee@mit.edu>
2025-02-26 11:14:43 -0500

    update cast_date_to_iso8601 to handle date and string

--- src/ol_dbt/macros/cast_date_to_iso8601.sql
+++ src/ol_dbt/macros/cast_date_to_iso8601.sql
@@ -1,3 +1,9 @@
 {% macro cast_date_to_iso8601(column_name) %}
-    to_iso8601(from_iso8601_date({{ column_name }}))
+  case
+   -- If the column is already a date, convert to ISO 8601
+    when try_cast({{ column_name }} AS date) is not null
+       then to_iso8601(try_cast({{ column_name }} AS date))
+    -- otherwise, convert to ISO 8601 from a string
+    else to_iso8601(from_iso8601_date(try_cast({{ column_name }} AS varchar)))
+end
 {% endmacro %}
