f71c9276720fdbf85ad9577a8bcc48cf5af38302
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-02-28 13:25:17 -0500

    fixed minor error removed 2 extra lines

--- src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
+++ src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
@@ -9,14 +9,19 @@ with allcoupons as (
 )
 
 , redeemed_coupons as (
-    select
-        coupon_id
-        , b2bcoupon_id
+    select coupon_id
     from allorders
     where redeemed = true
-    group by
-        coupon_id
-        , b2bcoupon_id
+    group by coupon_id
+)
+
+, redeemed_b2b_coupons as (
+    select b2bcoupon_id
+    from allorders
+    where 
+        redeemed = true
+        and coupon_id is null
+    group by b2bcoupon_id
 )
 
 select
@@ -32,18 +37,18 @@ select
     , allcoupons.coupon_source_table
     , allcoupons.b2bcoupon_id
     , allcoupons.coupon_id
-    , case
+    , case 
         when
-            redeemed_coupons.coupon_id is not null
-            or redeemed_coupons.b2bcoupon_id is not null
+            redeemed_coupons.coupon_id is not null 
+            or redeemed_b2b_coupons.b2bcoupon_id is not null 
             then true
         when
-            redeemed_coupons.coupon_id is null
-            and redeemed_coupons.b2bcoupon_id is null
-            then false
+            redeemed_coupons.coupon_id is null 
+            and redeemed_b2b_coupons.b2bcoupon_id is null
+            then false 
     end as redeemed
 from allcoupons
 left join redeemed_coupons
-    on
-        allcoupons.coupon_id = redeemed_coupons.coupon_id
-        or allcoupons.b2bcoupon_id = redeemed_coupons.b2bcoupon_id
+    on allcoupons.coupon_id = redeemed_coupons.coupon_id
+left join redeemed_b2b_coupons
+    on allcoupons.b2bcoupon_id = redeemed_b2b_coupons.b2bcoupon_id
