6f35c0664b1f25f8f329e0bb93f339c691eb6d18
Quazi <quazi@mit.edu>
2025-03-17 12:13:20 -0400

    Updated automation policies to also check if any upstream dependencies are missing.

--- src/ol_orchestrate/lib/automation_policies.py
+++ src/ol_orchestrate/lib/automation_policies.py
@@ -6,6 +6,9 @@ def upstream_or_code_changes() -> AutomationCondition:
     has_upstream_changes = AutomationCondition.any_deps_updated()
     has_code_changes = AutomationCondition.code_version_changed()
     newly_missing = AutomationCondition.newly_missing()
-    return no_upstream_dependencies_in_process & (
-        has_upstream_changes | has_code_changes | newly_missing
+    all_upstream_dependencies_present = ~AutomationCondition.any_deps_missing()
+    return (
+        no_upstream_dependencies_in_process
+        & (has_upstream_changes | has_code_changes | newly_missing)
+        & all_upstream_dependencies_present
     )
