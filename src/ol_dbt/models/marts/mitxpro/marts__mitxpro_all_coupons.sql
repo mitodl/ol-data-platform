71f8d5eaec4d97fed763ae2d37c63a3e65c1a22b
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-03-17 07:15:37 -0400

    enhance all coupons mart with additional fields to replace redash reports

--- src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
+++ src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
@@ -8,6 +8,21 @@ with allcoupons as (
     from {{ ref('int__mitxpro__ecommerce_allorders') }}
 )
 
+, ecommerce_coupon as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_coupon') }}
+)
+
+, ecommerce_couponpaymentversion as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_couponpaymentversion') }}
+)
+
+, ecommerce_company as (
+    select *
+    from {{ ref('int__mitxpro__ecommerce_company') }}
+)
+
 , redeemed_coupons as (
     select coupon_id
     from allorders
@@ -16,12 +31,16 @@ with allcoupons as (
 )
 
 , redeemed_b2b_coupons as (
-    select b2bcoupon_id
+    select 
+        b2bcoupon_id
+        , b2border_contract_number
     from allorders
     where
         redeemed = true
         and coupon_id is null
-    group by b2bcoupon_id
+    group by 
+        b2bcoupon_id
+        , b2border_contract_number
 )
 
 select
@@ -37,6 +56,14 @@ select
     , allcoupons.coupon_source_table
     , allcoupons.b2bcoupon_id
     , allcoupons.coupon_id
+    , ecommerce_couponpaymentversion.couponpaymentversion_num_coupon_codes
+    , ecommerce_couponpaymentversion.couponpaymentversion_max_redemptions
+    , ecommerce_couponpaymentversion.couponpayment_name
+    , ecommerce_couponpaymentversion.couponpaymentversion_id
+    , ecommerce_couponpaymentversion.couponpaymentversion_created_on
+    , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount_text
+    , ecommerce_company.company_name
+    , redeemed_b2b_coupons.b2border_contract_number
     , case
         when
             redeemed_coupons.coupon_id is not null
@@ -52,3 +79,9 @@ left join redeemed_coupons
     on allcoupons.coupon_id = redeemed_coupons.coupon_id
 left join redeemed_b2b_coupons
     on allcoupons.b2bcoupon_id = redeemed_b2b_coupons.b2bcoupon_id
+left join ecommerce_coupon
+    on allcoupons.coupon_id = ecommerce_coupon.coupon_id
+left join ecommerce_couponpaymentversion
+    on ecommerce_coupon.couponpayment_name = ecommerce_couponpaymentversion.couponpayment_name
+left join ecommerce_company
+    on ecommerce_couponpaymentversion.company_id = ecommerce_company.company_id
