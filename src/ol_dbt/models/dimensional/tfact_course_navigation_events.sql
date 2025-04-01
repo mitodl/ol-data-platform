87ee55a13f88893e8d340402979bbae205a0da2a
Rachel Lougee <rlougee@mit.edu>
2025-04-01 16:39:21 -0400

    update to navigation_events model

--- src/ol_dbt/models/dimensional/tfact_course_navigation_events.sql
+++ src/ol_dbt/models/dimensional/tfact_course_navigation_events.sql
@@ -12,9 +12,14 @@
 with mitxonline_navigation_events as (
     select
         user_username
+        , openedx_user_id
         , courserun_readable_id
         , useractivity_event_type as event_type
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as vertical_block_id
+        , json_query(useractivity_event_object, 'lax $.old' omit quotes) as starting_tab
+        , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
+        , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
+        , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
+        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -27,9 +32,14 @@ with mitxonline_navigation_events as (
 , xpro_navigation_events as (
     select
         user_username
+        , openedx_user_id
         , courserun_readable_id
         , useractivity_event_type as event_type
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as vertical_block_id
+        , json_query(useractivity_event_object, 'lax $.old' omit quotes) as starting_tab
+        , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
+        , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
+        , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
+        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -42,9 +52,14 @@ with mitxonline_navigation_events as (
 , mitxresidential_navigation_events as (
     select
         user_username
+        , user_id
         , courserun_readable_id
         , useractivity_event_type as event_type
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as vertical_block_id
+        , json_query(useractivity_event_object, 'lax $.old' omit quotes) as starting_tab
+        , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
+        , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
+        , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
+        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -57,9 +72,14 @@ with mitxonline_navigation_events as (
 , edxorg_navigation_events as (
     select
         user_username
+        , user_id
         , courserun_readable_id
         , useractivity_event_type as event_type
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as vertical_block_id
+        , json_query(useractivity_event_object, 'lax $.old' omit quotes) as starting_tab
+        , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
+        , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
+        , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
+        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -81,10 +101,17 @@ with mitxonline_navigation_events as (
 , combined as (
     select
         'mitxonline' as platform
-        , mitxonline_users.user_id
+        , coalesce(mitxonline_users.user_id, mitxonline_navigation_events.openedx_user_id) as user_id
         , mitxonline_navigation_events.courserun_readable_id
         , mitxonline_navigation_events.event_type
-        , mitxonline_navigation_events.vertical_block_id
+        , mitxonline_navigation_events.block_id
+        , coalesce(
+            mitxonline_navigation_events.current_tab
+            , mitxonline_navigation_events.starting_tab
+            , mitxonline_navigation_events.starting_url
+        ) as starting_position
+        , coalesce(mitxonline_navigation_events.ending_tab, mitxonline_navigation_events.ending_url)
+        as ending_position
         , mitxonline_navigation_events.current_tab
         , mitxonline_navigation_events.tab_count
         , mitxonline_navigation_events.event_timestamp
@@ -95,10 +122,16 @@ with mitxonline_navigation_events as (
 
     select
         'mitxpro' as platform
-        , xpro_users.user_id
+        , coalesce(xpro_users.user_id, xpro_navigation_events.openedx_user_id) as user_id
         , xpro_navigation_events.courserun_readable_id
         , xpro_navigation_events.event_type
-        , xpro_navigation_events.vertical_block_id
+        , xpro_navigation_events.block_id
+        , coalesce(
+            xpro_navigation_events.current_tab
+            , xpro_navigation_events.starting_tab
+            , xpro_navigation_events.starting_url
+        ) as starting_position
+        , coalesce(xpro_navigation_events.ending_tab, xpro_navigation_events.ending_url) as ending_position
         , xpro_navigation_events.current_tab
         , xpro_navigation_events.tab_count
         , xpro_navigation_events.event_timestamp
@@ -112,7 +145,9 @@ with mitxonline_navigation_events as (
         , user_id
         , courserun_readable_id
         , event_type
-        , vertical_block_id
+        , block_id
+        , coalesce(current_tab, starting_tab, starting_url) as starting_position
+        , coalesce(ending_tab, ending_url) as ending_position
         , current_tab
         , tab_count
         , event_timestamp
@@ -125,7 +160,9 @@ with mitxonline_navigation_events as (
         , user_id
         , courserun_readable_id
         , event_type
-        , vertical_block_id
+        , block_id
+        , coalesce(current_tab, starting_tab, starting_url) as starting_position
+        , coalesce(ending_tab, ending_url) as ending_position
         , current_tab
         , tab_count
         , event_timestamp
@@ -137,7 +174,9 @@ select
     , platform as platform_id
     , courserun_readable_id
     , event_type
-    , vertical_block_id
+    , block_id
+    , starting_position
+    , ending_position
     , event_timestamp
 
 from combined
