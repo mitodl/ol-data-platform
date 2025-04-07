e9280dc593fb092c144cd88d42e5b213eee0f976
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-04-07 17:02:40 -0400

    remove unneeded id's

--- src/ol_dbt/models/dimensional/dim_user.sql
+++ src/ol_dbt/models/dimensional/dim_user.sql
@@ -17,7 +17,7 @@ with mitxonline_users as (
     select
         mitxonline_users.user_username
         , mitxonline_users.user_email
-        , mitxonline_users.user_full_name
+        , mitxonline_users.user_full_name 
         , mitxonline_legaladdress.user_address_country
         , mitxonline_profile.user_highest_education
         , mitxonline_profile.user_gender
@@ -37,10 +37,6 @@ with mitxonline_users as (
 select
 {{ dbt_utils.generate_surrogate_key(['user_email']) }} as user_pk
     , user_id as user_mitxonline_id
-    , null as user_edxorg_id
-    , null as user_mitxpro_id
-    , null as user_bootcamps_id
-    , null as user_residential_id
     , user_username as user_mitxonline_username
     , null as user_edxorg_username
     , null as user_mitxpro_username
