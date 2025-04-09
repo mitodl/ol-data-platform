5f493da528bad7d277d2922a9c8b4289d13848b7
Rachel Lougee <rlougee@mit.edu>
2025-04-09 09:55:45 -0400

    fix the afact_problem_engagement and add sequential and chapter block_id

--- src/ol_dbt/models/dimensional/afact_problem_engagement.sql
+++ src/ol_dbt/models/dimensional/afact_problem_engagement.sql
@@ -1,71 +1,53 @@
-with course_problems as (
+with problem_structure as (
     select
         courserun_readable_id
-        , count(distinct problem_id) as num_of_problems
-    from tfact_problem_events
-    group by courserun_readable_id
+        , block_id as problem_block_fk
+        , sequential_block_id as sequential_block_fk
+        , chapter_block_id as chapter_block_fk
+    from {{ ref('dim_course_content') }}
+    where
+        block_category = 'problem'
+        and is_latest = true
 )
 
-, course_sections as (
+, problem_attempt_aggregated as (
     select
-        courserun_readable_id
-        , parent_block_id as section_block_id
-        , parent.block_title as section_name
-        , block_id as subsection_block_id
-        , block_title as subsection_name
-    from dim_course_content
-    left join dim_course_content as parent
-        on dim_course_content.parent_block_id = parent.block_id
-    where block_category = 'problem'
-)
-
-, problems_attempted as (
-    select
-        user_id
+        platform
+        , openedx_user_id
+        , problem_block_fk
         , courserun_readable_id
-        , count(distinct problem_id) as num_of_problems_attempted
-        , count(*) as total_num_of_attempts
-        , 100.0 * sum(case
-            when success = 'correct' then 1
-            else 0
-        end) / count(*) as problem_attempts_correct
-        , sum(cast(grade as double)) / nullif(sum(cast(max_grade as double)), 0) as problem_attempts_grade
-        , problem_block_id
-    from tfact_problem_events
+        , max(attempt) as num_of_attempts
+        , max(event_timestamp) as last_attempt_timestamp
+        , count(case when success = 'correct' then 1 end) as num_of_correct_attempts
+    from {{ ref('tfact_problem_events') }}
     where event_type = 'problem_check'
-    group by user_id, courserun_readable_id
+    group by platform, courserun_readable_id, problem_block_fk, openedx_user_id
 )
 
 , combined as (
     select
-       problems_attempted.user_id,
-        , problems_attempted.courserun_readable_id
-        , problems_attempted.problem_block_id as section_block_id
-        , course_sections.section_name
-        , course_sections.subsection_block_id
-        , course_sections.subsection_name
-        , course_problems.num_of_problems
-        , problems_attempted.num_of_problems_attempted
-        , problems_attempted.total_num_of_attempts
-        , problems_attempted.problem_attempts_correct
-        , problems_attempted.problem_attempts_grade
-    from problems_attempted
-    left join course_problems
-        on problems_attempted.courserun_readable_id = course_problems.courserun_readable_id
-    left join course_sections
-        on problems_attempted.problem_block_id = course_sections.subsection_block_id
+        problem_attempt_aggregated.platform
+        , problem_attempt_aggregated.openedx_user_id
+        , problem_attempt_aggregated.courserun_readable_id
+        , problem_attempt_aggregated.problem_block_fk
+        , problem_attempt_aggregated.num_of_attempts
+        , problem_attempt_aggregated.num_of_correct_attempts
+        , problem_attempt_aggregated.last_attempt_timestamp
+        , problem_structure.sequential_block_fk
+        , problem_structure.chapter_block_fk
+    from problem_attempt_aggregated
+    left join problem_structure
+        on problem_attempt_aggregated.problem_block_fk = problem_structure.problem_block_fk
 )
 
-select distinct
-    user_id
+select
+    platform
+    , openedx_user_id
     , courserun_readable_id
-    , section_block_id
-    , section_name
-    , subsection_block_id
-    , subsection_name
-    , num_of_problems
-    , num_of_problems_attempted,
-    , total_num_of_attempts
-    , problem_attempts_correct
-    , problem_attempts_grade
+    , problem_block_fk
+    , num_of_attempts
+    , num_of_correct_attempts
+    , sequential_block_fk
+    , chapter_block_fk
+    , last_attempt_timestamp
 from combined
