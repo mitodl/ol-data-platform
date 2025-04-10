673663c2415fdf676153de26c7ca05891ffa77f3
Rachel Lougee <rlougee@mit.edu>
2025-04-10 07:23:42 -0400

    refactor afact course engagement model and cleanup block_fkey int tfact navigation model

--- src/ol_dbt/models/dimensional/afact_course_page_engagement.sql
+++ src/ol_dbt/models/dimensional/afact_course_page_engagement.sql
@@ -1,93 +1,53 @@
-with pages_viewed as (
-    select
-        user_id
-        , courserun_readable_id
-        , block_id
-        , json_query(event_json, 'lax $.tab_count' omit quotes) as num_of_pages
-        , sum(nullif(cast(ending_position as double),0) - cast(starting_position as double)) as num_of_pages_viewed
-    from tfact_course_navigation_events
-    group by user_id, block_id, event_json
-)
-
-, sections_viewed as (
-    select
-        user_id
-        , count(distinct block_id) as num_of_sections_viewed
-    from tfact_course_navigation_events
-    where
-        -- most of the events are one of these two types
-        -- indicates a learner is done with a section
-        event_type in ('edx.ui.lms.sequence.next_selected',
-        -- indicates a learner is done with a section
-        event_type = 'edx.ui.lms.sequence.previous_selected')
-    group by user_id
-)
-
-, links_clicked as (
-    select
-        user_id,
-        , courserun_readable_id
-        , event_type
-        , block_id
-        , starting_position
-        , ending_position
-        , event_timestamp
-        , event_json
-    from tfact_course_navigation_events
-    where event_type = 'link_clicked'
-    group by user_id, block_id
-)
-
-, course_sections as (
+with vertical_structure as (
     select
         courserun_readable_id
-        , parent_block_id as section_block_id
-        , parent.block_title as section_name
-        , block_id as subsection_block_id
-        , block_title as subsection_name
-    from dim_course_content
-    left join dim_course_content as parent
-        on dim_course_content.parent_block_id = parent.block_id
-    where block_category in
-        ('course', 'chapter', 'sequential', 'vertical', 'discussion', 'html')
+        , block_id
+        , block_index
+        , sequential_block_id as sequential_block_fk
+        , chapter_block_id as chapter_block_fk
+    from {{ ref('dim_course_content') }}
+    where
+        block_category = 'vertical'
+        and is_latest = true
+)
+
+, page_navigation_events as (
+    select
+        platform
+        , user_fk
+        , openedx_user_id
+        , courserun_readable_id
+        , block_fk
+        , count(*) as num_of_views
+        , max(event_timestamp) as last_view_timestamp
+    from {{ ref('tfact_course_navigation_events') }}
+    where block_fk is not null
+    group by platform, courserun_readable_id, openedx_user_id, block_fk
 )
 
 , combined as (
     select
-        pages_viewed.user_id
-        , pages_viewed.courserun_readable_id
-        , user_course_videos.video_id as section_block_id
-        , course_sections.section_name
-        , course_sections.subsection_block_id
-        , course_sections.subsection_name
-        , pages_viewed.num_of_pages,
-        , pages_viewed.num_of_pages_viewed
-        , count(distinct course_sections.block_id) as num_of_sections
-        , sections_viewed.num_of_sections_viewed
-    from pages_viewed
-    left join sections_viewed
-        on pages_viewed.user_id = sections_viewed.user_id
-        and pages_viewed.courserun_readable_id = sections_viewed.courserun_readable_id
-    left join sections_viewed
-        on pages_viewed.user_id = sections_viewed.user_id
-        and pages_viewed.courserun_readable_id = sections_viewed.courserun_readable_id
-    left join links_clicked
-        on pages_viewed.user_id = links_clicked.user_id
-        and pages_viewed.courserun_readable_id = links_clicked.courserun_readable_id
-    left join course_sections
-        on pages_viewed.block_id = course_sections.subsection_block_id
+        page_navigation_events.platform
+        , page_navigation_events.openedx_user_id
+        , page_navigation_events.courserun_readable_id
+        , page_navigation_events.block_fk
+        , page_navigation_events.num_of_views
+        , page_navigation_events.last_view_timestamp
+        , vertical_structure.sequential_block_fk
+        , vertical_structure.chapter_block_fk
+    from page_navigation_events
+    left join vertical_structure
+        on page_navigation_events.block_fk = vertical_structure.block_id
 )
 
-select distinct
-    user_id,
-    courserun_readable_id,
-    section_block_id,
-    section_name,
-    subsection_block_id,
-    subsection_name,
-    num_of_pages,
-    num_of_pages_viewed,
-    -- todo: not sure if the tabs viewed or blocks navigated to count as "pages" in this context
-    num_of_sections,
-    num_of_sections_viewed
+select
+    platform
+    , user_fk
+    , openedx_user_id
+    , courserun_readable_id
+    , block_fk
+    , num_of_views
+    , sequential_block_fk
+    , chapter_block_fk
+    , last_view_timestamp
 from combined
--- src/ol_dbt/models/dimensional/tfact_course_navigation_events.sql
+++ src/ol_dbt/models/dimensional/tfact_course_navigation_events.sql
@@ -20,7 +20,11 @@ with mitxonline_navigation_events as (
         , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
         , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
         , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
+        , case
+            when json_query(useractivity_event_object, 'lax $.id' omit quotes) not like 'block-v1%'
+                then regexp_extract(json_query(useractivity_event_object, 'lax $.id' omit quotes), 'block-v1.*')
+            else json_query(useractivity_event_object, 'lax $.id' omit quotes)
+        end as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -41,7 +45,11 @@ with mitxonline_navigation_events as (
         , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
         , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
         , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
+        , case
+            when json_query(useractivity_event_object, 'lax $.id' omit quotes) not like 'block-v1%'
+                then regexp_extract(json_query(useractivity_event_object, 'lax $.id' omit quotes), 'block-v1.*')
+            else json_query(useractivity_event_object, 'lax $.id' omit quotes)
+        end as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -62,7 +70,11 @@ with mitxonline_navigation_events as (
         , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
         , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
         , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
+        , case
+            when json_query(useractivity_event_object, 'lax $.id' omit quotes) not like 'block-v1%'
+                then regexp_extract(json_query(useractivity_event_object, 'lax $.id' omit quotes), 'block-v1.*')
+            else json_query(useractivity_event_object, 'lax $.id' omit quotes)
+        end as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
@@ -83,7 +95,11 @@ with mitxonline_navigation_events as (
         , json_query(useractivity_event_object, 'lax $.new' omit quotes) as ending_tab
         , json_query(useractivity_event_object, 'lax $.current_url' omit quotes) as starting_url
         , json_query(useractivity_event_object, 'lax $.target_url' omit quotes) as ending_url
-        , json_query(useractivity_event_object, 'lax $.id' omit quotes) as block_id
+        , case
+            when json_query(useractivity_event_object, 'lax $.id' omit quotes) not like 'block-v1%'
+                then regexp_extract(json_query(useractivity_event_object, 'lax $.id' omit quotes), 'block-v1.*')
+            else json_query(useractivity_event_object, 'lax $.id' omit quotes)
+        end as block_id
         , json_query(useractivity_event_object, 'lax $.current_tab' omit quotes) as current_tab
         , json_query(useractivity_event_object, 'lax $.tab_count' omit quotes) as tab_count
         , from_iso8601_timestamp_nanos(useractivity_timestamp) as event_timestamp
