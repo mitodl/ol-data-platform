393673fa08b8aafda15151309e5ec2a5b5a564ee
Quazi <59845076+quazi-h@users.noreply.github.com>
2025-04-10 12:28:12 -0400

    Create engagement afact models (#1529)
    * create fact_course_navigation_events model
    
    * update to navigation_events model
    
    * create problem and video event models
    
    * add event_json field for less common attribute
    
    * update description per feedback
    
    * update description and add unique test
    
    * Added aggregate fact dimensional model definitions.
    Added afact tables for video, page, and problem engagement.
    Business logic for the page engagement will likely need a lot of refactoring.
    
    * yaml syntax fixes
    
    * Added aggregate fact dimensional model definitions.
    Added afact tables for video, page, and problem engagement.
    Business logic for the page engagement will likely need a lot of refactoring.
    
    # Conflicts:
    #	src/ol_dbt/models/dimensional/_dim__models.yml
    #	src/ol_dbt/models/dimensional/afact_course_page_engagement.sql
    
    * yaml syntax fixes
    
    * Creating dim_video and dim_problem (#1528)
    
    * dim_course_content, dim_video and dim_problem
    
    * update primary key
    
    * create fact_course_navigation_events model
    
    * update to navigation_events model
    
    * update description and add unique test
    
     Conflicts:
    	src/ol_dbt/models/dimensional/_dim__models.yml
    	src/ol_dbt/models/dimensional/tfact_video_events.sql
    
    * fix the afact_problem_engagement and add sequential and chapter block_id
    
    * refactor afact course engagement model and cleanup block_fkey int tfact navigation model
    
    * update description
    
    * remove unused key
    
    ---------
    
    Co-authored-by: Rachel Lougee <rlougee@mit.edu>

--- src/ol_dbt/models/dimensional/dim_course_content.sql
+++ src/ol_dbt/models/dimensional/dim_course_content.sql
@@ -74,6 +74,56 @@ with mitxonline_course_structure as (
     group by courserun_readable_id
 )
 
+, sequentials as (
+    select * from combined
+    where coursestructure_block_category = 'sequential'
+)
+
+, combined_with_sequentials as (
+    select
+        combined.*
+        , sequentials.coursestructure_block_id as sequential_block_id
+        , sequentials.coursestructure_block_title as sequential_title
+        , row_number() over (
+            partition by
+                combined.courserun_readable_id
+                , combined.coursestructure_block_index
+                , combined.coursestructure_retrieved_at
+            order by sequentials.coursestructure_block_index desc
+        ) as row_num
+    from combined
+    inner join sequentials
+        on
+            combined.courserun_readable_id = sequentials.courserun_readable_id
+            and combined.coursestructure_retrieved_at = sequentials.coursestructure_retrieved_at
+            and combined.coursestructure_block_index >= sequentials.coursestructure_block_index
+)
+
+, chapters as (
+    select * from combined
+    where coursestructure_block_category = 'chapter'
+)
+
+, combined_with_chapters as (
+    select
+        combined.*
+        , chapters.coursestructure_block_id as chapter_block_id
+        , chapters.coursestructure_block_title as chapter_title
+        , row_number() over (
+            partition by
+                combined.courserun_readable_id
+                , combined.coursestructure_block_index
+                , combined.coursestructure_retrieved_at
+            order by chapters.coursestructure_block_index desc
+        ) as row_num
+    from combined
+    inner join chapters
+        on
+            combined.courserun_readable_id = chapters.courserun_readable_id
+            and combined.coursestructure_retrieved_at = chapters.coursestructure_retrieved_at
+            and combined.coursestructure_block_index >= chapters.coursestructure_block_index
+)
+
 , combined_course_content as (
     select
         combined.courserun_readable_id
@@ -84,13 +134,28 @@ with mitxonline_course_structure as (
         , combined.coursestructure_block_title as block_title
         , combined.coursestructure_block_metadata as block_metadata
         , combined.coursestructure_retrieved_at as retrieved_at
+        , combined_with_chapters.chapter_block_id
+        , combined_with_chapters.chapter_title
+        , combined_with_sequentials.sequential_block_id
+        , combined_with_sequentials.sequential_title
         , if(latest_course_structure.max_retrieved_date is not null, true, false) as is_latest
     from combined
     left join latest_course_structure
         on
             combined.courserun_readable_id = latest_course_structure.courserun_readable_id
             and combined.coursestructure_retrieved_at = latest_course_structure.max_retrieved_date
-
+    left join combined_with_chapters
+        on
+            combined.courserun_readable_id = combined_with_chapters.courserun_readable_id
+            and combined.coursestructure_block_id = combined_with_chapters.coursestructure_block_id
+            and combined.coursestructure_retrieved_at = combined_with_chapters.coursestructure_retrieved_at
+            and combined_with_chapters.row_num = 1
+    left join combined_with_sequentials
+        on
+            combined.courserun_readable_id = combined_with_sequentials.courserun_readable_id
+            and combined.coursestructure_block_id = combined_with_sequentials.coursestructure_block_id
+            and combined.coursestructure_retrieved_at = combined_with_sequentials.coursestructure_retrieved_at
+            and combined_with_sequentials.row_num = 1
 )
 
 select
@@ -99,6 +164,8 @@ select
     , block_index
     , block_id
     , parent_block_id
+    , sequential_block_id
+    , chapter_block_id
     , block_category
     , block_title
     , block_metadata
