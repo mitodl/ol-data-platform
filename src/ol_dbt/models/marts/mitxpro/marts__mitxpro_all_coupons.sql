2ea52cb7290ef7b65e5732c5d9b6da5798af4aa6
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-05-12 18:15:49 -0400

    fix one duplicate record

--- src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
+++ src/ol_dbt/models/marts/mitxpro/marts__mitxpro_all_coupons.sql
@@ -69,12 +69,10 @@ with allcoupons as (
     group by ecommerce_coupon.couponpayment_name
 )
 
-, pull_product_readable_id as (
-    select
+, pull_product_regular as (
+    select 
         allorders.coupon_id
-        , allorders.b2bcoupon_id
         , ecommerce_couponpaymentversion.couponpaymentversion_id
-        , allorders.b2border_contract_number
         , coalesce(
             allorders.courserun_readable_id
             , mitxpro__programruns.programrun_readable_id
@@ -86,14 +84,41 @@ with allcoupons as (
     left join mitxpro__programruns
         on ecommerce_line.programrun_id = mitxpro__programruns.programrun_id
     left join ecommerce_couponpaymentversion
-        on
-            allorders.couponpaymentversion_payment_transaction
+        on 
+            allorders.couponpaymentversion_payment_transaction 
             = ecommerce_couponpaymentversion.couponpaymentversion_payment_transaction
-    where allorders.order_id is not null
+    where 
+        allorders.order_id is not null
+        and allorders.coupon_id is not null 
     group by
         allorders.coupon_id
-        , allorders.b2bcoupon_id
         , ecommerce_couponpaymentversion.couponpaymentversion_id
+        , coalesce(
+            allorders.courserun_readable_id
+            , mitxpro__programruns.programrun_readable_id
+            , allorders.program_readable_id
+        )
+)
+
+, pull_product_b2b as (
+    select 
+        allorders.b2bcoupon_id
+        , allorders.b2border_contract_number
+        , coalesce(
+            allorders.courserun_readable_id
+            , mitxpro__programruns.programrun_readable_id
+            , allorders.program_readable_id
+        ) as product_readable_id
+    from allorders
+    left join ecommerce_line
+        on allorders.line_id = ecommerce_line.line_id
+    left join mitxpro__programruns
+        on ecommerce_line.programrun_id = mitxpro__programruns.programrun_id
+    where 
+        allorders.order_id is not null
+        and allorders.b2bcoupon_id is not null
+    group by 
+        allorders.b2bcoupon_id
         , allorders.b2border_contract_number
         , coalesce(
             allorders.courserun_readable_id
@@ -123,8 +148,11 @@ select
     , ecommerce_couponpaymentversion.couponpaymentversion_discount_amount_text
     , ecommerce_company.company_name
     , redeemed_b2b_coupons.b2border_contract_number
-    , pull_product_readable_id.product_readable_id
     , coupons_used_by_name.coupons_used_count
+    , coalesce(
+        pull_product_regular.product_readable_id
+        , pull_product_b2b.product_readable_id
+    ) as product_readable_id
     , case
         when
             redeemed_coupons.coupon_id is not null
@@ -148,14 +176,11 @@ left join ecommerce_company
     on ecommerce_couponpaymentversion.company_id = ecommerce_company.company_id
 left join coupons_used_by_name
     on ecommerce_couponpaymentversion.couponpayment_name = coupons_used_by_name.couponpayment_name
-left join pull_product_readable_id
+left join pull_product_regular
+    on 
+        allcoupons.coupon_id = pull_product_regular.coupon_id
+        and ecommerce_couponpaymentversion.couponpaymentversion_id = pull_product_regular.couponpaymentversion_id
+left join pull_product_b2b
     on
-        (
-            (allcoupons.coupon_id = pull_product_readable_id.coupon_id)
-            or (allcoupons.b2bcoupon_id = pull_product_readable_id.b2bcoupon_id)
-        )
-        and
-        (
-            (ecommerce_couponpaymentversion.couponpaymentversion_id = pull_product_readable_id.couponpaymentversion_id)
-            or (redeemed_b2b_coupons.b2border_contract_number = pull_product_readable_id.b2border_contract_number)
-        )
+        allcoupons.b2bcoupon_id = pull_product_b2b.b2bcoupon_id
+        and redeemed_b2b_coupons.b2border_contract_number = pull_product_b2b.b2border_contract_number
