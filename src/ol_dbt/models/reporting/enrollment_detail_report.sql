d3936e075ad2e40406c867eeb8e5675737f1db14
rlougee <rlougee@mit.edu>
2025-06-16 15:59:37 -0400

    remove unused columns

--- src/ol_dbt/models/reporting/enrollment_detail_report.sql
+++ src/ol_dbt/models/reporting/enrollment_detail_report.sql
@@ -33,7 +33,7 @@ with enrollments as (
     select
         user_email
         , count(distinct course_title) as num_of_course_passed
-    from course_enrollments
+    from enrollments
     where courserungrade_is_passing = true
     group by user_email
 )
@@ -43,62 +43,41 @@ select
     , enrollments.courserunenrollment_id
     , enrollments.course_readable_id
     , enrollments.course_title
-    , enrollments.courserun_id
     , enrollments.courserun_is_current
     , enrollments.courserun_readable_id
     , enrollments.courserun_start_on
     , enrollments.courserun_end_on
     , enrollments.courserun_title
-    , enrollments.courseruncertificate_created_on
-    , enrollments.courseruncertificate_is_earned
-    , enrollments.courseruncertificate_url
-    , enrollments.courseruncertificate_uuid
     , enrollments.courserunenrollment_created_on
     , enrollments.courserunenrollment_enrollment_mode
     , enrollments.courserunenrollment_enrollment_status
     , enrollments.courserunenrollment_is_active
-    , enrollments.courserunenrollment_is_edx_enrolled
     , enrollments.courserunenrollment_upgraded_on
+    , enrollments.courseruncertificate_created_on
+    , enrollments.courseruncertificate_is_earned
+    , enrollments.courseruncertificate_url
     , enrollments.courserungrade_grade
     , enrollments.courserungrade_is_passing
-    , enrollments.order_id
-    , enrollments.order_reference_number
-    , enrollments.user_company
     , enrollments.user_country_code
     , enrollments.user_full_name
-    , enrollments.user_highest_education
-    , enrollments.user_id
-    , enrollments.user_hashed_id
     , enrollments.user_username
+    , lower(enrollments.user_email) as user_email
     , course_passed_counts.num_of_course_passed
     , orders.coupon_code
-    , orders.coupon_id
     , orders.coupon_name
-    , orders.coupon_type
     , orders.discount
-    , orders.order_total_price_paid_plus_tax
+    , enrollments.order_id
+    , enrollments.order_reference_number
     , orders.order_created_on
     , orders.order_state
-    , orders.receipt_authorization_code
-    , orders.receipt_bill_to_address_state
-    , orders.receipt_bill_to_address_country
     , orders.receipt_payment_method
     , orders.receipt_payment_amount
-    , orders.receipt_payment_currency
-    , orders.receipt_payment_card_number
-    , orders.receipt_payment_card_type
-    , orders.receipt_payment_transaction_type
-    , orders.receipt_payment_transaction_uuid
-    , orders.receipt_payer_name
     , orders.receipt_payer_email
-    , orders.receipt_payer_ip_address
     , orders.receipt_payment_timestamp
-    , orders.receipt_transaction_id
     , orders.unit_price
     , programs.program_name
-    , lower(enrollments.user_email) as user_email
     , if(
-        enrollments.platform = 'MITx Online'
+        enrollments.platform = '{{ var("mitxonline") }}'
         , concat('https://mitxonline.mit.edu/orders/receipt/', cast(orders.order_id as varchar))
         , null
     ) as receipt_url
