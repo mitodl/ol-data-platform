d469ab1665cf64f98189411ca6e0803548ef6dc1
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-06-20 12:54:49 -0400

    fix joins for learner engagement reporting

--- src/ol_dbt/models/reporting/learner_engagement_report.sql
+++ src/ol_dbt/models/reporting/learner_engagement_report.sql
@@ -69,19 +69,17 @@ with video_pre_query as (
         , coalesce(u.email, ou.email) as email
         , sum(a.num_of_views) as num_of_page_views
     from ol_warehouse_production_dimensional.afact_course_page_engagement as a
-    inner join ol_warehouse_production_dimensional.dim_course_content as b
+    left join ol_warehouse_production_dimensional.dim_course_content as b
         on
             a.block_fk = b.block_id
             and b.is_latest = true
-    inner join ol_warehouse_production_dimensional.dim_course_content as c
+    left join ol_warehouse_production_dimensional.dim_course_content as c
         on
             b.parent_block_id = c.block_id
             and c.is_latest = true
-            and replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = c.courserun_readable_id
-    inner join ol_warehouse_production_dimensional.dim_course_content as d
+    left join ol_warehouse_production_dimensional.dim_course_content as d
         on
             c.parent_block_id = d.block_id
-            and replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = d.courserun_readable_id
             and d.is_latest = true
     left join ol_warehouse_production_dimensional.dim_user as u
         on
@@ -121,7 +119,7 @@ with video_pre_query as (
     from video_pre_query as a
     inner join ol_warehouse_production_dimensional.dim_video as c
         on
-            replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = c.courserun_readable_id
+            a.courserun_readable_id = c.courserun_readable_id
             and a.video_block_fk = substring(c.video_block_pk, regexp_position(c.video_block_pk, 'block@') + 6)
     left join ol_warehouse_production_dimensional.dim_user as b
         on
@@ -135,20 +133,19 @@ with video_pre_query as (
         on replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = h.courserun_readable_id
     left join ol_warehouse_production_intermediate.int__combined__course_runs as h2
         on a.courserun_readable_id = h2.courserun_readable_id
-    inner join ol_warehouse_production_dimensional.dim_course_content as v
+    left join ol_warehouse_production_dimensional.dim_course_content as v
         on
             c.content_block_fk = v.content_block_pk
-            and replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = v.courserun_readable_id
-    inner join ol_warehouse_production_dimensional.dim_course_content as cc_subsection
+            and a.courserun_readable_id = v.courserun_readable_id
+    left join ol_warehouse_production_dimensional.dim_course_content as cc_subsection
         on
             v.parent_block_id = cc_subsection.block_id
-            and replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/')
-            = cc_subsection.courserun_readable_id
+            and a.courserun_readable_id = cc_subsection.courserun_readable_id
             and cc_subsection.is_latest = true
-    inner join ol_warehouse_production_dimensional.dim_course_content as cc_section
+    left join ol_warehouse_production_dimensional.dim_course_content as cc_section
         on
             cc_subsection.parent_block_id = cc_section.block_id
-            and replace(replace(a.courserun_readable_id, 'course-v1:', ''), '+', '/') = cc_section.courserun_readable_id
+            and a.courserun_readable_id = cc_section.courserun_readable_id
             and cc_section.is_latest = true
     group by
         a.platform
