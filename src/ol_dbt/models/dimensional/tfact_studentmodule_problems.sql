b887cc711b9011b51216a833afceb1c113b7cdc5
Quazi <quazi@mit.edu>
2025-07-07 15:20:24 -0400

    Updated joins to user and content in tfact_studentmodule_problems to be left joins. Updated event type for student module data to be "problem_submission".

--- src/ol_dbt/models/dimensional/tfact_studentmodule_problems.sql
+++ src/ol_dbt/models/dimensional/tfact_studentmodule_problems.sql
@@ -37,7 +37,7 @@ with mitxonline_studentmodule_problems as (
         , sm.user_id as openedx_user_id
         , sm.courserun_readable_id
         , sm.studentmodule_id
-        , 'problem_check' as event_type
+        , 'problem_submission' as event_type
         , sm.studentmodule_state_data as event_json
         , content.block_title as problem_name
         , sm.coursestructure_block_id as problem_block_id
@@ -48,8 +48,8 @@ with mitxonline_studentmodule_problems as (
         , sm.max_grade
         , sm.success
     from mitxonline_studentmodule_problems as sm
-    inner join users on sm.user_id = users.mitxonline_openedx_user_id
-    inner join content on sm.coursestructure_block_id = content.block_id
+    left join users on sm.user_id = users.mitxonline_openedx_user_id
+    left join content on sm.coursestructure_block_id = content.block_id
 )
 
 , mitxpro_studentmodule_combined as (
@@ -59,7 +59,7 @@ with mitxonline_studentmodule_problems as (
         , sm.user_id as openedx_user_id
         , sm.courserun_readable_id
         , sm.studentmodule_id
-        , 'problem_check' as event_type
+        , 'problem_submission' as event_type
         , sm.studentmodule_state_data as event_json
         , content.block_title as problem_name
         , sm.coursestructure_block_id as problem_block_id
@@ -70,8 +70,8 @@ with mitxonline_studentmodule_problems as (
         , sm.max_grade
         , sm.success
     from mitxpro_studentmodule_problems as sm
-    inner join users on sm.user_id = users.mitxpro_openedx_user_id
-    inner join content on sm.coursestructure_block_id = content.block_id
+    left join users on sm.user_id = users.mitxpro_openedx_user_id
+    left join content on sm.coursestructure_block_id = content.block_id
 )
 
 , residential_studentmodule_combined as (
@@ -81,7 +81,7 @@ with mitxonline_studentmodule_problems as (
         , sm.user_id as openedx_user_id
         , sm.courserun_readable_id
         , sm.studentmodule_id
-        , 'problem_check' as event_type
+        , 'problem_submission' as event_type
         , sm.studentmodule_state_data as event_json
         , content.block_title as problem_name
         , sm.coursestructure_block_id as problem_block_id
@@ -92,8 +92,8 @@ with mitxonline_studentmodule_problems as (
         , sm.max_grade
         , sm.success
     from mitxresidential_studentmodule_problems as sm
-    inner join users on sm.user_id = users.residential_openedx_user_id
-    inner join content on sm.coursestructure_block_id = content.block_id
+    left join users on sm.user_id = users.residential_openedx_user_id
+    left join content on sm.coursestructure_block_id = content.block_id
 )
 
 select *
