1ad931d4df683ebe42a6eee8e55d6da4ec23840f
rlougee <rlougee@mit.edu>
2025-07-21 12:36:04 -0400

    fix typo in staging tutorbotoutput

--- src/ol_dbt/models/staging/learn-ai/stg__learn_ai__app__postgres__chatbots_tutorbotoutput.sql
+++ src/ol_dbt/models/staging/learn-ai/stg__learn_ai__app__postgres__chatbots_tutorbotoutput.sql
@@ -5,9 +5,9 @@ with source as (
 {{ deduplicate_raw_table(order_by='_airbyte_extracted_at' , partition_columns = 'id') }}
 , cleaned as (
     select
-        id as turorbotoutput_id
+        id as tutorbotoutput_id
         , thread_id as chatsession_thread_id
-        , chat_json as turorbot_chat_json
+        , chat_json as tutorbot_chat_json
     from most_recent_source
 )
 
