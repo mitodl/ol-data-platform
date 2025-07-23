9e3e0ffc88b1e58f93d2339d34e81dd06856dd5d
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-07-23 10:33:09 -0400

    add coupon payment version id field

--- src/ol_dbt/models/marts/combined/marts__combined_discounts.sql
+++ src/ol_dbt/models/marts/combined/marts__combined_discounts.sql
@@ -26,7 +26,7 @@ with mitxonline_discount as (
     group by discount_code
 )
 
-select
+select 
     'MITx Online' as platform
     , a.discount_code
     , null as discount_name
@@ -46,16 +46,17 @@ select
         , c.program_readable_id
     ) as product_readable_id
     , coalesce(case when d.discount_code is not null then true end, false) as redeemed
-    , case
+    , case 
         when discount_type = 'percent-off' then cast(a.discount_amount as varchar) || '%'
         when discount_type = 'dollars-off' then '$' || cast(a.discount_amount as varchar) || ' off'
-        when discount_type = 'fixed-price' then '$' || cast(a.discount_amount as varchar) || ' fixed price'
-    end
+        when discount_type = 'fixed-price' then '$' || cast(a.discount_amount as varchar) || ' fixed price' 
+    end 
         as discount_amount_text
     , null as company_name
     , null as discount_type
     , null as b2bcoupon_id
     , a.discount_id
+    , null as couponpaymentversion_id
 from mitxonline_discount as a
 left join mitxonline_discountproduct as b
     on a.discount_id = b.discount_id
@@ -66,7 +67,7 @@ left join mitxonline_order_sum as d
 
 union all
 
-select
+select 
     'xPro' as platform
     , coupon_code as discount_code
     , coupon_name as discount_name
@@ -88,4 +89,5 @@ select
     , coupon_type as discount_type
     , b2bcoupon_id
     , coupon_id as discount_id
+    , couponpaymentversion_id
 from mitxpro_all_coupons
