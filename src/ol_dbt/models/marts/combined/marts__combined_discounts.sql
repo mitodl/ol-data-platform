f125bd8dfd5c612819ae0e40ea4d29f0ddad306d
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-07-24 13:22:57 -0400

    updates based on PR feedback

--- src/ol_dbt/models/marts/combined/marts__combined_discounts.sql
+++ src/ol_dbt/models/marts/combined/marts__combined_discounts.sql
@@ -28,75 +28,77 @@ with mitxonline_discount as (
 
 , mitxonline_product_joins as (
     select
-        b.discount_id
+        mitxonline_discountproduct.discount_id
         , coalesce(
-            c.courserun_readable_id
-            , c.program_readable_id
+            mitxonline_product.courserun_readable_id
+            , mitxonline_product.program_readable_id
         ) as product_readable_id
-    from mitxonline_discountproduct as b
-    left join mitxonline_product as c
-        on b.product_id = c.product_id
-    group by
-        b.discount_id
+    from mitxonline_discountproduct
+    left join mitxonline_product
+        on mitxonline_discountproduct.product_id = mitxonline_product.product_id
+    group by 
+        mitxonline_discountproduct.discount_id
         , coalesce(
-            c.courserun_readable_id
-            , c.program_readable_id
+            mitxonline_product.courserun_readable_id
+            , mitxonline_product.program_readable_id
         )
 )
 
-select
+select 
     'MITx Online' as platform
-    , a.discount_code
+    , mitxonline_discount.discount_code
     , null as discount_name
-    , a.discount_created_on
+    , mitxonline_discount.discount_created_on
     , null as payment_transaction
-    , a.discount_source
-    , a.discount_activated_on
-    , a.discount_expires_on
+    , mitxonline_discount.discount_source
+    , mitxonline_discount.discount_activated_on
+    , mitxonline_discount.discount_expires_on
     , null as num_discount_codes
-    , a.discount_max_redemptions
+    , mitxonline_discount.discount_max_redemptions
     , null as couponpayment_name
-    , null as b2border_contract_number
+    , null as b2b_contract_number
     , null as b2breceipt_reference_number
-    , d.coupons_used_count as discounts_used_count
-    , c.product_readable_id
-    , coalesce(case when d.discount_code is not null then true end, false) as redeemed
+    , coalesce(case when mitxonline_order_sum.discount_code is not null then true end, false) as redeemed
+    , mitxonline_order_sum.coupons_used_count as discounts_used_count
+    , mitxonline_product_joins.product_readable_id
     , case
-        when a.discount_type = 'percent-off' then cast(a.discount_amount as varchar) || '%'
-        when a.discount_type = 'dollars-off' then '$' || cast(a.discount_amount as varchar) || ' off'
-        when a.discount_type = 'fixed-price' then '$' || cast(a.discount_amount as varchar) || ' fixed price'
-    end
-        as discount_amount_text
+        when discount_type = 'percent-off'
+            then concat(format('%.2f', mitxonline_discount.discount_amount), '%')
+        when discount_type = 'dollars-off'
+            then concat('$', format('%.2f', mitxonline_discount.discount_amount))
+        when discount_type = 'fixed-price'
+            then concat('Fixed Price: ', format('%.2f', mitxonline_discount.discount_amount))
+    end as discount_amount_text
     , null as company_name
     , null as discount_type
     , null as b2bcoupon_id
-    , a.discount_id
+    , mitxonline_discount.discount_id
     , null as couponpaymentversion_id
-from mitxonline_discount as a
-left join mitxonline_product_joins as c
-    on a.discount_id = c.discount_id
-left join mitxonline_order_sum as d
-    on a.discount_code = d.discount_code
+from mitxonline_discount
+left join mitxonline_product_joins
+    on mitxonline_discount.discount_id = mitxonline_product_joins.discount_id
+left join mitxonline_order_sum
+    on mitxonline_discount.discount_code = mitxonline_order_sum.discount_code
 
 union all
 
-select
+select 
     'xPro' as platform
     , coupon_code as discount_code
     , coupon_name as discount_name
     , coupon_created_on as discount_created_on
     , payment_transaction
-    , discount_source
+    , case when discount_id is null then 'b2b' else discount_source end as discount_source
     , activated_on as discount_activated_on
     , expires_on as discount_expires_on
     , couponpaymentversion_num_coupon_codes as num_discount_codes
     , couponpaymentversion_max_redemptions as discount_max_redemptions
     , couponpayment_name
-    , b2border_contract_number
+    , b2b_contract_number
     , b2breceipt_reference_number
+    , redeemed
     , coupons_used_count as discounts_used_count
     , product_readable_id
-    , redeemed
     , discount_amount as discount_amount_text
     , company_name
     , coupon_type as discount_type
