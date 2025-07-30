209b02ad607b5b42c11daa6fd883cf873c290d5c
rlougee <rlougee@mit.edu>
2025-07-30 11:53:09 -0400

    stage zendesk users, brands and comments

--- src/ol_dbt/models/staging/zendesk/stg__zendesk__ticket.sql
+++ src/ol_dbt/models/staging/zendesk/stg__zendesk__ticket.sql
@@ -8,7 +8,6 @@ with source as (
         id as ticket_id
         , via as ticket_via_object
         , url as ticket_api_url
-        , tags as ticket_tags
         , type as ticket_type
         , status as ticket_status
         , description as ticket_description
@@ -23,14 +22,10 @@ with source as (
         , requester_id as ticket_requester_user_id
         , submitter_id as ticket_submitter_user_id
         , assignee_id as ticket_assignee_user_id
-        , email_cc_ids as ticket_email_cc_user_ids
-        , follower_ids as ticket_follower_user_ids
-        , collaborator_ids as ticket_collaborator_user_ids
         , forum_topic_id
         , ticket_form_id
         , organization_id
         , custom_status_id
-        , sharing_agreement_ids
         , allow_attachments as ticket_allow_attachments
         , allow_channelback as ticket_allow_channelback
         , has_incidents as ticket_has_incidents
@@ -39,13 +34,21 @@ with source as (
         , due_at as ticket_due_at
         , custom_fields as ticket_custom_fields
         , generated_timestamp as ticket_unix_timestamp
-        , json_query(satisfaction_rating, 'lax $.score' omit quotes) as ticket_satisfaction_rating_score
-        , json_query(satisfaction_rating, 'lax $.comment' omit quotes) as ticket_satisfaction_rating_comment
-        , json_query(satisfaction_rating, 'lax $.reason' omit quotes) as ticket_satisfaction_rating_reason
-        , json_query(via, 'lax $.channel' omit quotes) as ticket_source_channel
-        , json_query(via, 'lax $.source.from.address' omit quotes) as ticket_source_email
-        , json_query(via, 'lax $.source.from.ticket_id' omit quotes) as ticket_source_ticket_id
-        , json_query(via, 'lax $.source.rel' omit quotes) as ticket_source_rel
+        , nullif(tags, array[]) as ticket_tags
+        , nullif(email_cc_ids, array[]) as ticket_email_cc_user_ids
+        , nullif(follower_ids, array[]) as ticket_follower_user_ids
+        , nullif(collaborator_ids, array[]) as ticket_collaborator_user_ids
+        , nullif(sharing_agreement_ids, array[]) as sharing_agreement_ids
+        , nullif(json_query(satisfaction_rating, 'lax $.score' omit quotes), 'null')
+            as ticket_satisfaction_rating_score
+        , nullif(json_query(satisfaction_rating, 'lax $.comment' omit quotes), 'null')
+            as ticket_satisfaction_rating_comment
+        , nullif(json_query(satisfaction_rating, 'lax $.reason' omit quotes), 'null')
+            as ticket_satisfaction_rating_reason
+        , nullif(json_query(via, 'lax $.channel' omit quotes), 'null') as ticket_source_channel
+        , nullif(json_query(via, 'lax $.source.from.address' omit quotes), 'null') as ticket_source_email
+        , nullif(json_query(via, 'lax $.source.from.ticket_id' omit quotes), 'null') as ticket_source_ticket_id
+        , nullif(json_query(via, 'lax $.source.rel' omit quotes), 'null') as ticket_source_rel
         , {{ cast_timestamp_to_iso8601('created_at') }} as ticket_created_at
         , {{ cast_timestamp_to_iso8601('updated_at') }} as ticket_updated_at
     from most_recent_source
---
+++ src/ol_dbt/models/staging/zendesk/stg__zendesk__user.sql
@@ -0,0 +1,52 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__thirdparty__zendesk_support__users') }}
+)
+
+{{ deduplicate_raw_table(order_by='_airbyte_extracted_at' , partition_columns = 'id') }}
+, cleaned as (
+    select
+        id as user_id
+        , url as user_api_url
+        , name as user_name
+        , alias as user_alias
+        , email as user_email
+        , phone as user_phone_number
+        , photo as user_photo_object
+        , active as user_is_active
+        , permanently_deleted as user_is_permanently_deleted
+        , suspended as user_is_suspended
+        , shared as user_is_shared
+        , verified as user_is_verified
+        , details as user_details
+        , notes as user_notes
+        , chat_only as user_is_chat_only
+        , moderator as user_is_moderator
+        , restricted_agent as user_is_restricted_agent
+        , signature as user_signature
+        , locale as user_locale
+        , locale_id as user_locale_id
+        , time_zone as user_time_zone
+        , iana_time_zone as user_iana_time_zone
+        , user_fields
+        , shared_agent as user_is_shared_agent
+        , organization_id
+        , default_group_id as user_default_group_id
+        , ticket_restriction as user_ticket_restriction
+        , only_private_comments as user_only_private_comments
+        , two_factor_auth_enabled as user_has_two_factor_auth_enabled
+        , nullif(tags, array[]) as user_tags
+        , role as user_role
+        , case
+            when role_type = 0 then 'custom agent'
+            when role_type = 1 then 'light agent'
+            when role_type = 3 then 'chat agent'
+            when role_type = 4 then 'admin'
+            else role_type
+        end as user_role_type
+        , {{ cast_timestamp_to_iso8601('last_login_at') }} as user_last_login_at
+        , {{ cast_timestamp_to_iso8601('created_at') }} as user_created_at
+        , {{ cast_timestamp_to_iso8601('updated_at') }} as user_updated_at
+    from most_recent_source
+)
+
+select * from cleaned
