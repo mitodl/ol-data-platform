e7c3e120ae61759a025e7b86180ed18d8999cb9b
Rachel Lougee <rlougee@mit.edu>
2025-08-01 10:05:09 -0400

    Create intermediate zendesk ticket, ticket comment and relevant staging models (#1625)
    * Create intermediate zendesk ticket and relevant staging
    
    * update description
    
    * add ticket comment intermediate
    
    * remove email cc'd since its all null

---
+++ src/ol_dbt/models/staging/zendesk/stg__zendesk__group.sql
@@ -0,0 +1,20 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__thirdparty__zendesk_support__groups') }}
+)
+
+{{ deduplicate_raw_table(order_by='_airbyte_extracted_at' , partition_columns = 'id') }}
+, cleaned as (
+    select
+        id as group_id
+        , url as group_api_url
+        , name as group_name
+        , description as group_description
+        , default as group_is_default
+        , deleted as group_is_deleted
+        , is_public as group_is_public
+        , {{ cast_timestamp_to_iso8601('created_at') }} as group_created_at
+        , {{ cast_timestamp_to_iso8601('updated_at') }} as group_updated_at
+    from most_recent_source
+)
+
+select * from cleaned
