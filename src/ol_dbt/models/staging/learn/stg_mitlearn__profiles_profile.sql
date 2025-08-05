f31efc655cc38da700ccefae422cfd14134a5d6d
rlougee <rlougee@mit.edu>
2025-08-05 10:41:26 -0400

    fix issues after run dbt build

--- src/ol_dbt/models/staging/learn/stg_mitlearn__profiles_profile.sql
+++ src/ol_dbt/models/staging/learn/stg_mitlearn__profiles_profile.sql
@@ -8,18 +8,18 @@ with source as (
     select
         id as profile_id
         , user_id
-        , nullif(name, '') as user_name
-        , nullif(bio, '') as user_bio
-        , nullif(goals, '') as user_goals
-        , nullif(headline, '') as user_headline
-        , nullif(location, '') as user_location
-        , nullif(delivery, '') as user_delivery_preference
-        , nullif(current_education, '') as user_current_education
-        , nullif(time_commitment, '') as user_time_commitment
         , email_optin as user_email_optin
         , toc_optin as user_toc_optin
         , certificate_desired as user_certificate_desired
         , completed_onboarding as user_completed_onboarding
+        , nullif(name, '') as user_name
+        , nullif(bio, '') as user_bio
+        , nullif(goals, array[]) as user_goals
+        , nullif(headline, '') as user_headline
+        , nullif(location, '') as user_location
+        , nullif(delivery, array[]) as user_delivery_preference
+        , nullif(current_education, '') as user_current_education
+        , nullif(time_commitment, '') as user_time_commitment
         , nullif(image, '') as user_image_url
         , nullif(image_small, '') as user_image_small_url
         , nullif(image_medium, '') as user_image_medium_url
