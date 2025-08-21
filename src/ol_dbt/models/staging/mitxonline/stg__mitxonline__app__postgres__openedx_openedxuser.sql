c50a95d6b15ba0356baaffc0e94ffd8acacccb1a
rlougee <rlougee@mit.edu>
2025-08-21 16:27:49 -0400

    stage openedx_openedxuser

---
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__openedx_openedxuser.sql
@@ -0,0 +1,19 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__openedx_openedxuser') }}
+)
+
+, cleaned as (
+
+    select
+        id as openedxuser_id
+        , user_id
+        , platform as openedxuser_platform
+        , edx_username as openedxuser_username
+        , desired_edx_username as openedxuser_desired_username
+        , has_been_synced as openedxuser_has_been_synced
+        , {{ cast_timestamp_to_iso8601('created_on') }} as openedxuser_created_on
+        , {{ cast_timestamp_to_iso8601('updated_on') }} as openedxuser_updated_on
+    from source
+)
+
+select * from cleaned
