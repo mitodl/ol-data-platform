1ae07ce6cabcae5a9d19c8ddc371f5c53b363e6c
Rachel Lougee <rlougee@mit.edu>
2025-09-23 11:40:32 -0400

    Edx course certificate signatories (#1696)
    * stage edx course signature and policy tables
    
    * add edx signatory to migration table
    
    * update description

---
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__course_policy.sql
@@ -0,0 +1,18 @@
+with source as (
+    select * from {{ source('ol_warehouse_raw_data', 'raw__edxorg__s3__course_structure__course_policy') }}
+)
+
+{{ deduplicate_raw_table(order_by='_airbyte_extracted_at' , partition_columns = 'course_id') }}
+, cleaned as (
+
+    select
+        course_id as courserun_readable_id
+        , trim(both '"' from display_coursenumber) as display_coursenumber -- noqa: PRS
+        , self_paced as courserun_is_self_paced
+        , nullif(tabs, '[]') as courserun_tabs_array
+        , nullif(advanced_modules, '[]') as course_advanced_module_array
+        , discussions_settings as course_discussion_settings_object
+    from most_recent_source
+)
+
+select * from cleaned
