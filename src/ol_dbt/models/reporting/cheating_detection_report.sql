efc099cc335bbac44dc62b3da20e356be378c5f7
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-10-07 08:07:45 -0400

    remove extra column added in error

--- src/ol_dbt/models/reporting/cheating_detection_report.sql
+++ src/ol_dbt/models/reporting/cheating_detection_report.sql
@@ -93,7 +93,7 @@ with problem_events as (
         or upper(chapter_name) like '%EXAM %'
         or upper(unit_name) like '%EXAM %', true
         ) as exam_indicator
-        , coalesce((upper(unit_name) like '%HOMEWORK%'
+        , coalesce((upper(unit_name) like '%HOMEWORK%' 
         or upper(chapter_name) like '%HOMEWORK%'), true
         ) as hw_indicator
         , max(max_grade) as max_possible_grade
@@ -140,7 +140,7 @@ with problem_events as (
         or upper(chapter_name) like '%EXAM %'
         or upper(unit_name) like '%EXAM %', true
         )
-        , coalesce((upper(unit_name) like '%HOMEWORK%'
+        , coalesce((upper(unit_name) like '%HOMEWORK%' 
             or upper(chapter_name) like '%HOMEWORK%'), true
         )
 )
@@ -152,7 +152,7 @@ with problem_events as (
         , problem_block_fk
         , approx_percentile(time_spent_on_problem, 0.1) as time_spent_percentile_10
     from final
-    group by
+    group by 
         platform
         , courserun_readable_id
         , problem_block_fk
@@ -175,7 +175,6 @@ select
     , final.time_spent_on_problem
     , final.time_spent_on_problem_nolimit
     , final.courserungrade_grade
-    , approx_percentile(time_spent_on_problem, 0.5)
     , case when final.time_spent_on_problem < ten_percent_time.time_spent_percentile_10
         then 1 else 0 end time_flag
 from final
