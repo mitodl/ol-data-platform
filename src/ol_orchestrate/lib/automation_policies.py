6f6e4fed4a3415f3c448df0725f884941678fdad
rlougee <rlougee@mit.edu>
2025-10-09 11:50:55 -0400

    update any_deps_updated automation policy to use data version

--- src/ol_orchestrate/lib/automation_policies.py
+++ src/ol_orchestrate/lib/automation_policies.py
@@ -3,7 +3,9 @@ from dagster import AutomationCondition
 
 def upstream_or_code_changes() -> AutomationCondition:
     no_upstream_dependencies_in_process = ~AutomationCondition.any_deps_in_progress()
-    has_upstream_changes = AutomationCondition.any_deps_updated()
+    has_upstream_changes = AutomationCondition.any_deps_updated().replace(
+        "newly_updated", AutomationCondition.data_version_changed()
+    )
     has_code_changes = AutomationCondition.code_version_changed()
     newly_missing = AutomationCondition.newly_missing()
     all_upstream_dependencies_present = ~AutomationCondition.any_deps_missing()
