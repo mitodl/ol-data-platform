ffcb9cd21f06dac5d771960e2f898b3279fde9d0
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-10-21 14:57:30 -0400

    add new org admin reporting table

---
+++ src/ol_dbt/models/reporting/organization_administration_report.sql
@@ -0,0 +1,28 @@
+with enrollment_detail as (
+    select * from {{ ref('marts__combined_course_enrollment_detail') }}
+)
+
+, certificate_org_data as (
+    select 
+        platform
+        , course_title
+        , courserun_readable_id
+        , user_email
+        , cast(substring(courseruncertificate_created_on, 1, 10) as date) as certificate_created_date
+    from enrollment_detail
+    group by 
+        platform
+        , course_title
+        , courserun_readable_id
+        , user_email
+        , cast(substring(courseruncertificate_created_on, 1, 10) as date)
+)
+
+select
+    platform
+    , course_title
+    , courserun_readable_id
+    , certificate_created_date
+    , user_email
+    , case when certificate_created_date is not null then 1 else 0 end as certificate_count
+from certificate_org_data
