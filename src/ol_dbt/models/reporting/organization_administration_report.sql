d881dbe9bcdaab15e3cb65ab333545e310d52a4d
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2025-10-23 12:57:58 -0400

    updates based on pr feedback

--- src/ol_dbt/models/reporting/organization_administration_report.sql
+++ src/ol_dbt/models/reporting/organization_administration_report.sql
@@ -2,27 +2,35 @@ with enrollment_detail as (
     select * from {{ ref('marts__combined_course_enrollment_detail') }}
 )
 
+, user_course_roles as (
+    select * from {{ ref('int__combined__user_course_roles') }}
+)
+
+, org_field as (
+    select 
+        distinct courserun_readable_id 
+        , organization
+    from user_course_roles
+)
+
 , certificate_org_data as (
-    select
-        platform
+    select 
+        distinct platform
         , course_title
         , courserun_readable_id
         , user_email
         , cast(substring(courseruncertificate_created_on, 1, 10) as date) as certificate_created_date
     from enrollment_detail
-    group by
-        platform
-        , course_title
-        , courserun_readable_id
-        , user_email
-        , cast(substring(courseruncertificate_created_on, 1, 10) as date)
 )
 
 select
-    platform
-    , course_title
-    , courserun_readable_id
-    , certificate_created_date
-    , user_email
-    , case when certificate_created_date is not null then 1 else 0 end as certificate_count
+    certificate_org_data.platform
+    , certificate_org_data.course_title
+    , certificate_org_data.courserun_readable_id
+    , certificate_org_data.certificate_created_date
+    , certificate_org_data.user_email
+    , case when certificate_org_data.certificate_created_date is not null then 1 else 0 end as certificate_count
+    , org_field.organization
 from certificate_org_data
+left join org_field
+    on certificate_org_data.courserun_readable_id = org_field.courserun_readable_id
