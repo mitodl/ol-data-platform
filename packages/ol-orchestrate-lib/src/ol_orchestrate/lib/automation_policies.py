25adbf4568a788aaf4e329b139c6148dad8564f6
rlougee <rlougee@mit.edu>
2025-10-29 15:49:49 -0400

    move the change to upstream_or_code_changes

--- packages/ol-orchestrate-lib/src/ol_orchestrate/lib/automation_policies.py
+++ packages/ol-orchestrate-lib/src/ol_orchestrate/lib/automation_policies.py
@@ -2,6 +2,7 @@ from dagster import AutomationCondition
 
 
 def upstream_or_code_changes() -> AutomationCondition:
+    not_in_progress = ~AutomationCondition.in_progress()
     no_upstream_dependencies_in_process = ~AutomationCondition.any_deps_in_progress()
     has_upstream_changes = AutomationCondition.any_deps_updated().replace(
         "newly_updated", AutomationCondition.data_version_changed()
@@ -10,7 +11,8 @@ def upstream_or_code_changes() -> AutomationCondition:
     newly_missing = AutomationCondition.newly_missing()
     all_upstream_dependencies_present = ~AutomationCondition.any_deps_missing()
     return (
-        no_upstream_dependencies_in_process
+        not_in_progress
+        & no_upstream_dependencies_in_process
         & (has_upstream_changes | has_code_changes | newly_missing)
         & all_upstream_dependencies_present
     )
