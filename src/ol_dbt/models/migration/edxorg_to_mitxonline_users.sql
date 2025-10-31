492949e0d010b49b6e4d49a4554a8256c133390a
rlougee <rlougee@mit.edu>
2025-10-31 14:13:47 -0400

    update to remove duplicate

--- src/ol_dbt/models/migration/edxorg_to_mitxonline_users.sql
+++ src/ol_dbt/models/migration/edxorg_to_mitxonline_users.sql
@@ -13,16 +13,31 @@ with edx_certificate as (
     select * from {{ ref('int__mitx__users') }}
 )
 
-select distinct
-    edx_certificate.user_email
-    , mitx_user.user_full_name
-    , mitx_user.user_gender
-    , mitx_user.user_birth_year
-    , mitx_user.user_address_country
-from edx_certificate
-inner join edx_run on edx_certificate.courseware_id = edx_run.courseware_id ---match only the migrated course runs
-left join mitx_user on edx_certificate.user_mitxonline_username = mitx_user.user_mitxonline_username
-left join mitx_user as mitx_user2 on edx_certificate.user_email = mitx_user2.user_mitxonline_email
+, edx_certificate_user as (
+   select
+        edx_certificate.*
+        , mitx_user.user_gender
+        , mitx_user.user_birth_year
+        , mitx_user.user_address_country
+        , row_number() over (
+              partition by edx_certificate.user_email order by mitx_user.user_joined_on_edxorg desc
+        ) as row_number
+    from edx_certificate
+    inner join mitx_user on edx_certificate.user_email = mitx_user.user_edxorg_email
+    inner join edx_run on edx_certificate.courseware_id = edx_run.courseware_id ---match only the migrated course runs
+
+)
+
+select
+    edx_certificate_user.user_email
+    , edx_certificate_user.user_full_name
+    , edx_certificate_user.user_gender
+    , edx_certificate_user.user_birth_year
+    , edx_certificate_user.user_address_country
+from edx_certificate_user
+left join mitx_user as mitx_user1 on edx_certificate_user.user_mitxonline_username = mitx_user1.user_mitxonline_username
+left join mitx_user as mitx_user2 on edx_certificate_user.user_email = mitx_user2.user_mitxonline_email
 where
-    mitx_user.user_mitxonline_username is null
+    edx_certificate_user.row_number = 1
+    and mitx_user1.user_mitxonline_username is null
     and mitx_user2.user_mitxonline_email is null
