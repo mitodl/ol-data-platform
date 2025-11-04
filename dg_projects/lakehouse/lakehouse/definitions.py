a81e76af4c16513789815076d53e415301458b14
sar <sar.haidar@gmail.com>
2025-11-04 11:25:57 -0600

    Use Airbyte API integration in data_platform

--- dg_projects/lakehouse/lakehouse/definitions.py
+++ dg_projects/lakehouse/lakehouse/definitions.py
@@ -23,11 +23,11 @@ from dagster_dbt import (
 )
 from ol_orchestrate.lib.constants import DAGSTER_ENV, VAULT_ADDRESS
 from ol_orchestrate.lib.utils import authenticate_vault
+from ol_orchestrate.resources.airbyte import AirbyteOSSWorkspace
 from ol_orchestrate.resources.secrets.vault import Vault
 
 from lakehouse.assets.lakehouse.dbt import DBT_REPO_DIR, full_dbt_project
 from lakehouse.assets.superset import create_superset_asset
-from lakehouse.resources.airbyte import AirbyteOSSWorkspace
 from lakehouse.resources.superset_api import SupersetApiClientFactory
 
 airbyte_host_map = {
