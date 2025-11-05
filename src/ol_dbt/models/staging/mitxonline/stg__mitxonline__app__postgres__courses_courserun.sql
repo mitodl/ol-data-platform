a1250337ba173a675633e19e231fb4d4d565100b
Rachel Lougee <rlougee@mit.edu>
2025-11-05 14:57:58 -0500

    Add B2B related dbt models (#1763)
    * stage b2b_organizationpage
    
    * indentation
    
    * stage b2b_contractpage
    
    * b2b_contractpage
    
    * Create int__mitxonline__b2b_contract_to_courseruns
    
    * remove extra line

--- src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_courserun.sql
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_courserun.sql
@@ -14,6 +14,7 @@ with source as (
         , courseware_url_path as courserun_url
         , run_tag as courserun_tag
         , is_self_paced as courserun_is_self_paced
+        , b2b_contract_id
         , case
             when courseware_id like 'MITx/%' or courseware_id like 'course-v1:MITx+%'
                 then '{{ var("edxorg") }}'
@@ -26,6 +27,7 @@ with source as (
         ,{{ cast_timestamp_to_iso8601('enrollment_end') }} as courserun_enrollment_end_on
         ,{{ cast_timestamp_to_iso8601('expiration_date') }} as courserun_expired_on
         ,{{ cast_timestamp_to_iso8601('upgrade_deadline') }} as courserun_upgrade_deadline
+        ,{{ cast_timestamp_to_iso8601('certificate_available_date') }} as courserun_certificate_available_on
         ,{{ cast_timestamp_to_iso8601('created_on') }} as courserun_created_on
         ,{{ cast_timestamp_to_iso8601('updated_on') }} as courserun_updated_on
     from source
