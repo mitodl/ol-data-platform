89df7ae038fed51a7ce18897bea712cbc8a6eed3
Rachel Lougee <rlougee@mit.edu>
2026-01-08 11:16:17 -0500

    Fix data issues in edxorg_to_mitxonline_enrollments (#1829)
    * Update the edx to mitxonline migration model
    
    * fix order_by
    
    * copilot feedback

--- src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__course_certificate_signatory.sql
+++ src/ol_dbt/models/staging/edxorg/stg__edxorg__s3__course_certificate_signatory.sql
@@ -3,7 +3,30 @@ with source as (
     from {{ source('ol_warehouse_raw_data', 'raw__edxorg__s3__course_structure__course_certificate_signatory') }}
 )
 
-{{ deduplicate_raw_table(order_by='_airbyte_extracted_at' , partition_columns = 'id, course_id') }}
+, source_sorted as (
+    select
+        *
+        , dense_rank() over (partition by course_id order by _ab_source_file_last_modified desc) as dense_row
+    from source
+)
+
+, most_recent_source as (
+    select * from source_sorted
+    where dense_row = 1
+)
+
+, deduplicated as (
+    select
+        *
+        , row_number() over (partition by id, course_id order by _airbyte_extracted_at desc) as row_num
+    from most_recent_source
+)
+
+, final_source as (
+    select * from deduplicated
+    where row_num = 1
+)
+
 , cleaned as (
 
     select
@@ -15,7 +38,7 @@ with source as (
         , trim(name) as signatory_name
         , regexp_replace(trim(name), '^(Professor |Dr\. )', '') as signatory_normalized_name
         , concat('https://courses.edx.org', nullif(signature_image_path, '')) as signatory_image_url
-    from most_recent_source
+    from final_source
 )
 
 select * from cleaned
