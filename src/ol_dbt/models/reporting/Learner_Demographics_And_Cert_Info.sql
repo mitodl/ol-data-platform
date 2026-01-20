0234acb415879b13999f5abb310f4cca72155c66
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2026-01-20 14:44:01 -0500

    fixed mixed-case

--- src/ol_dbt/models/reporting/Learner_Demographics_And_Cert_Info.sql
+++ src/ol_dbt/models/reporting/Learner_Demographics_And_Cert_Info.sql
@@ -15,19 +15,19 @@ with combined__users as (
 )
 
 , dedp_indicators as (
-    select
+    select 
         user_hashed_id
         , 1 as Any_DEDP_Program_Indicator
-        , count(distinct case
+        , count(distinct case 
             when program_title =
             'Data, Economics, and Design of Policy: International Development'
             then user_hashed_id else null end) as DEDP_International_Program_Indicator
-        , count (distinct case
+        , count (distinct case 
             when program_title =
             'Data, Economics, and Design of Policy: Public Policy'
             then user_hashed_id else null end) as DEDP_Public_Policy_Program_Indicator
     from program_enrollment_detail
-    where program_title in
+    where program_title in 
         ('Data, Economics, and Design of Policy'
         , 'Data, Economics, and Design of Policy: International Development'
         , 'Data, Economics, and Design of Policy: Public Policy')
@@ -37,7 +37,7 @@ with combined__users as (
 )
 
 , dedp_certs as (
-    select
+    select 
         combined_course_enrollment_detail.user_hashed_id
         , count(distinct combined_course_enrollment_detail.course_readable_id) as DEDP_Course_Cert_Count
     from combined_course_enrollment_detail
@@ -68,13 +68,16 @@ select
     , combined__users.first_course_start_datetime
     , combined__users.last_course_start_datetime
     , combined__users.num_of_course_enrolled
-    , dedp_certs.DEDP_Course_Cert_Count
-    , case when dedp_indicators.Any_DEDP_Program_Indicator= 1 then 'Y' else null end as Any_DEDP_Program_Cert_Ind
-    , case when dedp_indicators.DEDP_International_Program_Indicator= 1 then 'Y' else null end as DEDP_International_Program_Cert_Ind
-    , case when dedp_indicators.DEDP_Public_Policy_Program_Indicator= 1 then 'Y' else null end as DEDP_Public_Policy_Program_Cert_Ind
+    , dedp_certs.dedp_course_cert_count
+    , case when dedp_indicators.Any_DEDP_Program_Indicator= 1 then 'Y' else null end as any_dedp_program_cert_ind
+    , case when dedp_indicators.DEDP_International_Program_Indicator= 1 then 'Y' else null end as dedp_international_program_cert_ind
+    , case when dedp_indicators.DEDP_Public_Policy_Program_Indicator= 1 then 'Y' else null end as dedp_public_policy_program_cert_ind
 from combined__users
 left join dedp_indicators
     on combined__users.user_hashed_id = dedp_indicators.user_hashed_id
 left join dedp_certs
     on combined__users.user_hashed_id = dedp_certs.user_hashed_id
 where combined__users.user_hashed_id is not null
+
+
+
