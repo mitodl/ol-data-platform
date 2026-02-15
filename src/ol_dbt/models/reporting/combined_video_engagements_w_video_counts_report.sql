c204d757177c030519a2be025ab0e168b531a51c
Quazi <quazi@mit.edu>
2026-02-14 21:23:29 -0500

    Add combined_video_engagements_w_video_counts_report
    - Create reporting model that extends marts__combined_video_engagements
    - Add total_video_count per chapter via subquery
    - Add courserun_is_current boolean (checks if course is currently active)
    - Add user_watched_video_count aggregation
    - Add max_video_index tracking
    - Filter for play_video events only
    - Add full documentation and tests to _reporting__models.yml
    - Replaces Superset virtual dataset with physical table

---
+++ src/ol_dbt/models/reporting/combined_video_engagements_w_video_counts_report.sql
@@ -0,0 +1,54 @@
+with combined_video_engagements as (
+    select * from {{ ref('marts__combined_video_engagements') }}
+)
+
+, video_counts_by_chapter as (
+    select
+        chapter_title
+        , courserun_readable_id
+        , count(distinct video_title) as total_video_count
+    from combined_video_engagements
+    group by
+        chapter_title
+        , courserun_readable_id
+)
+
+select
+    combined_video_engagements.user_email
+    , combined_video_engagements.chapter_title
+    , combined_video_engagements.courserun_readable_id
+    , video_counts_by_chapter.total_video_count
+    , combined_video_engagements.platform
+    , case
+        when cast(substring(combined_video_engagements.courserun_start_on, 1, 10) as date) <= current_date
+            and (
+                combined_video_engagements.courserun_end_on is null
+                or cast(substring(combined_video_engagements.courserun_end_on, 1, 10) as date) >= current_date
+            )
+            then true
+        else false
+    end as courserun_is_current
+    , combined_video_engagements.course_readable_id
+    , count(distinct combined_video_engagements.video_title) as user_watched_video_count
+    , max(combined_video_engagements.video_index) as max_video_index
+from combined_video_engagements
+join video_counts_by_chapter
+    on combined_video_engagements.chapter_title = video_counts_by_chapter.chapter_title
+    and combined_video_engagements.courserun_readable_id = video_counts_by_chapter.courserun_readable_id
+where combined_video_engagements.video_event_type = 'play_video'
+group by
+    combined_video_engagements.user_email
+    , combined_video_engagements.chapter_title
+    , combined_video_engagements.courserun_readable_id
+    , video_counts_by_chapter.total_video_count
+    , combined_video_engagements.platform
+    , case
+        when cast(substring(combined_video_engagements.courserun_start_on, 1, 10) as date) <= current_date
+            and (
+                combined_video_engagements.courserun_end_on is null
+                or cast(substring(combined_video_engagements.courserun_end_on, 1, 10) as date) >= current_date
+            )
+            then true
+        else false
+    end
+    , combined_video_engagements.course_readable_id
