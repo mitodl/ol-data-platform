1bcd7e345093675b09675417e4bd3f71fbe54b0e
Quazi <quazi@mit.edu>
2026-02-17 09:48:29 -0500

    Add program_enrollment_with_user_report
    - Create reporting model that extends marts__combined_program_enrollment_detail
    - Join marts__combined__users for user demographic data (education, industry, company, etc.)
    - Join marts__combined_coursesinprogram and marts__combined_course_enrollment_detail for courses_taken_in_program
    - Add capstone_ind indicator based on course title
    - Add program_complete_days calculated from first courserun start to certificate date
    - Add full documentation and tests to _reporting__models.yml
    - Replaces Superset virtual dataset with physical table

---
+++ src/ol_dbt/models/reporting/program_enrollment_with_user_report.sql
@@ -0,0 +1,71 @@
+with enrollment_detail as (
+    select * from {{ ref('marts__combined_program_enrollment_detail') }}
+)
+
+, combined_users as (
+    select * from {{ ref('marts__combined__users') }}
+)
+
+, courses_in_program as (
+    select * from {{ ref('marts__combined_coursesinprogram') }}
+)
+
+, course_enrollment_detail as (
+    select * from {{ ref('marts__combined_course_enrollment_detail') }}
+)
+
+, courses_detail as (
+    select
+        courses_in_program.program_name
+        , course_enrollment_detail.user_email
+        , count(distinct courses_in_program.course_readable_id) as courses_taken_in_program
+        , sum(
+            case
+                when upper(courses_in_program.course_title) like '%CAPSTONE%' then 1
+                else 0
+            end
+        ) as capstone_sum
+        , min(cast(substring(course_enrollment_detail.courserun_start_on, 1, 10) as date))
+            as first_courserun_start_on_date
+    from courses_in_program
+    inner join course_enrollment_detail
+        on courses_in_program.course_readable_id = course_enrollment_detail.course_readable_id
+    where course_enrollment_detail.courserunenrollment_enrollment_mode = 'verified'
+    group by
+        courses_in_program.program_name
+        , course_enrollment_detail.user_email
+)
+
+select
+    enrollment_detail.*
+    , coalesce(
+        combined_users.user_highest_education, combined_users2.user_highest_education
+    ) as user_highest_education
+    , coalesce(combined_users.user_industry, combined_users2.user_industry) as user_industry
+    , coalesce(combined_users.user_company, combined_users2.user_company) as user_company
+    , coalesce(combined_users.user_job_title, combined_users2.user_job_title) as user_job_title
+    , coalesce(combined_users.user_birth_year, combined_users2.user_birth_year) as user_birth_year
+    , coalesce(combined_users.user_gender, combined_users2.user_gender) as user_gender
+    , coalesce(
+        combined_users.user_address_country, combined_users2.user_address_country
+    ) as user_address_country
+    , courses_detail.courses_taken_in_program
+    , combined_users.user_street_address
+    , combined_users.user_address_city
+    , combined_users.user_address_state_or_territory
+    , combined_users.user_address_postal_code
+    , case when courses_detail.capstone_sum > 0 then 'Y' else 'N' end as capstone_ind
+    , date_diff(
+        'day'
+        , courses_detail.first_courserun_start_on_date
+        , cast(substring(enrollment_detail.programcertificate_created_on, 1, 10) as date)
+    ) as program_complete_days
+from enrollment_detail
+left join combined_users
+    on enrollment_detail.platform_name = 'edX.org'
+    and enrollment_detail.user_id = combined_users.user_edxorg_id
+left join combined_users as combined_users2
+    on enrollment_detail.user_hashed_id = combined_users2.user_hashed_id
+left join courses_detail
+    on enrollment_detail.program_name = courses_detail.program_name
+    and enrollment_detail.user_email = courses_detail.user_email
