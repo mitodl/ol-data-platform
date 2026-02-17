23fadefff4d87f2a4f8c7a62d4ee332f9df3c1ba
Quazi <quazi@mit.edu>
2026-02-17 12:13:26 -0500

    Add page_engagement_views_report
    - Create reporting model joining afact_course_page_engagement with course content hierarchy and user data
    - Join dim_course_content for unit, subsection, and section titles and indexes
    - Join dim_user for user email and full name
    - Join int__combined__course_runs for course title
    - Add full documentation and tests to _reporting__models.yml
    - Replaces Superset virtual dataset with physical table

---
+++ src/ol_dbt/models/reporting/page_engagement_views_report.sql
@@ -0,0 +1,57 @@
+with page_engagement as (
+    select * from {{ ref('afact_course_page_engagement') }}
+)
+
+, dim_user as (
+    select * from {{ ref('dim_user') }}
+)
+
+, course_runs as (
+    select
+        course_title
+        , courserun_readable_id
+    from {{ ref('int__combined__course_runs') }}
+    group by
+        course_title
+        , courserun_readable_id
+)
+
+, unit_blocks as (
+    select * from {{ ref('dim_course_content') }}
+    where block_category = 'vertical'
+    and is_latest = true
+)
+
+, subsection_blocks as (
+    select * from {{ ref('dim_course_content') }}
+    where is_latest = true
+)
+
+, section_blocks as (
+    select * from {{ ref('dim_course_content') }}
+    where is_latest = true
+)
+
+select
+    dim_user.email as user_email
+    , dim_user.full_name
+    , page_engagement.platform
+    , page_engagement.courserun_readable_id
+    , page_engagement.num_of_views
+    , unit_blocks.block_title as unit_title
+    , subsection_blocks.block_title as subsection_title
+    , subsection_blocks.block_index as subsection_block_index
+    , section_blocks.block_title as section_title
+    , section_blocks.block_index as section_block_index
+    , course_runs.course_title
+from page_engagement
+inner join unit_blocks
+    on page_engagement.block_fk = unit_blocks.block_id
+inner join subsection_blocks
+    on page_engagement.sequential_block_fk = subsection_blocks.block_id
+inner join section_blocks
+    on page_engagement.chapter_block_fk = section_blocks.block_id
+inner join dim_user
+    on page_engagement.openedx_user_id = dim_user.mitxonline_openedx_user_id
+left join course_runs
+    on page_engagement.courserun_readable_id = course_runs.courserun_readable_id
