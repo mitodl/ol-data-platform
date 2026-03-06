82b765587a67c556d44351119d9bea8e4369cde0
Rachel Lougee <rlougee@mit.edu>
2026-03-06 10:45:23 -0500

    Add mitx program entitlement to dbt models (#1958)
    * staging program entitlement
    
    * add entitlement user to migration users
    
    * add dbt test and use union distinct

--- src/ol_dbt/models/migration/edxorg_to_mitxonline_users.sql
+++ src/ol_dbt/models/migration/edxorg_to_mitxonline_users.sql
@@ -6,6 +6,13 @@ with edx_certificate as (
     where user_email not like 'retired__user%' and user_username not like 'retired__user%'
 )
 
+, program_entitlements as (
+    select distinct
+        user_email
+        , user_id as user_edxorg_id
+    from {{ ref('stg__edxorg__program_entitlement') }}
+)
+
 , mitx_user as (
     select * from {{ ref('int__mitx__users') }}
 )
@@ -35,4 +42,21 @@ left join mitx_user as mitx_user2 on lower(edx_certificate_user.user_email) = lo
 where
     edx_certificate_user.row_number = 1
     and mitx_user1.user_mitxonline_username is null
+    and mitx_user2.user_mitxonline_email is null
+
+union distinct
+
+select
+    program_entitlements.user_email
+    , mitx_user.user_full_name
+    , mitx_user.user_gender
+    , mitx_user.user_birth_year
+    , mitx_user.user_address_country
+from program_entitlements
+left join mitx_user
+     on program_entitlements.user_edxorg_id = mitx_user.user_edxorg_id
+left join mitx_user as mitx_user2
+    on lower(program_entitlements.user_email) = lower(mitx_user2.user_mitxonline_email)
+where
+    mitx_user.user_mitxonline_id is null
     and mitx_user2.user_mitxonline_email is null
