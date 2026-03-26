e674c4155e865d9527f457451ebe3c33ce9fe102
Kate Delaney <134744245+KatelynGit@users.noreply.github.com>
2026-03-26 16:06:51 -0400

    add last virtual to physical table Data_Detail_Nav (#2040)
    * add last virtual to physical table Data Detail Nav

---
+++ src/ol_dbt/models/reporting/Data_Detail_Nav.sql
@@ -0,0 +1,29 @@
+with navigation_events as (
+    select * from {{ ref('tfact_course_navigation_events') }}
+)
+
+, combined__course_runs as (
+    select * from {{ ref('int__combined__course_runs') }}
+)
+
+select
+    navigation_events.platform
+    , combined__course_runs.course_title
+    , navigation_events.courserun_readable_id
+    , navigation_events.user_username
+    , navigation_events.event_type
+    , navigation_events.starting_position
+    , navigation_events.ending_position
+    , navigation_events.event_timestamp
+from  navigation_events
+inner join combined__course_runs
+    on navigation_events.courserun_readable_id = combined__course_runs.courserun_readable_id
+group by
+    navigation_events.platform
+    , combined__course_runs.course_title
+    , navigation_events.courserun_readable_id
+    , navigation_events.user_username
+    , navigation_events.event_type
+    , navigation_events.starting_position
+    , navigation_events.ending_position
+    , navigation_events.event_timestamp
