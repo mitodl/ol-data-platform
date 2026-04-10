9a4dedd4811d294857c4c851d257322c5e75ecf3
rlougee <rlougee@mit.edu>
2026-04-10 14:58:25 -0400

    feat: add tax information to fact order and related models

---
+++ src/ol_dbt/models/dimensional/dim_tax_rate.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='table'
+) }}
+
+with xpro_tax_rates as (
+    select * from {{ ref('stg__mitxpro__app__postgres__ecommerce_taxrate') }}
+)
+
+--- Grain: one row per unique tax rate.
+select
+    {{ dbt_utils.generate_surrogate_key(['taxrate_country_code']) }} as tax_rate_pk
+    , taxrate_country_code as country_code
+    , taxrate_tax_rate as tax_rate
+    , taxrate_tax_rate_name as tax_rate_name
+    , taxrate_is_active as is_active
+from xpro_tax_rates
