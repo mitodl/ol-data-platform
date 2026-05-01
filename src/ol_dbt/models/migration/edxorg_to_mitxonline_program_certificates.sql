0f8a70895d059c5fe5831b2ad7772097b8915810
rlougee <rlougee@mit.edu>
2026-05-01 17:04:17 -0400

    feat: normalize program titles in migration model and update certificate page revision logic

--- src/ol_dbt/models/migration/edxorg_to_mitxonline_program_certificates.sql
+++ src/ol_dbt/models/migration/edxorg_to_mitxonline_program_certificates.sql
@@ -1,13 +1,24 @@
-with edxorg_program_certificates as (
+with edxorg_program_certificates_raw as (
+    select
+        *
+        , case
+            when program_title = 'MIT Finance' then 'Finance'
+            when program_title = 'Supply Chain Management' then 'MITx MicroMasters® Program in Supply Chain Management'
+            else program_title
+        end as normalized_program_title
+    from {{ ref('int__edxorg__mitx_program_certificates') }}
+    where user_username not like 'retired__user%'
+)
+
+, edxorg_program_certificates as (
     select * from (
         select
             *
             , row_number() over (
-                partition by user_id, program_title
+                partition by user_id, normalized_program_title
                 order by program_certificate_awarded_on
             ) as row_num
-        from {{ ref('int__edxorg__mitx_program_certificates') }}
-        where user_username not like 'retired__user%'
+        from edxorg_program_certificates_raw
     )
     where row_num = 1
 )
@@ -43,7 +54,7 @@ with edxorg_program_certificates as (
 , mitxonline_program_certificate_page as (
     select
         program_pages.program_id
-        , certificate_page.wagtail_page_live_pagerevision_id as certificate_page_revision_id
+        , min(certificate_page.wagtail_page_live_pagerevision_id) as certificate_page_revision_id
     from program_pages
     join wagtail_page
         on program_pages.wagtail_page_id = wagtail_page.wagtail_page_id
@@ -51,6 +62,7 @@ with edxorg_program_certificates as (
         on certificate_page.wagtail_page_path like wagtail_page.wagtail_page_path || '%'
         and certificate_page.wagtail_page_path <> wagtail_page.wagtail_page_path
         and certificate_page.wagtail_page_slug like 'certificate%'
+    group by program_pages.program_id
 )
 
 select
@@ -69,7 +81,7 @@ select
     , mitxonline_program_certificate_page.certificate_page_revision_id
 from edxorg_program_certificates
 left join mitxonline_programs
-    on lower(mitxonline_programs.program_title) = lower(edxorg_program_certificates.program_title)
+    on lower(mitxonline_programs.program_title) = lower(edxorg_program_certificates.normalized_program_title)
 left join mitx__users
     on edxorg_program_certificates.user_id = mitx__users.user_edxorg_id
 left join mitx__users as mitx_users_by_email
