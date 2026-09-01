40816828096d33a9527719f6e0a4aabd99f82f26
rlougee <rlougee@mit.edu>
2026-09-01 11:16:14 -0400

    fix(raw-data): deduplicate raw tables and update source references in SQL files

--- src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__b2b_organizationpage.sql
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__b2b_organizationpage.sql
@@ -2,6 +2,8 @@ with source as (
     select * from {{ source('ol_warehouse_raw_data', 'raw__mitxonline__app__postgres__b2b_organizationpage') }}
 )
 
+{{ deduplicate_raw_table(order_by='_airbyte_extracted_at', partition_columns='page_ptr_id') }}
+
 , cleaned as (
     select
         page_ptr_id as organization_id,
@@ -10,7 +12,7 @@ with source as (
         logo as organization_logo,
         description as organization_description,
         sso_organization_id
-    from source
+    from most_recent_source
 )
 
 select * from cleaned
