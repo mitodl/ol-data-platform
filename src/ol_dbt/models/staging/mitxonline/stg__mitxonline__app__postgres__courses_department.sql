bca3586db6cf2e021461e43eaa8242756fc67292
rlougee <rlougee@mit.edu>
2026-09-01 13:48:05 -0400

    fix(raw-data): deduplicate raw tables and update source references in SQL files

--- src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_department.sql
+++ src/ol_dbt/models/staging/mitxonline/stg__mitxonline__app__postgres__courses_department.sql
@@ -4,13 +4,15 @@ with source as (
     select * from {{ source('ol_warehouse_raw_data','raw__mitxonline__app__postgres__courses_department') }}
 )
 
+{{ deduplicate_raw_table(order_by='_airbyte_extracted_at', partition_columns='id') }}
+
 , cleaned as (
     select
         id as coursedepartment_id
         , name as coursedepartment_name
         ,{{ cast_timestamp_to_iso8601('created_on') }} as coursedepartment_created_on
         ,{{ cast_timestamp_to_iso8601('updated_on') }} as coursedepartment_updated_on
-    from source
+    from most_recent_source
 )
 
 select * from cleaned
